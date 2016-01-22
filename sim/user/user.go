f3bc03d55c66dbb0e5359d8001b2bbef720b7e26
Rob Pike <r@google.com>
2016-01-22 10:02:42 -0800

    sim: actually include changes that update to new upspin interface definitions
    The previous CL was just the deletion of an unused package.
    Sigh.
    
    Change-Id: I4ef22eddaf2ecfcaf6d625cb47ee2dca5707e006

--- sim/user/user.go
+++ sim/user/user.go
@@ -4,18 +4,19 @@ package user
 import (
 	"errors"
 
-	"upspin.googlesource.com/upspin.git/sim/path"
-	"upspin.googlesource.com/upspin.git/sim/ref"
+	"upspin.googlesource.com/upspin.git/upspin"
 )
 
-// Service maps user names to references to the root of the user's tree.
+// Service maps user names to potential machines holdining root of the user's tree.
 type Service struct {
-	root map[path.UserName][]ref.Location
+	root map[upspin.UserName][]upspin.NetAddr
 }
+
+var _ upspin.User = (*Service)(nil)
 
 // UserLookup reports the set of locations the user's directory might be,
 // with the earlier entries being the best choice; later entries are fallbacks.
-func (s *Service) Lookup(name path.UserName) ([]ref.Location, error) {
+func (s *Service) Lookup(name upspin.UserName) ([]upspin.NetAddr, error) {
 	locs, ok := s.root[name]
 	if !ok {
 		return nil, errors.New("no such user")
