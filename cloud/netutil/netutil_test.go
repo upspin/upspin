68bab5b324547180075b7b8998116bb044076ec5
Eduardo Pinheiro <edpin@google.com>
2016-01-25 13:25:37 -0800

    netutil: utility functions for dealing with networking and http conditions.
    Change-Id: Ic33ac39690bccf25cfea299632b1e27b56892d03

---
+++ cloud/netutil/netutil_test.go
@@ -0,0 +1,94 @@
+package netutil
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"testing"
+)
+
+type ExpectingResponseWriter struct {
+	expectedResponse string
+	response         string
+	header           http.Header
+}
+
+func (e *ExpectingResponseWriter) Write(response []byte) (n int, err error) {
+	e.response = string(response)
+	return len(response), nil
+}
+
+func (e *ExpectingResponseWriter) Header() http.Header {
+	return e.header
+}
+
+func (e *ExpectingResponseWriter) WriteHeader(int) {
+}
+
+func (e *ExpectingResponseWriter) Verify(t *testing.T) {
+	if e.expectedResponse != e.response {
+		t.Errorf("Expected %q got %q", e.expectedResponse, e.response)
+	}
+}
+
+func NewExpectingResponseWriter(expected string) *ExpectingResponseWriter {
+	resp := &ExpectingResponseWriter{
+		header:           make(http.Header),
+		expectedResponse: expected,
+	}
+	return resp
+}
+
+func TestSendJSONErrorString(t *testing.T) {
+	resp := NewExpectingResponseWriter("{error:\"Something bad happened\"}")
+	SendJSONErrorString(resp, "Something bad happened")
+	resp.Verify(t)
+}
+
+func TestSendJSONError(t *testing.T) {
+	resp := NewExpectingResponseWriter("{error:\"error reading:EOF\"}")
+	SendJSONError(resp, "error reading:", io.EOF)
+	resp.Verify(t)
+}
+
+func TestSendJSONReply(t *testing.T) {
+	resp := NewExpectingResponseWriter("{\"A\":\"foo\",\"B\":32}")
+	SendJSONReply(resp, &struct {
+		A string
+		B int
+	}{A: "foo", B: 32})
+	resp.Verify(t)
+}
+
+var (
+	data = "some data"
+)
+
+func TestBufferRequest(t *testing.T) {
+	resp := NewExpectingResponseWriter("") // Nothing is sent
+	req, err := http.NewRequest("POST", "http://localhost:8080/put", bytes.NewBufferString(data))
+	if err != nil {
+		t.Fatal("Can't make new request")
+	}
+	buf := BufferRequest(resp, req, 1000)
+	if len(buf) != len(data) {
+		t.Fatalf("Buffer size mismatch")
+	}
+	if string(buf) != data {
+		t.Fatalf("Expected %q got %q", data, string(buf))
+	}
+	resp.Verify(t)
+}
+
+func TestBufferRequestTooBig(t *testing.T) {
+	resp := NewExpectingResponseWriter("{error:\"Invalid request\"}") // Request is too big
+	req, err := http.NewRequest("POST", "http://localhost:8080/put", bytes.NewBufferString(data))
+	if err != nil {
+		t.Fatal("Can't make new request")
+	}
+	buf := BufferRequest(resp, req, 5)
+	if len(buf) > 5 {
+		t.Fatalf("Buffer size mismatch")
+	}
+	resp.Verify(t)
+}
