4d54550db11d8221719c16a969d5b14eabf62882
Eduardo Pinheiro <edpin@google.com>
2016-01-25 16:46:08 -0800

    nettest: helper functions for testing http code
    Change-Id: I629b1f7021bdc8b8086931099e22b294f432e552
    Reviewed-on: https://upspin-review.git.corp.google.com/1085
    Reviewed-by: Rob Pike <r@google.com>

---
+++ cloud/netutil/nettest/nettest.go
@@ -0,0 +1,48 @@
+// Package nettest implements helper functions for writing http tests
+package nettest
+
+import (
+	"net/http"
+	"testing"
+)
+
+// ExpectingResponseWriter mimics a ResponseWriter and compares an
+// expected response with the actual response written by callers.
+type ExpectingResponseWriter struct {
+	expectedResponse string
+	response         string
+	header           http.Header
+}
+
+// Write implements io.Writer.
+func (e *ExpectingResponseWriter) Write(response []byte) (n int, err error) {
+	e.response = string(response)
+	return len(response), nil
+}
+
+// Header returns the http header.
+func (e *ExpectingResponseWriter) Header() http.Header {
+	return e.header
+}
+
+// WriteHeader writes the status code.
+func (e *ExpectingResponseWriter) WriteHeader(int) {
+}
+
+// Verify checks that the response expected is the same as the one
+// received. If they're different, it logs the error to the output of
+// the test.
+func (e *ExpectingResponseWriter) Verify(t *testing.T) {
+	if e.expectedResponse != e.response {
+		t.Errorf("Expected %q got %q", e.expectedResponse, e.response)
+	}
+}
+
+// NewExpectingResponseWriter creates a new object with the expected response.
+func NewExpectingResponseWriter(expected string) *ExpectingResponseWriter {
+	resp := &ExpectingResponseWriter{
+		header:           make(http.Header),
+		expectedResponse: expected,
+	}
+	return resp
+}
