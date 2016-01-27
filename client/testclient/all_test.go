c64b304178d5911cf418d60b6c75efd2b9b5d0ae
Rob Pike <r@google.com>
2016-01-27 11:19:03 -0800

    client/testclient: test File implementation
    This adds simple tests for the File implementation of the test client.
    The implementation itself is also ready for code review now.
    
    Other than the long list of TODOs, and perhaps a few more tests,
    this finishes the testclient implementation, absent crypto and absent
    the right story about how to stitch together a working system.
    
    Change-Id: Ie78bcdf2581a754aebfd3be5c742c830e7372f54

--- client/testclient/all_test.go
+++ client/testclient/all_test.go
@@ -1,6 +1,8 @@
 package testclient
 
 import (
+	"bytes"
+	"math/rand"
 	"testing"
 
 	"upspin.googlesource.com/upspin.git/directory/testdir"
@@ -70,5 +72,168 @@ func TestPutGetTopLevelFile(t *testing.T) {
 	}
 	if string(data) != text {
 		t.Fatalf("get of %q has text %q; should be %q", fileName, data, text)
+	}
+}
+
+const (
+	Max      = 100 * 1000 // Must be > 100.
+	fileName = root + "file"
+)
+
+func setupFileIO(t *testing.T) (*Client, upspin.File, []byte) {
+	ss := teststore.NewService(upspin.NetAddr{Addr: testAddr})
+	client := New(testdir.NewService(ss), ss)
+	_, err := client.MakeDirectory(user)
+	if err != nil {
+		t.Fatal("make directory:", err)
+	}
+	f, err := client.Create(fileName)
+	if err != nil {
+		t.Fatal("create file:", err)
+	}
+
+	// Create a data set with each byte equal to its offset.
+	data := make([]byte, Max)
+	for i := range data {
+		data[i] = uint8(i)
+	}
+	return client, f, data
+}
+
+func TestFileSequentialAccess(t *testing.T) {
+	client, f, data := setupFileIO(t)
+
+	// Write the file in randomly sized chunks until it's full.
+	for offset, length := 0, 0; offset < Max; offset += length {
+		// Pick a random length.
+		length = rand.Intn(Max / 100)
+		if offset+length > Max {
+			length = Max - offset
+		}
+		n, err := f.Write(data[offset : offset+length])
+		if err != nil {
+			t.Fatalf("Write(offset %d length %d): %v", offset, length, err)
+		}
+		if n != length {
+			t.Fatalf("Write length failed: offset %d expected %d got %d", offset, length, n)
+		}
+	}
+	err := f.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Now read it back with a similar scan.
+	f, err = client.Open(fileName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	buf := make([]byte, Max)
+	for offset, length := 0, 0; offset < Max; offset += length {
+		length = rand.Intn(Max / 100)
+		if offset+length > Max {
+			length = Max - offset
+		}
+		n, err := f.Read(buf[offset : offset+length])
+		if err != nil {
+			t.Fatalf("Read(offset %d length %d): %v", offset, length, err)
+		}
+		if n != length {
+			t.Fatalf("Read length failed: offset %d expected %d got %d", offset, length, n)
+		}
+		for i := offset; i < offset+length; i++ {
+			if buf[i] != data[i] {
+				t.Fatalf("Read at %d (%#x): expected %#.2x got %#.2x", i, i, data[i], buf[i])
+			}
+		}
+	}
+}
+
+func TestFileRandomAccess(t *testing.T) {
+	client, f, data := setupFileIO(t)
+
+	// Use WriteAt at random offsets and random sizes to create file.
+	// Start with a map of bools (easy) saying the byte has been written.
+	// Loop until its length is the file size, meaning every byte has been written.
+	written := make(map[int]bool)
+	for len(written) != Max {
+		// Pick a random offset and length.
+		offset := rand.Intn(Max)
+		// Don't bother starting at a known location - speeds up the coverage.
+		for written[offset] {
+			offset = rand.Intn(Max)
+		}
+		length := rand.Intn(Max / 100)
+		if offset+length > Max {
+			length = Max - offset
+		}
+		n, err := f.WriteAt(data[offset:offset+length], int64(offset))
+		if err != nil {
+			t.Fatalf("WriteAt(offset %d length %d): %v", offset, length, err)
+		}
+		if n != length {
+			t.Fatalf("WriteAt length failed: offset %d expected %d got %d", offset, length, n)
+		}
+		for i := offset; i < offset+length; i++ {
+			written[i] = true
+		}
+	}
+	err := f.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read file back all at once, for simple verification.
+	f, err = client.Open(fileName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	result := make([]byte, Max)
+	n, err := f.Read(result)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != Max {
+		t.Fatalf("Read: expected %d got %d", Max, n)
+	}
+	if !bytes.Equal(data, result) {
+		for i, c := range data {
+			if result[i] != c {
+				t.Fatalf("byte at offset %d should be %#.2x is %#.2x", i, c, result[i])
+			}
+		}
+	}
+
+	// Now use a similar algorithm to WriteAt but with ReadAt to check random access.
+	read := make(map[int]bool)
+	buf := make([]byte, Max)
+	for len(read) != Max {
+		// Pick a random offset and length.
+		offset := rand.Intn(Max)
+		// Don't bother starting at a known location - speeds up the coverage.
+		for read[offset] {
+			offset = rand.Intn(Max)
+		}
+		length := rand.Intn(Max / 100)
+		if offset+length > Max {
+			length = Max - offset
+		}
+		n, err := f.ReadAt(buf[offset:offset+length], int64(offset))
+		if err != nil {
+			t.Fatalf("ReadAt(offset %d length %d): %v", offset, length, err)
+		}
+		if n != length {
+			t.Fatalf("ReadAt length failed: offset %d expected %d got %d", offset, length, n)
+		}
+		for i := offset; i < offset+length; i++ {
+			if buf[i] != data[i] {
+				t.Fatalf("ReadAt at %d (%#x): expected %#.2x got %#.2x", i, i, data[i], buf[i])
+			}
+		}
+		for i := offset; i < offset+length; i++ {
+			read[i] = true
+		}
 	}
 }
