183f4560fa744b39322d14fcd8a23f5d5991c6c8
Eduardo Pinheiro <edpin@google.com>
2016-01-28 08:54:45 -0800

    store/cmd/cli: command line interface for talking to a store server on the localhost
    - Very rudimentary, but useful for testing a local instance.
    - Also useful in figuring out permission issues (try running an instance
      on your machine and talking to it using this cli).
    
    Change-Id: Ia80decd4e3a86c1aae759d6f7d616eae66452477

---
+++ store/cmd/cli/client.go
@@ -0,0 +1,129 @@
+package main
+
+// Simple client for using the Store interface on the command line.
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	store "upspin.googlesource.com/upspin.git/store/gcp"
+	"upspin.googlesource.com/upspin.git/upspin"
+)
+
+var (
+	Store upspin.Store = store.New("http://localhost:8080", &http.Client{})
+
+	inFile  = flag.String("in", "", "input file")
+	outFile = flag.String("out", "", "output file")
+)
+
+func main() {
+	flag.Usage = Usage
+	flag.Parse()
+
+	if len(flag.Args()) != 2 {
+		Usage()
+	}
+
+	switch strings.ToLower(flag.Arg(0)) {
+	case "get":
+		get(flag.Arg(1))
+	case "put":
+		put(flag.Arg(1))
+	default:
+		log.Println("Can't understand command. Use GET or PUT")
+		Usage()
+	}
+}
+
+func Usage() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\tcli [flags] <GET|PUT> <ref>\n")
+	fmt.Fprintf(os.Stderr, "Flags:\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func get(refStr string) {
+	ref := upspin.Reference{
+		Key:     refStr,
+		Packing: upspin.EllipticalEric,
+	}
+	loc := upspin.Location{
+		Reference: ref,
+	}
+
+	buf, _, err := innerGet(loc, 0)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Write to outfile or to stdout if none set
+	var output *os.File
+	if *outFile == "" {
+		output = os.Stdout
+	} else {
+		output, err = os.Create(*outFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer output.Close()
+	}
+	_, err = io.Copy(output, bytes.NewReader(buf))
+	if err != nil {
+		log.Fatal("Copying to output failed: %v", err)
+	}
+}
+
+func innerGet(loc upspin.Location, count int) ([]byte, []upspin.Location, error) {
+	if count > 3 {
+		return nil, nil, errors.New("Too many redirections")
+	}
+	buf, locs, err := Store.Get(loc)
+	if err != nil {
+		log.Fatalf("Error getting from server: %v", err)
+	}
+	if locs != nil {
+		log.Println("We got redirected. Following...")
+		buf, locs, err = innerGet(locs[0], count+1)
+	}
+	return buf, locs, err
+}
+
+func put(refStr string) {
+	var input *os.File
+	var err error
+	if *inFile == "" {
+		input = os.Stdin
+	} else {
+		input, err = os.Open(*inFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer input.Close()
+	}
+
+	data, err := ioutil.ReadAll(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ref := upspin.Reference{
+		Key:     refStr,
+		Packing: upspin.EllipticalEric,
+	}
+
+	loc, err := Store.Put(ref, data)
+	if err != nil {
+		log.Fatalf("Error putting to server: %v", err)
+	}
+	log.Printf("Put file to storage. Location: %v", loc)
+}
