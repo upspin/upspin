a14777f00326bf79bdc6ac0aa993a02e369828a3
Eduardo Pinheiro <edpin@google.com>
2016-01-29 08:57:45 -0800

    directory/gcp: incorporates r's comments and p's CL
    - Tests pass and library builds.
    
    Change-Id: I26d4de6f45bfc4d299628db9aa93ca4143cd75c8
    Reviewed-on: https://upspin-review.git.corp.google.com/1123
    Reviewed-by: Rob Pike <r@google.com>

--- directory/gcp/directory.go
+++ directory/gcp/directory.go
@@ -32,7 +32,7 @@ type HTTPClientInterface interface {
 	Do(req *http.Request) (resp *http.Response, err error)
 }
 
-// Creates a concrete implementation of Directory, pointing to a server at a given URL and port.
+// New returns a concrete implementation of Directory, pointing to a server at a given URL and port.
 func New(serverURL string, client HTTPClientInterface) *Directory {
 	return &Directory{
 		serverURL: serverURL,
@@ -41,21 +41,22 @@ func New(serverURL string, client HTTPClientInterface) *Directory {
 }
 
 func (d *Directory) Lookup(name upspin.PathName) (*upspin.DirEntry, error) {
-	panic("not implemented")
+	return nil, newError("Lookup", name, errors.New("Lookup unimplemented"))
 }
 
 func (d *Directory) Put(name upspin.PathName, data []byte) (upspin.Location, error) {
-	panic("not implemented")
+	var zeroLoc upspin.Location
+	return zeroLoc, newError("Put", name, errors.New("Put unimplemented"))
 }
 
 func (d *Directory) MakeDirectory(dirName upspin.PathName) (upspin.Location, error) {
 	var zeroLoc upspin.Location // The zero location
-	op := "mkdir"
+	op := "MakeDirectory"
 
 	// Prepares a request to put dirName to the server
 	parsed, err := path.Parse(dirName)
 	if err != nil {
-		return zeroLoc, NewError(op, string(dirName), err)
+		return zeroLoc, newError(op, dirName, err)
 	}
 	dirEntry := upspin.DirEntry{
 		Name: dirName,
@@ -69,21 +70,21 @@ func (d *Directory) MakeDirectory(dirName upspin.PathName) (upspin.Location, err
 	}
 	body, err := json.Marshal(dirEntry)
 	if err != nil {
-		return zeroLoc, NewError(op, string(dirName), err)
+		return zeroLoc, newError(op, dirName, err)
 	}
 	data := bytes.NewBuffer(body)
 	req, err := http.NewRequest(netutil.Post, fmt.Sprintf("%s/put", d.serverURL), data)
 	if err != nil {
-		return zeroLoc, NewError(op, string(dirName), err)
+		return zeroLoc, newError(op, dirName, err)
 	}
 	resp, err := d.client.Do(req)
 	if err != nil {
-		return zeroLoc, NewError(op, string(dirName), err)
+		return zeroLoc, newError(op, dirName, err)
 	}
 
 	// Check the response
 	if resp.StatusCode != http.StatusOK {
-		return zeroLoc, NewError(op, string(dirName),
+		return zeroLoc, newError(op, dirName,
 			errors.New(fmt.Sprintf("server error: %v", resp.StatusCode)))
 	}
 
@@ -92,24 +93,32 @@ func (d *Directory) MakeDirectory(dirName upspin.PathName) (upspin.Location, err
 	// TODO(edpin): maybe add a limit here to the size of bytes we return?
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return zeroLoc, NewError(op, string(dirName), err)
+		return zeroLoc, newError(op, dirName, err)
 	}
 	loc, err := parser.LocationResponse(respBody)
 	if err != nil {
-		return zeroLoc, NewError(op, string(dirName), err)
+		return zeroLoc, newError(op, dirName, err)
 	}
 	if loc == nil {
-		return zeroLoc, NewError(op, string(dirName), errors.New("server returned null Location"))
+		return zeroLoc, newError(op, dirName, errors.New("server returned null Location"))
 	}
 
 	return *loc, nil
 }
 
 func (d *Directory) Glob(pattern string) ([]*upspin.DirEntry, error) {
-	panic("Not implemented yet")
+	return nil, newError("Glob", "", errors.New("Glob unimplemented"))
 }
 
-// fetchKeysExistingKeys fetches wrapped keys for the given directory (possibly cached).
+func (d *Directory) Dial(upspin.ClientContext, upspin.Location) (interface{}, error) {
+	return nil, newError("Dial", "", errors.New("Dial unimplemented"))
+}
+
+func (d *Directory) ServerUserName() string {
+	return "Not sure what to return here yet. Next CL will fix it."
+}
+
+// fetchKeys fetches wrapped keys for the given directory (possibly cached).
 func (d *Directory) fetchKeys(dirName upspin.PathName) []upspin.WrappedKey {
 	// TODO: implement
 	return []upspin.WrappedKey{upspin.WrappedKey{
@@ -123,10 +132,10 @@ func (d *Directory) signDirectoryEntry(dirName upspin.PathName) []byte {
 	return make([]byte, 32) // TODO: implement
 }
 
-func NewError(op string, path string, err error) *os.PathError {
+func newError(op string, path upspin.PathName, err error) *os.PathError {
 	return &os.PathError{
 		Op:   op,
-		Path: path,
+		Path: string(path),
 		Err:  err,
 	}
 }
