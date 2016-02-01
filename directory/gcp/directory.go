d7990696be624f0bbc93b487fa6bb62035679673
Eduardo Pinheiro <edpin@google.com>
2016-02-01 12:58:15 -0800

    directory/gcp: implementation of Directory.Put
    Change-Id: Ia0e5c248ffffdeafac6846eb68520c5019e80814

--- directory/gcp/directory.go
+++ directory/gcp/directory.go
@@ -3,6 +3,7 @@ package directory
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -19,8 +20,9 @@ import (
 
 // Directory is an implementation of upspin.Directory that uses GCP to store its data.
 type Directory struct {
-	serverURL string
-	client    HTTPClientInterface
+	serverURL    string
+	storeService upspin.Store
+	client       HTTPClientInterface
 }
 
 // Guarantee we implement the interface
@@ -34,10 +36,11 @@ type HTTPClientInterface interface {
 }
 
 // New returns a concrete implementation of Directory, pointing to a server at a given URL and port.
-func New(serverURL string, client HTTPClientInterface) *Directory {
+func New(serverURL string, storeService upspin.Store, client HTTPClientInterface) *Directory {
 	return &Directory{
-		serverURL: serverURL,
-		client:    client,
+		serverURL:    serverURL,
+		storeService: storeService,
+		client:       client,
 	}
 }
 
@@ -80,7 +83,74 @@ func (d *Directory) Lookup(name upspin.PathName) (*upspin.DirEntry, error) {
 
 func (d *Directory) Put(name upspin.PathName, data []byte) (upspin.Location, error) {
 	var zeroLoc upspin.Location
-	return zeroLoc, newError("Put", name, errors.New("Put unimplemented"))
+	const op = "Put"
+
+	parsed, err := path.Parse(name)
+	if err != nil {
+		return zeroLoc, newError(op, name, err)
+	}
+
+	// First, stores the data itself, to find a location.
+	ref := upspin.Reference{
+		Key:     "", // The server will compute one. But we should request exactly what we expect when we have crypto here
+		Packing: upspin.EllipticalEric,
+	}
+	loc, err := d.storeService.Put(ref, data)
+	if err != nil {
+		return zeroLoc, err
+	}
+	// We now have a final Location in loc. We now create a
+	// directory entry for this Location.  From here on, if an
+	// error occurs, we'll have a dangling block. We could delete
+	// it, but we can always do fsck-style operations to find them
+	// later.
+	dirEntry := upspin.DirEntry{
+		Name: name,
+		Metadata: upspin.Metadata{
+			IsDir:       false,
+			Sequence:    0, // TODO: server does not increment currently
+			Signature:   signBlob(makeSignableBlob(data, name), d.getUserPrivateKey()),
+			WrappedKeys: d.fetchKeys(parsed.Drop(1).Path()), // Inherited from the parent dir
+		},
+	}
+
+	// Encode dirEntry as JSON
+	dirEntryJSON, err := json.Marshal(dirEntry)
+	if err != nil {
+		return zeroLoc, newError(op, name, err)
+	}
+
+	// Prepare a put request to the server
+	req, err := http.NewRequest(netutil.Get, fmt.Sprintf("%s/put", d.serverURL), bytes.NewBuffer(dirEntryJSON))
+	if err != nil {
+		return zeroLoc, newError(op, name, err)
+	}
+	resp, err := d.client.Do(req)
+	if err != nil {
+		return zeroLoc, newError(op, name, err)
+	}
+
+	// Get response.
+	if resp.StatusCode != http.StatusOK {
+		return zeroLoc, newError(op, name, errors.New(fmt.Sprintf("server error: %v", resp.StatusCode)))
+	}
+
+	// Read the body of the response
+	defer resp.Body.Close()
+	// TODO(edpin): maybe add a limit here to the size of bytes we return?
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return zeroLoc, newError(op, name, err)
+	}
+	err = parser.ErrorResponse(respBody)
+	if err == nil {
+		return zeroLoc, newError(op, name, errors.New("server returned null result"))
+	}
+	if err.Error() != "Success" {
+		return zeroLoc, newError(op, name, err)
+	}
+
+	return loc, nil
 }
 
 func (d *Directory) MakeDirectory(dirName upspin.PathName) (upspin.Location, error) {
@@ -160,9 +230,33 @@ func (d *Directory) fetchKeys(dirName upspin.PathName) []upspin.WrappedKey {
 	}}
 }
 
+// getUserPrivateKey fetches the user's private keys for signing.
+func (d *Directory) getUserPrivateKey() []byte {
+	// TODO: implement
+	return []byte("private keys")
+}
+
 // signDirectoryEntry uses this client's private key to sign the directory entry.
 func (d *Directory) signDirectoryEntry(dirName upspin.PathName) []byte {
 	return make([]byte, 64) // TODO: implement
+}
+
+// TODO: this can take other params and use some other encoding method. This is just a sample code, mostly copied from teststore.MakeBlob.
+// makeSignableBlob concatenates the file contents with a full file name for the user to sign.
+func makeSignableBlob(fileContents []byte, fileName upspin.PathName) []byte {
+	fileNameStr := string(fileName)
+	message := make([]byte, 4+len(fileNameStr)+len(fileContents)) // 4 bytes is excessive worst case for path length.
+	n := binary.PutUvarint(message, uint64(len(fileNameStr)))
+	copy(message[n:], fileNameStr)
+	copy(message[n+len(fileNameStr):], fileContents)
+	message = message[:n+len(fileNameStr)+len(fileContents)]
+	return message
+}
+
+// signBlob uses the user's privateKey to sign a blob of data.
+func signBlob(data []byte, privateKey []byte) []byte {
+	// TODO: implement
+	return []byte("signed")
 }
 
 func newError(op string, path upspin.PathName, err error) *os.PathError {
