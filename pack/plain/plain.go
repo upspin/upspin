1524021351db9cfe0d6aac29f29c4568674e4340
Rob Pike <r@google.com>
2016-02-01 15:50:50 -0800

    pack: add a registry for packing algorithms and a trivial no-op Packing
    Proposal for the design.
    
    We may decide to allocate buffers for the returned data from Pack and Unpack
    but this proposal requires the caller to provide the buffer.
    
    I worry about what to do with an unrecognized Packing. We may end up
    providing some sort of default set of Packings automatically installed.
    
    Code compiles but is otherwise untested.
    
    Change-Id: I344355e107e86eee501ed841fd9caac2052c1cf8

---
+++ pack/plain/plain.go
@@ -0,0 +1,50 @@
+// Package plain is the no-op Packing that passes the data untouched.
+// The path name is not stored in the packed data.
+package plain
+
+import (
+	"errors"
+
+	"upspin.googlesource.com/upspin.git/pack"
+	"upspin.googlesource.com/upspin.git/upspin"
+)
+
+type plainPack struct{}
+
+var _ upspin.Packer = plainPack{}
+
+func init() {
+	pack.Register(plainPack{})
+}
+
+var errTooShort = errors.New("PlainPack: destination slice too short")
+
+func (p plainPack) Packing() upspin.Packing {
+	return upspin.PlainPack
+}
+
+func (p plainPack) Pack(cipher, clear, packdata []byte, name upspin.PathName) (int, error) {
+	if len(cipher) < len(clear) {
+		return 0, errTooShort
+	}
+	return copy(cipher, clear), nil
+}
+
+func (p plainPack) Unpack(clear, cipher, packdata []byte) (upspin.PathName, int, error) {
+	if len(clear) < len(cipher) {
+		return "", 0, errTooShort
+	}
+	return "", copy(clear, cipher), nil
+}
+
+func (p plainPack) PackLen(clear []byte, name upspin.PathName) int {
+	return len(clear)
+}
+
+func (p plainPack) UnpackLen(cipher []byte) int {
+	return len(cipher)
+}
+
+func (p plainPack) String() string {
+	return "plain"
+}
