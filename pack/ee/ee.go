8db78ce68e1f08aa19e91d0899ac084e895a3248
[Eric Grosse] <[ehg@google.com]>
2016-02-10 10:00:02 -0800

    ee/ee.go: end-to-end crypto in Packers interface
    Still not really ready for review, but if anyone wants to
    send mid-course corrections on my novice code, I will happily
    take them.
    
    Change-Id: I4264a6c100ec9585724375d04e12d448ed881608

---
+++ pack/ee/ee.go
@@ -0,0 +1,199 @@
+// Package ee implements elliptic-curve end-to-end-encrypted packers.
+// The first version, EEp256Pack, uses curve P256, SHA256, and AES-CTR 128;
+// future versions, sharing code, will implement P386 and AES 256.
+// Alice shares a file with Bob by picking a new random symmetric key, encrypting the file,
+// wrapping the symmetric encryption key with Bob's public key, signing the file using
+// her own elliptic curve private key, and sending the ciphertext and metadata to a
+// directory server.
+package ee
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
+	"fmt"
+
+	"upspin.googlesource.com/upspin.git/pack"
+	"upspin.googlesource.com/upspin.git/path"
+	"upspin.googlesource.com/upspin.git/upspin"
+)
+
+type eePack struct {
+	factotum  string // where to do private key operations; maybe a chan?
+	keyserver string // where to lookup public keys;  maybe a chan?
+}
+
+var _ upspin.Packer = eePack{}
+
+func init() {
+	pack.Register(eePack{"factotum", "keyserver"})
+}
+
+var errTooShort = errors.New("EEPack: destination slice too short")
+
+const (
+	wrapPrefix = "wrap:" // for debugging draft;  should never appear in production
+	aesKeylen  = 16
+)
+
+// Signature should be ECDSA but use sha256 as a placeholder for now   TODO
+type Signature [sha256.Size]byte
+
+// WrappedKey holds a key that will decrypt file contents. The key is itself
+// encrypted with user's private key.  A 16-bit hash of the user's public
+// key is stored alongside to make it easier to find which key to use if many are
+// present.
+type WrappedKey struct {
+	Hash      [2]byte // 16-bit hash of public key for user.
+	Encrypted []byte  // Data decryption key, itself encrypted with public key for user.
+}
+type WrappedKeys []WrappedKey
+
+func (eePack) Packing() upspin.Packing {
+	return upspin.EEp256Pack
+}
+
+func (e eePack) Pack(ciphertext, cleartext []byte, meta *upspin.Metadata, name upspin.PathName) (int, error) {
+	if len(ciphertext) < len(cleartext) {
+		return 0, errTooShort
+	}
+	dkey := make([]byte, aesKeylen)
+	_, err := rand.Read(dkey)
+	if err != nil {
+		return 0, err
+	}
+	n, err := encrypt(ciphertext, cleartext, dkey)
+	if err != nil {
+		return 0, err
+	}
+
+	parsed, err := path.Parse(name) // first wrapped key is for owner = parsed.User
+	if err != nil {
+		return 0, fmt.Errorf("EEPack: filename does not parse: %s", parsed.User)
+	}
+	usernames := []string{string(parsed.User)} // TODO should be readers of directory
+
+	mess := []byte(fmt.Sprintf("%2x:%s:%x:%q", e.Packing(), name, dkey, ciphertext))
+	sig := Signature(sha256.Sum256(mess)) // TODO should be ECDSA using factotum
+	wrap := make([]WrappedKey, len(usernames))
+	for i, user := range usernames {
+		hash16 := cksum(user)                                // TODO call keyserver
+		key := []byte(wrapPrefix + hex.EncodeToString(dkey)) // TODO rfc6637 §8 using factotum
+		wrap[i] = WrappedKey{hash16, key}
+	}
+
+	meta.PackData = pdMarshal(sig, wrap)
+	return n, nil
+}
+
+func (e eePack) Unpack(cleartext, ciphertext []byte, meta *upspin.Metadata, name upspin.PathName) (int, error) {
+	if len(cleartext) < len(ciphertext) {
+		return 0, errTooShort
+	}
+
+	var mismatch error
+	dkey := make([]byte, aesKeylen)
+	sig, wrap := pdUnmarshal(meta.PackData, name) // TODO update pdUnmarshal
+	parsed, err := path.Parse(name)               // first wrapped key is for owner = parsed.User
+	if err != nil {
+		return 0, fmt.Errorf("EEPack: filename does not parse: %s", parsed.User)
+	}
+	mykey := e.keyserver + "/" + string(parsed.User) // TODO replace by keyserver.Lookup
+	hash16 := cksum(mykey)
+	for _, w := range wrap {
+		if hash16 != w.Hash {
+			continue
+		}
+		if string(w.Encrypted[0:len(wrapPrefix)]) != wrapPrefix {
+			return 0, fmt.Errorf("malformed wrapped key: %s", w.Encrypted)
+		}
+		// TODO   why encoding/hex instead of %X with Print and Scan?
+		n, err := hex.Decode(dkey, w.Encrypted[len(wrapPrefix):]) // TODO  call factotum
+		if err != nil || n != aesKeylen {
+			return 0, fmt.Errorf("malformed wrapped hex: %s", w.Encrypted)
+		}
+		mess := []byte(fmt.Sprintf("%2x:%s:%x:%x", e.Packing(), name, dkey, ciphertext))
+		sum := sha256.Sum256(mess)
+		if sum != sig {
+			// TODO verify ECDSA; just use hash for now as sanity check
+			mismatch = fmt.Errorf("retrieval %x does not verify: %s != %s", hash16, sum, sig)
+			continue // maybe one of the other keys will work
+		}
+		return decrypt(cleartext, ciphertext, dkey)
+	}
+	if mismatch != nil {
+		return 0, mismatch
+	}
+	return 0, fmt.Errorf("no wrapped key for me")
+}
+
+func (eePack) PackLen(cleartext []byte, meta *upspin.Metadata, name upspin.PathName) int {
+	return len(cleartext)
+}
+
+func (eePack) UnpackLen(ciphertext []byte, meta *upspin.Metadata) int {
+	return len(ciphertext)
+}
+
+func (eePack) String() string {
+	return "ee" // TODO what is supposed to go here?
+}
+
+func pdMarshal(sig Signature, wrap []WrappedKey) []byte {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, sig)
+	if err != nil {
+		panic("binary.Write sig failed")
+	}
+	err = binary.Write(buf, binary.BigEndian, wrap)
+	if err != nil {
+		panic("binary.Write wrap failed")
+	}
+	return buf.Bytes()
+}
+
+func pdUnmarshal(pd []byte, name upspin.PathName) (sig Signature, wrap []WrappedKey) {
+	panic("unimplemented") // TODO
+}
+
+func encrypt(ciphertext, cleartext, dkey []byte) (int, error) {
+	if len(dkey) != aesKeylen {
+		panic("unimplemented") // should recognize 32 as AES 256
+	}
+	block, err := aes.NewCipher(dkey)
+	if err != nil {
+		panic(err)
+	}
+	iv := make([]byte, aes.BlockSize)
+	// iv=0 is ok because we're CERTAIN that dkey is random and not reused
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(ciphertext, cleartext)
+	return len(cleartext), nil
+}
+
+func decrypt(cleartext, ciphertext, dkey []byte) (int, error) {
+	block, err := aes.NewCipher(dkey)
+	if err != nil {
+		panic(err)
+	}
+	iv := make([]byte, aes.BlockSize)
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(cleartext, ciphertext)
+	return len(ciphertext), nil
+}
+
+func cksum(s string) (hash16 [2]byte) {
+	// TODO is there already something lying around we can use?
+	var h uint16
+	for _, c := range s {
+		h += uint16(c)
+	}
+	hash16[0] = byte(h >> 8)
+	hash16[1] = byte(h & 0xff)
+	return hash16
+}
