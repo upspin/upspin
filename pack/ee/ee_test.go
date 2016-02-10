c7361310fe89eac76cc9d6e4c9b4cb9427dd94ee
[Eric Grosse] <[ehg@google.com]>
2016-02-10 10:00:02 -0800

    ee/ee.go: crypto Packers
    Fix camelCase and other issues from Rob's review.  Still a few
    TODO to give more precise sizes, but fine to use as is.
    
    Change-Id: I4264a6c100ec9585724375d04e12d448ed881608
    Reviewed-on: https://upspin-review.git.corp.google.com/1210
    Reviewed-by: Rob Pike <r@google.com>

---
+++ pack/ee/ee_test.go
@@ -0,0 +1,48 @@
+package ee
+
+import (
+	"testing"
+
+	"upspin.googlesource.com/upspin.git/pack"
+	"upspin.googlesource.com/upspin.git/upspin"
+)
+
+func TestRegister(t *testing.T) {
+	p := pack.Lookup(upspin.EEp256Pack)
+	if p == nil {
+		t.Fatal("Lookup failed")
+	}
+	if p.Packing() != upspin.EEp256Pack {
+		t.Fatalf("expected EEp256Pack, got %q", p)
+	}
+}
+
+func TestPack(t *testing.T) {
+	const (
+		name upspin.PathName = "user@google.com/file/of/user"
+		text                 = "this is some text"
+	)
+	packer := pack.Lookup(upspin.EEp256Pack)
+
+	// First pack.
+	data := []byte(text)
+	meta := upspin.Metadata{}
+	cipher := make([]byte, packer.PackLen(nil, data, &meta, name))
+	m, err := packer.Pack(nil, cipher, data, &meta, name)
+	if err != nil {
+		t.Fatal("Pack: ", err)
+	}
+	cipher = cipher[:m]
+
+	// Now unpack.
+	clear := make([]byte, packer.UnpackLen(nil, cipher, &meta))
+	m, err = packer.Unpack(nil, clear, cipher, &meta, name)
+	if err != nil {
+		t.Fatal("Unpack: ", err)
+	}
+	clear = clear[:m]
+	str := string(clear[:m])
+	if str != text {
+		t.Errorf("text: expected %q; got %q", text, str)
+	}
+}
---
+++ pack/keygen/keygen.go
@@ -0,0 +1,63 @@
+// Keygen creates local files secret.P-256 and public.P-256
+// which contain the private and public parts of a keypair.
+// Eventually this will be provided by ssh-agent or e2email
+// or something else, but we need a minimally usable and
+// safe tool for initial testing.
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("keygen: ")
+	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		log.Fatalf("key not generated: %s\n", err)
+	}
+
+	private, err := os.Create(filepath.Join(sshdir(), "secret.upspinkey"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = private.Chmod(0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = private.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	public, err := os.Create(filepath.Join(sshdir(), "public.upspinkey"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = private.WriteString(priv.D.String() + "\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = public.WriteString(priv.X.String() + "\n" + priv.Y.String() + "\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = public.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func sshdir() string {
+	user, err := user.Current()
+	if err != nil {
+		log.Fatal("no user")
+	}
+	return filepath.Join(user.HomeDir, ".ssh")
+}
