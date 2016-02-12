b294f0b012993efbfe1f5b490f99ddedacc64362
Eduardo Pinheiro <edpin@google.com>
2016-02-11 17:36:37 -0800

    parser: improved parsers and tests
    Change-Id: Ifd6b5aa84c6b84a4017b399673f12d35189b0c6a
    Reviewed-on: https://upspin-review.git.corp.google.com/1203
    Reviewed-by: Rob Pike <r@google.com>

---
+++ cloud/netutil/parser/parser_test.go
@@ -0,0 +1,195 @@
+package parser
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+
+	"upspin.googlesource.com/upspin.git/upspin"
+)
+
+const (
+	serverParsingErrorPrefix = "can't parse reply from server:"
+)
+
+var (
+	location = upspin.Location{
+		Endpoint: upspin.Endpoint{
+			Transport: upspin.GCP,
+			NetAddr:   "some server",
+		},
+		Reference: upspin.Reference{
+			Key: "abcd",
+		},
+	}
+	dirEntry = upspin.DirEntry{
+		Name:     "foo",
+		Location: location,
+	}
+)
+
+func TestLocationResponse(t *testing.T) {
+	locJSON, err := json.Marshal(location)
+	if err != nil {
+		t.Fatal(err)
+	}
+	loc, err := LocationResponse(locJSON)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	if loc == nil {
+		t.Fatal("Expected a valid location, got nil")
+	}
+	if loc.Reference.Key != location.Reference.Key {
+		t.Errorf("Expected key %v, got %v", location.Reference.Key, loc.Reference.Key)
+	}
+}
+
+func TestLocationResponseBadError(t *testing.T) {
+	loc, err := LocationResponse([]byte(`{"endpoint:"foo", "bla bla bla"}`))
+	if err == nil {
+		t.Fatal("Expected error, got nil")
+	}
+	if loc != nil {
+		t.Fatalf("Expected a zero Location, got %v", *loc)
+	}
+	if !strings.HasPrefix(err.Error(), serverParsingErrorPrefix) {
+		t.Fatalf("Expected error prefix %q, got %q", serverParsingErrorPrefix, err)
+	}
+}
+
+func TestLocationResponseEmptyError(t *testing.T) {
+	loc, err := LocationResponse([]byte(""))
+	if err == nil {
+		t.Fatal("Expected error, got nil")
+	}
+	if loc != nil {
+		t.Fatalf("Expected a zero Location, got %v", *loc)
+	}
+	expectedError := "empty server response"
+	if err.Error() != expectedError {
+		t.Fatalf("Expected error %q, got %q", expectedError, err)
+	}
+}
+
+func TestLocationResponseWithProperError(t *testing.T) {
+	loc, err := LocationResponse([]byte(`{"error":"something bad happened"}`))
+	if err == nil {
+		t.Fatalf("Expected error, got nil: %v", loc)
+	}
+	if loc != nil {
+		t.Fatalf("Expected a zero Location, got %v", *loc)
+	}
+	expectedError := "something bad happened"
+	if err.Error() != expectedError {
+		t.Fatalf("Expected error %q, got %q", expectedError, err)
+	}
+}
+
+func TestKeyResponse(t *testing.T) {
+	key, err := KeyResponse([]byte(`{"key": "1234"}`))
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	if key != "1234" {
+		t.Errorf("Expected key 1234, got %v", key)
+	}
+}
+
+func TestKeyResponseBadError(t *testing.T) {
+	key, err := KeyResponse([]byte("bla bla bla"))
+	if err == nil {
+		t.Fatal("Expected error, got nil")
+	}
+	if key != "" {
+		t.Fatalf("Expected a nil key, got %v", key)
+	}
+	if !strings.HasPrefix(err.Error(), serverParsingErrorPrefix) {
+		t.Fatalf("Expected error prefix %q, got %q", serverParsingErrorPrefix, err)
+	}
+}
+
+func TestDirEntryResponse(t *testing.T) {
+	dirJSON, err := json.Marshal(dirEntry)
+	if err != nil {
+		t.Fatal(err)
+	}
+	dir, err := DirEntryResponse(dirJSON)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	if dir == nil {
+		t.Fatal("Expected a valid dirEntry, got nil")
+	}
+	if dir.Location.Reference.Key != dirEntry.Location.Reference.Key {
+		t.Errorf("Expected key %v, got %v", dirEntry.Location.Reference.Key, dir.Location.Reference.Key)
+	}
+}
+
+func TestDirEntryResponseBadError(t *testing.T) {
+	dir, err := DirEntryResponse([]byte(`{"Name":"path","Location":"loc"}`))
+	if err == nil {
+		t.Fatal("Expected error, got nil")
+	}
+	if dir != nil {
+		t.Fatalf("Expected a nil DirEntry, got %v", *dir)
+	}
+	if !strings.HasPrefix(err.Error(), serverParsingErrorPrefix) {
+		t.Fatalf("Expected error prefix %q, got %q", serverParsingErrorPrefix, err)
+	}
+}
+
+func TestDirEntryResponseNilDirEntry(t *testing.T) {
+	dir, err := DirEntryResponse(nil)
+	if err == nil {
+		t.Fatal("Expected error, got none")
+	}
+	if dir != nil {
+		t.Errorf("Expected nil dir, got %v", *dir)
+	}
+	expectedError := "empty server response"
+	if err.Error() != expectedError {
+		t.Fatalf("Expected error %q, got %q", expectedError, err)
+	}
+}
+
+func TestDirEntryResponseEmptyDirEntry(t *testing.T) {
+	dir, err := DirEntryResponse([]byte(""))
+	if err == nil {
+		t.Fatal("Expected error, got none")
+	}
+	if dir != nil {
+		t.Errorf("Expected nil dir, got %v", *dir)
+	}
+	expectedError := "empty server response"
+	if err.Error() != expectedError {
+		t.Fatalf("Expected error %q, got %q", expectedError, err)
+	}
+}
+
+func TestDirEntryResponseInvalidDirEntry(t *testing.T) {
+	dir, err := DirEntryResponse([]byte("bla"))
+	if err == nil {
+		t.Fatal("Expected error, got none")
+	}
+	if dir != nil {
+		t.Errorf("Expected nil dir, got %v", *dir)
+	}
+	if !strings.HasPrefix(err.Error(), serverParsingErrorPrefix) {
+		t.Fatalf("Expected error prefix %q, got %q", serverParsingErrorPrefix, err)
+	}
+}
+
+func TestDirEntryResponseWithProperError(t *testing.T) {
+	dir, err := DirEntryResponse([]byte(`{"error":"something terrible happened"}`))
+	if err == nil {
+		t.Fatal("Expected error, got nil")
+	}
+	if dir != nil {
+		t.Fatalf("Expected a nil DirEntry, got %v", *dir)
+	}
+	expectedError := "something terrible happened"
+	if err.Error() != expectedError {
+		t.Fatalf("Expected error %q, got %q", expectedError, err)
+	}
+}
