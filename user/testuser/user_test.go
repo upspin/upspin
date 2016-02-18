9444b5cbe679b2d6551bc61cf8ef1f5d727672b3
Eduardo Pinheiro <edpin@google.com>
2016-02-18 14:49:34 -0800

    pack/unsafe: a simple packer that updates metadata and a GCP client.
    - Added a packer that does signature validation and obfuscation, so we
        can test packdata end-to-end.
    - Added a GCP client that uses a testuser with some mock public keys
     for encrypting files.
    - Created a simple CLI that uses the Client with testuser and the GCP
      instances of Store and Directory.
    - All tests pass.
    - Some hacks remain.
    
    Change-Id: I681e15dabe307be21fdf9fb80e4113244c3c5f35
    Reviewed-on: https://upspin-review.git.corp.google.com/1280
    Reviewed-by: Rob Pike <r@google.com>

---
+++ user/testuser/user_test.go
@@ -0,0 +1,102 @@
+package testuser
+
+import (
+	"testing"
+
+	"upspin.googlesource.com/upspin.git/access"
+	"upspin.googlesource.com/upspin.git/upspin"
+
+	_ "upspin.googlesource.com/upspin.git/directory/testdir"
+	_ "upspin.googlesource.com/upspin.git/store/teststore"
+)
+
+var (
+	userName = upspin.UserName("joe@blow.com")
+)
+
+func setup(t *testing.T) (upspin.User, *upspin.Context) {
+	c := &upspin.Context{}
+	e := upspin.Endpoint{
+		Transport: upspin.InProcess,
+		NetAddr:   "", // ignored
+	}
+	u, err := access.BindUser(c, e)
+	if err != nil {
+		t.Fatal(err)
+	}
+	c.Store, err = access.BindStore(c, e)
+	if err != nil {
+		t.Fatal(err)
+	}
+	c.Directory, err = access.BindDirectory(c, e)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return u, c
+}
+
+func TestInstallAndLookup(t *testing.T) {
+	u, ctxt := setup(t)
+	testUser, ok := u.(*Service)
+	if !ok {
+		t.Fatal("Not a testuser Service")
+	}
+
+	err := testUser.Install(userName, ctxt.Directory)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	eRecv, keys, err := u.Lookup(userName)
+	if err != nil {
+		t.Fatal("Expected no error, got %v", err)
+	}
+	if len(keys) != 0 {
+		t.Errorf("Expected no keys for user %v, got %d", userName, len(keys))
+	}
+	if len(eRecv) != 1 {
+		t.Fatalf("Expected 1 endpoint, got %d", len(eRecv))
+	}
+	if eRecv[0].Transport != upspin.InProcess {
+		t.Error("Expected endpoint to be %d, but instead it was %d", upspin.InProcess, eRecv[0].Transport)
+	}
+}
+
+func TestPublicKeysAndUsers(t *testing.T) {
+	u, _ := setup(t)
+	testUser, ok := u.(*Service)
+	if !ok {
+		t.Fatal("Not a testuser Service")
+	}
+	const testKey = "pub key1"
+	testUser.SetPublicKeys(userName, []upspin.PublicKey{
+		upspin.PublicKey(testKey),
+	})
+
+	_, keys, err := u.Lookup(userName)
+	if err != nil {
+		t.Fatal("Expected no error, got %v", err)
+	}
+	if len(keys) != 1 {
+		t.Fatalf("Expected 1 key for user %v, got %d", userName, len(keys))
+	}
+	if string(keys[0]) != testKey {
+		t.Errorf("Expected key %s, got %s", testKey, keys[0])
+	}
+
+	users := testUser.ListUsers()
+	if len(users) != 1 {
+		t.Fatalf("Expected 1 user, got %d", len(users))
+	}
+	if users[0] != userName {
+		t.Errorf("Expected user %s, got %v", userName, users[0])
+	}
+
+	// Delete keys for user
+	testUser.SetPublicKeys(userName, nil)
+
+	users = testUser.ListUsers()
+	if len(users) != 0 {
+		t.Fatalf("Expected 0 users, got %d", len(users))
+	}
+}
