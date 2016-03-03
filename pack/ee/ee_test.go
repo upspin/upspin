6c47ef2643dee8b5c8afbb3502463c86a50dcd13
Eduardo Pinheiro <edpin@google.com>
2016-03-03 14:50:44 -0800

    eepack: preparing for using the keyserver
    Eric: I wanted to do this in stages to prevent royal screw-ups. This has
    no functional changes, but does the following:
    
    - Separates reading the .ssh files from obtaining the keys (so real soon
      this can be a read from the network)
    - Uses the Context for finding the recipient and the recipient's private
      key.
    - Converts our upspin.PrivateKey and upspin.PublicKey types to the
      internal ecsda representation only where needed (the keyserver will
      return our upspin type, so we must be capable of converting)
    - IMPORTANT: made one small but important change: when we verify the
      signature, it's the file's owner's pubkey that we must use, not our
      own. Currently, these are the same, since we can't share yet, but
      please check on line 266 that this is correct.
    
    Bonus:
    - Made the code not be prone to race conditions by getting rid of the
      global variables. :-)
    
    Change-Id: Ie15ba72076570974576c1fe83801000c6046cf31

--- pack/ee/ee_test.go
+++ pack/ee/ee_test.go
@@ -1,6 +1,9 @@
 package ee
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"upspin.googlesource.com/upspin.git/pack"
@@ -17,20 +20,20 @@ func TestRegister(t *testing.T) {
 	}
 }
 
-func testPackAndUnpack(t *testing.T, packer upspin.Packer, name upspin.PathName, text []byte) {
+func testPackAndUnpack(t *testing.T, ctx *upspin.Context, packer upspin.Packer, name upspin.PathName, text []byte) {
 	// First pack.
 	data := []byte(text)
 	meta := upspin.Metadata{}
-	cipher := make([]byte, packer.PackLen(nil, data, &meta, name))
-	m, err := packer.Pack(nil, cipher, data, &meta, name)
+	cipher := make([]byte, packer.PackLen(ctx, data, &meta, name))
+	m, err := packer.Pack(ctx, cipher, data, &meta, name)
 	if err != nil {
 		t.Fatal("Pack: ", err)
 	}
 	cipher = cipher[:m]
 
 	// Now unpack.
-	clear := make([]byte, packer.UnpackLen(nil, cipher, &meta))
-	m, err = packer.Unpack(nil, clear, cipher, &meta, name)
+	clear := make([]byte, packer.UnpackLen(ctx, cipher, &meta))
+	m, err = packer.Unpack(ctx, clear, cipher, &meta, name)
 	if err != nil {
 		t.Fatal("Unpack: ", err)
 	}
@@ -43,18 +46,51 @@ func testPackAndUnpack(t *testing.T, packer upspin.Packer, name upspin.PathName,
 
 func TestPack256(t *testing.T) {
 	const (
-		name upspin.PathName = "user@google.com/file/of/user.256"
-		text                 = "this is some text 256"
+		user    upspin.UserName = "user@google.com"
+		name                    = upspin.PathName(user + "/file/of/user.256")
+		text                    = "this is some text 256"
+		packing                 = upspin.EEp256Pack
 	)
-	packer := pack.Lookup(upspin.EEp256Pack)
-	testPackAndUnpack(t, packer, name, []byte(text))
+	ctx, packer := setup(t, user, packing)
+	testPackAndUnpack(t, ctx, packer, name, []byte(text))
 }
 
 func TestPack521(t *testing.T) {
 	const (
-		name upspin.PathName = "user@google.com/file/of/user.521"
-		text                 = "this is some text 521"
+		user    upspin.UserName = "user@google.com"
+		name                    = upspin.PathName(user + "/file/of/user.521")
+		text                    = "this is some text 521"
+		packing                 = upspin.EEp521Pack
 	)
-	packer := pack.Lookup(upspin.EEp521Pack)
-	testPackAndUnpack(t, packer, name, []byte(text))
+	ctx, packer := setup(t, user, packing)
+	testPackAndUnpack(t, ctx, packer, name, []byte(text))
+}
+
+func setup(t *testing.T, name upspin.UserName, packing upspin.Packing) (*upspin.Context, upspin.Packer) {
+	ctx := &upspin.Context{
+		UserName: name,
+		Packing:  packing,
+	}
+	packer := pack.Lookup(packing)
+	ctx.PrivateKey = privateKey(t, name, packing)
+	return ctx, packer
+}
+
+// privateKey returns the private key of the current user by reading file from ~/.ssh/.
+func privateKey(t *testing.T, user upspin.UserName, packing upspin.Packing) upspin.PrivateKey {
+	f, err := os.Open(filepath.Join(sshdir(), fmt.Sprintf("secret.%d.upspinkey", packing)))
+	if err != nil {
+		t.Fatal("If you haven't already, run keygen.")
+	}
+	defer f.Close()
+	buf := make([]byte, 200) // big enough for P-521
+	n, err := f.Read(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if buf[n-1] == '\n' {
+		n--
+	}
+	buf = buf[:n]
+	return upspin.PrivateKey(buf)
 }
