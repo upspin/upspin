603edcc12696161b58d762eb0fe0df30df3d0c89
Eduardo Pinheiro <edpin@google.com>
2016-03-04 16:43:05 -0800

    gcpdir: interpret Access as special ACL file.
    Change-Id: I5ce3d3957812bfd0861613a31637f85d47b4116d

--- client/gcpclient/client.go
+++ client/gcpclient/client.go
@@ -4,15 +4,15 @@
 package gcpclient
 
 import (
+	"errors"
 	"fmt"
-	"log"
 
+	"upspin.googlesource.com/upspin.git/access"
 	"upspin.googlesource.com/upspin.git/bind"
 	"upspin.googlesource.com/upspin.git/client/common/file"
 	"upspin.googlesource.com/upspin.git/pack"
 	"upspin.googlesource.com/upspin.git/path"
 	"upspin.googlesource.com/upspin.git/upspin"
-	"upspin.googlesource.com/upspin.git/user/testuser"
 
 	_ "upspin.googlesource.com/upspin.git/directory/gcpdir"
 	_ "upspin.googlesource.com/upspin.git/pack/unsafe"
@@ -44,93 +44,50 @@ func New(context *upspin.Context) upspin.Client {
 	}
 }
 
-// NewForTesting creates a new upspin.Client talking to the GCP
-// backends located at storeURL and dirURL and the User service
-// pre-loaded with the given user keys.
-func NewForTesting(storeURL string, dirURL string, userKeys []UserKeys) upspin.Client {
-	context := &upspin.Context{
-		Packing:  upspin.UnsafePack,
-		UserName: upspin.UserName("edpin@google.com"),
-		KeyPair: upspin.KeyPair{
-			Public:  []byte("Zee Kee"),
-			Private: []byte("Zee Kee"),
-		},
+// updateReadersInMeta traverses the given path (which must not be
+// nil) backwards looking for the closest directory that contains an
+// Access file (present as the DirEntry's Metadata.Reader). When it
+// finds one, it overwrites the metadata (which must not be nil) with
+// it. If not found, meta.Readers will be untouched.
+func (c *Client) updateReadersInMeta(meta *upspin.Metadata, path *path.Parsed) error {
+	for len(path.Elems) > 0 {
+		dirEntry, err := c.context.Directory.Lookup(path.Drop(1).Path())
+		if err != nil {
+			return err
+		}
+		if dirEntry.Metadata.Readers != nil {
+			meta.Readers = dirEntry.Metadata.Readers
+			return nil
+		}
 	}
-
-	context.User = newUser(context)
-	context.Store = newStore(context, storeURL)
-	context.Directory = newDirectory(context, dirURL)
-
-	// TODO: this is a hack.
-	testuser := context.User.(*testuser.Service)
-	for _, uk := range userKeys {
-		testuser.SetPublicKeys(uk.User, []upspin.PublicKey{uk.Public})
-	}
-
-	return New(context)
-}
-
-// newUser creates a new in-process upspin.User client.
-func newUser(context *upspin.Context) upspin.User {
-	if context == nil {
-		return nil
-	}
-	e := upspin.Endpoint{
-		Transport: upspin.InProcess,
-		NetAddr:   "",
-	}
-	u, err := bind.User(context, e)
-	if err != nil {
-		log.Fatalf("Can't bind to User: %v", err)
-	}
-	return u
-}
-
-// newStore creates a new upspin.Store client for talking to a GCP
-// server located at storeURL
-func newStore(context *upspin.Context, storeURL string) upspin.Store {
-	if context == nil {
-		return nil
-	}
-	e := upspin.Endpoint{
-		Transport: upspin.GCP,
-		NetAddr:   upspin.NetAddr(storeURL),
-	}
-	s, err := bind.Store(context, e)
-	if err != nil {
-		log.Fatalf("Can't bind to Store: %v", err)
-	}
-	return s
-}
-
-// newDirectory creates a new upspin.Directory client for talking to a GCP
-// server located at dirURL
-func newDirectory(context *upspin.Context, dirURL string) upspin.Directory {
-	if context == nil {
-		return nil
-	}
-	if context.Store == nil {
-		log.Fatal("Need a Store to initialize a Directory.")
-	}
-	e := upspin.Endpoint{
-		Transport: upspin.GCP,
-		NetAddr:   upspin.NetAddr(dirURL),
-	}
-	d, err := bind.Directory(context, e)
-	if err != nil {
-		log.Fatalf("Can't bind to Directory: %v", err)
-	}
-	return d
+	return nil
 }
 
 func (c *Client) Put(name upspin.PathName, data []byte) (upspin.Location, error) {
+	// Treat pathname ending in "/Access" as special.
+
+	isAccessFile, parsed := access.IsAccessFile(name)
+	if parsed == nil {
+		return zeroLoc, errors.New("error parsing path %s", name)
+	}
+	if isAccessFile {
+		return c.context.Directory.Put(name, data, []byte(""))
+	}
+
 	// Encrypt data according to the preferred packer
 	// TODO: Do a Lookup in the parent directory to find the overriding packer.
 	packer := pack.Lookup(c.context.Packing)
 	if packer == nil {
 		return zeroLoc, fmt.Errorf("unrecognized Packing %d for %q", c.context.Packing, name)
 	}
+
 	meta := &upspin.Metadata{}
+	// Figure out if there are readers for this file.
+	err = c.updateReadersInMeta(meta, parsed)
+	if err != nil {
+		return zeroLoc, err
+	}
+
 	// Get a buffer big enough for this data
 	cipherLen := packer.PackLen(c.context, data, meta, name)
 	if cipherLen < 0 {
@@ -138,8 +95,7 @@ func (c *Client) Put(name upspin.PathName, data []byte) (upspin.Location, error)
 	}
 	// TODO: Some packers don't update the meta in PackLen, but some do. If not done, update it now.
 	if len(meta.PackData) == 0 {
-		meta.PackData = make(upspin.PackData, 1)
-		meta.PackData[0] = byte(c.context.Packing)
+		meta.PackData = []byte{byte(c.context.Packing)}
 	}
 	cipher := make([]byte, cipherLen)
 	n, err := packer.Pack(c.context, cipher, data, meta, name)
@@ -178,7 +134,7 @@ func (c *Client) getRootDir(name upspin.PathName) (upspin.Directory, error) {
 		}
 	}
 	if err == nil {
-		err = fmt.Errorf("gcpclient: no such user %q", parsed.User)
+		err = fmt.Errorf("gcpclient: no endpoint for user %q", parsed.User)
 	}
 	return nil, err
 }
