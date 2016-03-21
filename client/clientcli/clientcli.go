ce3b0115d56d17b23998a090966d89818fdead13
Eduardo Pinheiro <edpin@google.com>
2016-03-21 15:39:43 -0700

    clientcli: implement ls and ls -l
    - Invented my own -l format for now, which shows endpoints and readers.
    - Fixed a bug in Client where it was not forcing packing for Access to
      be Plain.
    - Fixed a sharing problem whereas the Client must also lookup the
      parentdir to find the Readers or the Packing won't know about it.
    - Various lint clean ups in this part of the tree.
    
    Change-Id: I3c971e6105572545f0db25e4cb6099b09b84e784

--- client/clientcli/clientcli.go
+++ client/clientcli/clientcli.go
@@ -1,4 +1,4 @@
-// Simple utility for reading/writing files on GCP using the Unsafe packing.
+// Simple utility for exercising the client against the user's default context.
 package main
 
 import (
@@ -13,6 +13,8 @@ import (
 
 	"upspin.googlesource.com/upspin.git/client"
 	"upspin.googlesource.com/upspin.git/context"
+	"upspin.googlesource.com/upspin.git/endpoint"
+	"upspin.googlesource.com/upspin.git/path"
 	"upspin.googlesource.com/upspin.git/upspin"
 
 	// Load useful packers
@@ -26,17 +28,18 @@ import (
 )
 
 var (
-	inFile  = flag.String("in", "", "full pathname of file to be Put or empty for stdin")
-	outFile = flag.String("out", "", "output file")
-	c       = newClient()
+	inFile     = flag.String("in", "", "full pathname of file to be Put or empty for stdin")
+	outFile    = flag.String("out", "", "output file")
+	longFormat = flag.Bool("l", false, "Enables long format for ls")
+	c          = newClient()
 )
 
 func main() {
-	flag.Usage = Usage
+	flag.Usage = usage
 	flag.Parse()
 
 	if len(flag.Args()) != 2 {
-		Usage()
+		usage()
 	}
 
 	path := upspin.PathName(flag.Arg(1))
@@ -47,13 +50,15 @@ func main() {
 		put(path)
 	case "get":
 		get(path)
-	default: // TODO: ls
+	case "ls":
+		ls(path)
+	default:
 		fmt.Fprintf(os.Stderr, "Can't understand command: %v\n", flag.Arg(0))
-		Usage()
+		usage()
 	}
 }
 
-func Usage() {
+func usage() {
 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
 	fmt.Fprintf(os.Stderr, "\tcli [flags] <mkdir|put|get> <path>\n")
 	fmt.Fprintf(os.Stderr, "Flags:\n")
@@ -112,6 +117,105 @@ func get(pathName upspin.PathName) {
 	_, err = io.Copy(output, bytes.NewReader(data))
 	if err != nil {
 		log.Fatalf("Copying to output failed: %v", err)
+	}
+}
+
+func ls(pathName upspin.PathName) {
+	var de []*upspin.DirEntry
+	var err error
+	n := len(pathName)
+	if pathName[n-1:n] == "/" {
+		de, err = c.Glob(string(pathName) + "*")
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+
+	}
+	if *longFormat {
+		printLongDirEntries(de)
+	} else {
+		printShortDirEntries(de)
+	}
+}
+
+func printShortDirEntries(de []*upspin.DirEntry) {
+	for _, e := range de {
+		fmt.Printf("%s\n", e.Name)
+	}
+}
+
+type domains map[string][]string // map[domain][]user
+
+// addUser parses and adds a new user to the map of users and domains.
+func (d domains) addUser(userName upspin.UserName) {
+	user, domain, err := path.UserAndDomain(userName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	users, found := d[domain]
+	if !found {
+		d[domain] = make([]string, 1, 5)
+	}
+	d[domain] = append(users, user)
+}
+
+// getUsersByDomain writes a list of users grouped by domain. If more than one user exists for given domain,
+// the usernames are appended together separated by a comma, as in p,r,edpin@google.com bar,baz@foo.com.
+func (d domains) writeUsersByDomain(buf *bytes.Buffer) {
+	var doms []string
+	for dom := range d {
+		doms = append(doms, dom)
+	}
+	for j, dom := range doms {
+		users := d[dom]
+		for i, u := range users {
+			buf.WriteString(u)
+			if i < len(users)-1 {
+				buf.WriteByte(',')
+			}
+		}
+		buf.WriteByte('@')
+		buf.WriteString(dom)
+		if j < len(doms)-1 {
+			buf.WriteByte(' ')
+		}
+	}
+}
+
+func formatReaders(readers []upspin.UserName) string {
+	const (
+		maxLen = 44
+	)
+	if len(readers) == 0 {
+		return ""
+	}
+	domainMap := domains{}
+	for _, r := range readers {
+		domainMap.addUser(r)
+	}
+	var formatted bytes.Buffer
+	domainMap.writeUsersByDomain(&formatted)
+
+	if formatted.Len() >= maxLen-3 {
+		formatted.Truncate(maxLen)
+		formatted.WriteString("...")
+	}
+	return formatted.String()
+}
+
+func printLongDirEntries(de []*upspin.DirEntry) {
+	for _, e := range de {
+		isDirChar := '_'
+		if e.Metadata.IsDir {
+			isDirChar = 'd'
+			n := len(e.Name)
+			if e.Name[n-1:n] != "/" {
+				e.Name = e.Name + "/"
+			}
+		}
+		endpt := endpoint.String(&e.Location.Endpoint)
+		fmt.Printf("%c %d [%s]\t[%s]\t%s\n", isDirChar, e.Metadata.Sequence, endpt, formatReaders(e.Metadata.Readers), e.Name)
 	}
 }
 
