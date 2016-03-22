c2c885ed0a57153624ccd30adacec606b4c34470
Eduardo Pinheiro <edpin@google.com>
2016-03-22 13:56:29 -0700

    admin: scripts for GCE maintenance
    - Builds binaries, pushes them to upspin.io and restarts them.
    - Lint errors checked with shellcheck. All clean.
    
    Change-Id: I6c9890541555e6fee845e135c72847d4616e67b4

---
+++ cmd/admin/deploy-servers.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+#
+# This script builds server binaries and deploys them to GCP sequentially.
+# Ensure you have proper SSH keys to login to upspin.io.
+#
+# Usage:
+#
+# ./deploy-servers.sh [user|directory|store|frontend] [-d] [-b]
+#
+# If a server name is not given, all are rebuilt and redeployed.
+# -d deploy only -- does not rebuild servers.
+# -b build only -- does not deploy servers.
+
+errors=()
+root=""
+deployonly=0
+buildonly=0
+default_serverlist=(user directory store frontend)
+
+# Builds the named binary statically.
+function build {
+    server=$1
+    pushd "$root/cmd/$server" >/dev/null
+    echo "=== Building $server ..."
+    runsafely env CGO_ENABLED=0 go build -v -o "/tmp/$server"
+    popd >/dev/null
+}
+
+# Deploys the named binary to GCE and restarts it
+function deploy {
+    server=$1
+    echo "=== Deploying $server ..."
+    # Copy binary to GCE
+    runsafely scp "/tmp/$server" upspin.io:/tmp
+    # Stop service and move binary
+    runsafely ssh upspin.io "cd /var/www; sudo supervisorctl stop upspin-$server; sudo cp /tmp/$server ."
+    if [ "$server" == "frontend" ]; then
+        runsafely ssh upspin.io "cd /var/www; sudo setcap CAP_NET_BIND_SERVICE=+eip /var/www/frontend"
+    fi
+    # Re-deploy service
+    runsafely ssh upspin.io "sudo supervisorctl start upspin-$server"
+}
+
+# Finds the root of project upspin by looking in the current directory and in $GOPATH and puts it in $root
+function find_root {
+    local test_dir="$GOPATH/src/upspin.googlesource.com/upspin.git"
+    if [ -f "$test_dir/upspin/upspin.go" ]; then
+        root=$test_dir
+    elif [ -f "../../upspin/upspin.go" ]; then
+        pushd ../..
+        root=$(pwd)
+        popd
+    else
+        root="not found"
+    fi
+}
+
+# Runs the command and captures it in case of errors.
+function runsafely {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        local msg="=== error with $*"
+        errors[${#errors[*]}]="$msg"
+        echo "$msg" >&2
+    fi
+    return $status
+}
+
+function main {
+    local serverlist=()
+    while [[ "$#" -gt 0 ]]; do
+        local key="$1"
+
+        case $key in
+            -d|--deploy-only)
+            deployonly=1
+            ;;
+            -b|--build-only)
+            buildonly=1
+            ;;
+            store)
+            serverlist[${#serverlist[*]}]="store"
+            ;;
+            directory)
+            serverlist[${#serverlist[*]}]="directory"
+            ;;
+            user)
+            serverlist[${#serverlist[*]}]="user"
+            ;;
+            frontend)
+            serverlist[${#serverlist[*]}]="frontend"
+            ;;
+            *)
+            echo "Error parsing option $key"
+            exit
+            ;;
+        esac
+        shift
+    done
+
+    find_root
+    echo "Root of Upspin: $root"
+
+    if [[ ${#serverlist[@]} == 0 ]]; then
+        serverlist=("${default_serverlist[@]}")
+    fi
+
+    if [[ $deployonly -gt 0 && $buildonly -gt 0 ]]; then
+        echo "Nothing to do."
+        exit
+    fi
+
+    echo "Going to deploy the following: ${serverlist[@]}"
+
+    for server in "${serverlist[@]}"; do
+        if [ $deployonly == 0 ]; then
+            build $server
+        fi
+        if [ $buildonly == 0 ]; then
+            deploy $server
+        fi
+    done
+
+    if [[ ${#errors[@]} == 0 ]]; then
+        echo "Success"
+    else
+        echo "${#errors[@]} errors found:"
+        for ((i = 0; i < ${#errors[@]}; i++)); do
+            echo "${errors[$i]}"
+        done
+    fi
+}
+
+main "$@"
