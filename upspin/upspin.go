f9851ebd7baaf5c25b5e179f94d17905c2c8c9b4
Rob Pike <r@google.com>
2016-03-24 11:05:19 +1100

    upspin: modify Time comment to make it clear it's not dependable
    Response to a post-commit code review.
    
    Change-Id: I200ac8e423075f2b63c95276f3c5400f3f1aee53

--- upspin/upspin.go
+++ upspin/upspin.go
@@ -246,7 +246,7 @@ type Metadata struct {
 	IsDir    bool       // The file is a directory.
 	Sequence int64      // The sequence (version) number of the item.
 	Size     uint64     // Length of file in bytes.
-	Time     Time       // Time file was last written.
+	Time     Time       // Time associated with file; might be when it was last written.
 	Readers  []UserName // Users (or groups) allowed to read this entry (only used if IsDir).
 	PackData []byte     // Packing-specific metadata stored in directory.
 }
