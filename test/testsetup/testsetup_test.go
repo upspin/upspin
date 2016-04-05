7754266d8952dd0add48c2b5d82934cccf80aa45
Eduardo Pinheiro <edpin@google.com>
2016-04-05 13:05:17 -0700

    test: add a test framework.
    - Creating a directory tree and test users should now be simple(r).
    - Will migrate all_test.go to this framework if we're satisfied with it.
    
    All tests pass.
    No new lint errors.
    go vet is quiet.
    
    Change-Id: Ie3d8c6c0743d72143d41653d6213d73143894db2

---
+++ test/testsetup/testsetup_test.go
@@ -0,0 +1,61 @@
+package testsetup_test
+
+import (
+	"testing"
+
+	s "upspin.googlesource.com/upspin.git/test/testsetup"
+	"upspin.googlesource.com/upspin.git/upspin"
+
+	_ "upspin.googlesource.com/upspin.git/directory/testdir"
+	_ "upspin.googlesource.com/upspin.git/store/teststore"
+)
+
+func TestInProcess(t *testing.T) {
+	const (
+		userName = "testuser@testdomain.com"
+		content2 = "yo! file2"
+		verbose  = true
+	)
+	client, context, err := s.InProcess()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = s.AddUser(context, upspin.UserName(userName), upspin.EEp256Pack)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testSetup := s.Setup{
+		s.N(userName+"/Dir1/", ""),
+		s.N(userName+"/Dir1/file1.txt", "yo! file1"),
+		s.N(userName+"/Dir2/", ""),
+		s.N(userName+"/Dir2/file2.txt", content2),
+	}
+
+	err = s.Tree(client, verbose, testSetup)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Now check the tree was setup correctly
+	de, err := context.Directory.Glob(userName + "/*")
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectedDirs := []upspin.PathName{upspin.PathName(userName + "/Dir1"), upspin.PathName(userName + "/Dir2")}
+	if len(de) != len(expectedDirs) {
+		t.Errorf("Expected %d dir entries, got %d", len(expectedDirs), len(de))
+	}
+	for i := 0; i < len(expectedDirs); i++ {
+		if de[i].Name != expectedDirs[i] {
+			t.Errorf("Expected entry %s, got %s", expectedDirs[i], de[i].Name)
+		}
+	}
+	data, err := client.Get(upspin.PathName(userName + "/Dir2/file2.txt"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(data) != content2 {
+		t.Errorf("Expected content %q, got %q", content2, data)
+	}
+}
