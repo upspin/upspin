33d7644eeb2b5b09cc70528a6f5332621816516f
Eduardo Pinheiro <edpin@google.com>
2016-04-05 11:16:30 -0700

    keygen: break keygen into functional pieces for reuse.
    - I'm working on a testing framework to requires we
      generate new keys on demand. Rather than calling a
      binary, I'm breaking keygen into pieces and will re-use
      them in another CL.
    
    TESTED:
    Keygen still generates keys and --secretseed still re-generates a previously known key.
    
    All tests pass.
    No new lint errors
    go vet is silent.
    
    Change-Id: I94f187b302df40e850de6cb4f355bc7cfa0b26e0

--- cmd/keygen/keygen.go
+++ cmd/keygen/keygen.go
@@ -6,11 +6,6 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
 	"encoding/binary"
 	"flag"
 	"fmt"
@@ -20,9 +15,8 @@ import (
 
 	"upspin.googlesource.com/upspin.git/cmd/keygen/proquint"
 	"upspin.googlesource.com/upspin.git/pack"
+	"upspin.googlesource.com/upspin.git/pack/ee"
 	"upspin.googlesource.com/upspin.git/upspin"
-
-	_ "upspin.googlesource.com/upspin.git/pack/ee" // needed for init() registration
 )
 
 var (
@@ -31,43 +25,11 @@ var (
 	where   = flag.String("where", "", "directory to write keys. If empty, $HOME/.ssh/")
 )
 
-// drng is an io.Reader returning deterministic random bits seeded from aesKey.
-type drng struct {
-	aes     cipher.Block
-	counter uint32
-	random  []byte
-}
-
-func (d *drng) Read(p []byte) (n int, err error) {
-	lenp := len(p)
-	n = lenp
-	var drand [16]byte
-	for n > 0 {
-		if len(d.random) == 0 {
-			binary.BigEndian.PutUint32(drand[0:4], d.counter)
-			d.counter++
-			binary.BigEndian.PutUint32(drand[4:8], d.counter)
-			d.counter++
-			binary.BigEndian.PutUint32(drand[8:12], d.counter)
-			d.counter++
-			binary.BigEndian.PutUint32(drand[12:16], d.counter)
-			d.counter++
-			d.random = drand[:]
-			d.aes.Encrypt(d.random, d.random)
-		}
-		m := copy(p, d.random)
-		n -= m
-		p = p[m:]
-		d.random = d.random[m:]
-	}
-	return lenp, nil
-}
-
-func createKeys(curve elliptic.Curve, packer upspin.Packer) {
-
+func createKeys(pack upspin.Packing) {
 	// Pick secret 128 bits.
 	// TODO  Consider whether we are willing to ask users to write long seeds for P521.
 	b := make([]byte, 16)
+	var proquintStr string
 	if len(*secret) > 0 {
 		if len((*secret)) != 47 || (*secret)[5] != '-' {
 			log.Fatalf("expected secret like\n lusab-babad-gutih-tugad.gutuk-bisog-mudof-sakat\n"+
@@ -77,31 +39,17 @@ func createKeys(curve elliptic.Curve, packer upspin.Packer) {
 			binary.BigEndian.PutUint16(b[2*i:2*i+2], proquint.Decode([]byte((*secret)[6*i:6*i+5])))
 		}
 	} else {
-		_, err := rand.Read(b)
-		if err != nil {
-			log.Fatalf("key not generated: %s", err)
-		}
+		ee.GenEntropy(b)
 		proquints := make([]interface{}, 8)
 		for i := 0; i < 8; i++ {
 			proquints[i] = proquint.Encode(binary.BigEndian.Uint16(b[2*i : 2*i+2]))
 		}
-		fmt.Printf("-secretseed %s-%s-%s-%s.%s-%s-%s-%s\n", proquints...)
+		proquintStr = fmt.Sprintf("-secretseed %s-%s-%s-%s.%s-%s-%s-%s\n", proquints...)
 		// Ignore punctuation on input;  this format is just to help the user keep their place.
+
 	}
 
-	// Create crypto deterministic random generator from b.
-	d := &drng{}
-	cipher, err := aes.NewCipher(b)
-	if err != nil {
-		panic("can't happen")
-	}
-	d.aes = cipher
-
-	// Generate random key-pair.
-	priv, err := ecdsa.GenerateKey(curve, d)
-	if err != nil {
-		log.Fatalf("key not generated: %s", err)
-	}
+	keyPair, err := ee.CreateKeys(pack, b)
 
 	// Save the keys to files.
 	private, err := os.Create(filepath.Join(keydir(), "secret.upspinkey"))
@@ -116,14 +64,10 @@ func createKeys(curve elliptic.Curve, packer upspin.Packer) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = private.WriteString(priv.D.String() + "\n")
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = public.WriteString(packer.String() + "\n" + priv.X.String() + "\n" + priv.Y.String() + "\n")
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	private.WriteString(string(keyPair.Private))
+	public.WriteString(string(keyPair.Public))
+
 	err = private.Close()
 	if err != nil {
 		log.Fatal(err)
@@ -132,12 +76,12 @@ func createKeys(curve elliptic.Curve, packer upspin.Packer) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	if proquintStr != "" {
+		fmt.Print(proquintStr)
+	}
 }
 
 func main() {
-	// because ee.common.curve is not exported
-	curve := []elliptic.Curve{16: elliptic.P256(), 18: elliptic.P384(), 17: elliptic.P521()}
-
 	log.SetFlags(0)
 	log.SetPrefix("keygen: ")
 	flag.Parse()
@@ -151,7 +95,7 @@ func main() {
 	if packer == nil {
 		log.Fatalf("unrecognized packing %s", *packing)
 	}
-	createKeys(curve[packer.Packing()], packer)
+	createKeys(packer.Packing())
 }
 
 func keydir() string {
