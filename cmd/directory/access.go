80e6081295fea4fdbc76f95e34d6485f62838093
Eduardo Pinheiro <edpin@google.com>
2016-04-08 16:15:09 -0700

    directory: server updates to handle Access files.
    - The root is now special. It stores the parsed contents
      of all Access files in a map for quick lookups.
    - New or updated Access files are parsed upon upload.
    - Groups are parsed on-demand when requested by the Access file.
      (Though, this is currently not working and Group updates will force a
      re-parsing of all Access files, potentially.
    - To fetch contents from the Store, the Directory service must
      authenticate itself with Store like everyone. This introduces the
      problem that we must store a key for the Dir service. Currently,
      such key is hard-coded. This will change before ehg@ has a heart attack.
      But relying on him for guidance.
    - Added caching to root and to all dir entries for performance reasons.
    - Breaks the server into "modules", which I'm calling simply "files" (not
      sure what the convention is for files within the same package).
    - TODO: Groups.
    - Test coverage is still pretty decent at 75%. We can always add more error-path checking.
    
    All tests (prior to Rob's breaking CL) passed.
    No new lint errors.
    go vet is clean.
    Inspected imports for craziness.
    
    Change-Id: Ib04096336d815cb2ff97bcae04215933ea4d3955

---
+++ cmd/directory/access.go
@@ -0,0 +1,78 @@
+package main
+
+// This file handles parsing Access and Group files, updating the root and verifying access.
+
+import (
+	"fmt"
+
+	"errors"
+
+	"upspin.googlesource.com/upspin.git/access"
+	"upspin.googlesource.com/upspin.git/path"
+	"upspin.googlesource.com/upspin.git/upspin"
+)
+
+// updateAccess handles fetching and parsing a new or updated Access file and caches its parsed representation in root.accessFiles.
+func (d *dirServer) updateAccess(accessPath *path.Parsed, location *upspin.Location) error {
+	buf, err := d.storeClient.Get(location)
+	if err != nil {
+		return err
+	}
+	acc, err := access.Parse(accessPath.Path(), buf)
+	if err != nil {
+		// access.Parse already sets the path, no need to duplicate it here.
+		return newDirError("UpdateAccess", "", err.Error())
+	}
+	root, err := d.getRoot(accessPath.User)
+	if err != nil {
+		return err
+	}
+	root.accessFiles[accessPath.Path()] = acc
+	err = d.putRoot(accessPath.User, root)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// hasRight reports whether the user has the right on the path. It's assumed that all prior verifications have taken
+// place, such as verifying whether the user is writing to a file that existed as a directory or vice-versa, etc.
+func (d *dirServer) hasRight(op string, user upspin.UserName, right access.Right, pathName upspin.PathName) (bool, error) {
+	parsedPathToCheck, err := path.Parse(pathName)
+	if err != nil {
+		return false, newDirError(op, pathName, err.Error())
+	}
+	root, err := d.getRoot(parsedPathToCheck.User)
+	if err != nil {
+		return false, err
+	}
+
+	// Now we need to find the relevant Access file. Start with the parent dir.
+	accessDir := parsedPathToCheck
+	for {
+		accessDir = accessDir.Drop(1)
+
+		acc, found := root.accessFiles[path.Join(accessDir.Path(), "Access")]
+		if found {
+			return d.checkRights(user, right, parsedPathToCheck.Path(), acc)
+		}
+
+		// If we reached the root, there's nothing else to do.
+		if accessDir.IsRoot() {
+			break
+		}
+	}
+	// We did not find any Access file. The root should have an implicit one. This is an error.
+	return false, errors.New("No Access file found anywhere")
+}
+
+// checkRights is a convenience function that applies the Can method of the access entry given using the user, right and path provided.
+func (d *dirServer) checkRights(user upspin.UserName, right access.Right, pathName upspin.PathName, acc *access.Access) (bool, error) {
+	can, morePaths, err := acc.Can(user, right, pathName)
+	if err == access.ErrNeedGroup {
+		// TODO: fetch groups.
+		return false, fmt.Errorf("need more groups: %+v TBD", morePaths)
+	}
+	logMsg.Printf("Access check: user %s attempting to %v file %s: allowed=%v", user, right, pathName, can)
+	return can, nil
+}
---
+++ cmd/directory/storage.go
@@ -0,0 +1,96 @@
+package main
+
+// This file deals with low-level storage of dir entries on GCP, their mashaling and unmarshaling and caching.
+
+// TODO: we only have one server, so we know when the cache is invalid which is never, since we update it every time
+// we update the backend. But this won't be true if we ever have more than one server serving a given user. At that point,
+// we need to subscribe to updates from GCP and invalidate the cache accordingly.
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"upspin.googlesource.com/upspin.git/path"
+	"upspin.googlesource.com/upspin.git/upspin"
+)
+
+var (
+	errEntryNotFound = newDirError("download", "", "pathname not found")
+)
+
+func (d *dirServer) getDirEntry(parsedPath *path.Parsed) (*upspin.DirEntry, error) {
+	if parsedPath.IsRoot() {
+		root, err := d.getRoot(parsedPath.User)
+		if err != nil {
+			return nil, err
+		}
+		return root.dirEntry, nil
+	}
+	return d.getNonRoot(parsedPath.Path())
+}
+
+// putDirEntry writes to the backend at the given path a dir entry that could be a root or a regular dir entry.
+// If it's a root dir entry, it first attempts to read an expected-to-exist root in order to update it.
+func (d *dirServer) putDirEntry(parsedPath *path.Parsed, dirEntry *upspin.DirEntry) error {
+	if parsedPath.IsRoot() {
+		root, err := d.getRoot(parsedPath.User)
+		if err != nil {
+			return err
+		}
+		root.dirEntry = dirEntry
+		return d.putRoot(parsedPath.User, root)
+	}
+	return d.putNonRoot(parsedPath.Path(), dirEntry)
+}
+
+// getNonRoot returns the dir entry for the given path, possibly going to stable storage to find it.
+func (d *dirServer) getNonRoot(path upspin.PathName) (*upspin.DirEntry, error) {
+	logMsg.Printf("Looking up dir entry %q on storage backend", path)
+
+	// Check cache first.
+	if dir, ok := d.dirCache.Get(path); ok {
+		return dir.(*upspin.DirEntry), nil
+	}
+
+	// Not in cache.
+	savedDirEntry := new(upspin.DirEntry)
+	buf, err := d.getCloudBytes(path)
+	if err != nil {
+		return savedDirEntry, err
+	}
+	err = json.Unmarshal(buf, savedDirEntry)
+	if err != nil {
+		return savedDirEntry, newDirError("getmeta", path, fmt.Sprintf("json unmarshal failed retrieving metadata: %v", err))
+	}
+	d.dirCache.Add(path, savedDirEntry)
+	return savedDirEntry, nil
+}
+
+// putNonRoot forcibly writes to stable storage the given dir entry at the canonical path on the
+// backend without checking anything but the marshaling.
+func (d *dirServer) putNonRoot(path upspin.PathName, dirEntry *upspin.DirEntry) error {
+	// TODO(ehg): if using crypto packing here, as we should, how will secrets get to code at service startup?
+	// Save on cache.
+	d.dirCache.Add(path, dirEntry)
+	jsonBuf, err := json.Marshal(dirEntry)
+	if err != nil {
+		// This is really bad. It means we created a DirEntry that does not marshal to JSON.
+		errMsg := fmt.Sprintf("internal server error: conversion to json failed: %s", err)
+		logErr.Printf("WARN: %s: %s: %+v", errMsg, path, dirEntry)
+		return newDirError("putmeta", path, errMsg)
+	}
+	logMsg.Printf("Storing dir entry at %q", path)
+	_, err = d.cloudClient.Put(string(path), jsonBuf)
+	return err
+}
+
+// getCloudBytes fetches the path from the storage backend.
+func (d *dirServer) getCloudBytes(path upspin.PathName) ([]byte, error) {
+	logMsg.Printf("Downloading DirEntry from GCP: %s", path)
+	data, err := d.cloudClient.Download(string(path))
+	if err != nil {
+		// TODO: differentiate FILE NOT FOUND from other errors.
+		return nil, errEntryNotFound
+	}
+	return data, nil
+}
