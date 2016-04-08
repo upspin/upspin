7d4d57557bf7daf55a719fe2004ae43bba5fa2c3
Eduardo Pinheiro <edpin@google.com>
2016-04-08 16:15:09 -0700

    directory: server updates to handle Access files.
    - The root is now special. It stores the parsed contents
      of all Access files in a map for quick lookups.
    - New or updated Access files are parsed upon upload.
    - Groups are parsed on-demand when requested by the Access file.
      (Though, this is currently not working and Group updates will force a
      re-parsing of all Access files, potentially.
    - To fetch contents from the Store, the Directory service must
      authenticate itself with Store like everyone. This introduces the
      problem that we must store a key for the Dir service. Currently,
      such key is hard-coded. This will change before ehg@ has a heart attack.
      But relying on him for guidance.
    - Added caching to root and to all dir entries for performance reasons.
    - Breaks the server into "modules", which I'm calling simply "files" (not
      sure what the convention is for files within the same package).
    - TODO: Groups.
    - Test coverage is still pretty decent at 75%. We can always add more error-path checking.
    
    All tests (prior to Rob's breaking CL) passed.
    No new lint errors.
    go vet is clean.
    Inspected imports for craziness.
    
    Change-Id: Ib04096336d815cb2ff97bcae04215933ea4d3955
    Reviewed-on: https://upspin-review.git.corp.google.com/1623
    Reviewed-by: Rob Pike <r@google.com>

---
+++ cmd/directory/store_client.go
@@ -0,0 +1,67 @@
+package main
+
+// This file handles contacting the Store server. We don't use gcpstore because we don't want cross-dependencies between
+// servers and clients. Also, we don't have a context. We do, however, have a private key for identity purposes.
+// TODO(ehg): figure out where to derive the directory server's private key from.
+
+import (
+	"fmt"
+	"net/http"
+
+	"upspin.googlesource.com/upspin.git/auth"
+	"upspin.googlesource.com/upspin.git/cloud/netutil"
+	"upspin.googlesource.com/upspin.git/upspin"
+)
+
+const (
+	maxBufLen = 1024 * 1024 // 1MB is the max size we fetch from the Store.
+)
+
+var (
+	dirServerName = upspin.UserName("upspin-dir@upspin.io")
+	// TODO(ehg): reach for the heart attack medication now.
+	dirServerKeys = upspin.KeyPair{
+		Public:  upspin.PublicKey("p256\n12753464240987498461983148972112771989345466331613802629415048914390179140074\n113057636188867924404487991022758917365734968737512850580456009882603240653118"),
+		Private: upspin.PrivateKey("68857421579026555549183754996095119843468813533440727584492238901752627939653"),
+	}
+)
+
+// storeClient is an authenticating HTTP client that talks to an Upspin Store server.
+type storeClient struct {
+	http *auth.HTTPClient
+}
+
+func newStoreClient(http *auth.HTTPClient) *storeClient {
+	return &storeClient{
+		http: http,
+	}
+}
+
+// Get fetches the contents of a reference pointed to by a location. It implicitly binds to the endpoint in the location.
+func (s *storeClient) Get(loc *upspin.Location) ([]byte, error) {
+	const op = "GetRef"
+	url := fmt.Sprintf("%s/get?ref=%s", loc.Endpoint.NetAddr, loc.Reference)
+	req, err := http.NewRequest(netutil.Get, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	return s.requestAndReadResponseBody(op, req)
+}
+
+// requestAndReadResponseBody sends a request over the HTTP client and reads the body of
+// the reply up to a safe limit.
+func (s *storeClient) requestAndReadResponseBody(op string, req *http.Request) ([]byte, error) {
+	resp, err := s.http.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, newDirError(op, "", fmt.Sprintf("store server error code: %d", resp.StatusCode))
+	}
+	// Read the body of the response
+	buf, err := netutil.BufferResponse(resp, maxBufLen)
+	if err != nil {
+		return nil, newDirError(op, "", err.Error())
+	}
+	return buf, nil
+}
