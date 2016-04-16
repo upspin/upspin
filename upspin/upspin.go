b8fcc0809f6fba0e67fc429f7c984b3c3fed6f27
Eduardo Pinheiro <edpin@google.com>
2016-04-16 14:26:16 -0700

    directory: implement Delete.
    - I'm starting to simplify the REST API, but I want to do a
      refactor-only change later. Handling Delete as a sub-handler
      of Put for now.
    
    All tests pass. Lint and vet are happy.
    
    Change-Id: Ic56185d39b080ae9687e56ccc2d0b2c5aab42e77
    Reviewed-on: https://upspin-review.git.corp.google.com/1694
    Reviewed-by: David Presotto <p@google.com>
    Reviewed-by: Rob Pike <r@google.com>

--- upspin/upspin.go
+++ upspin/upspin.go
@@ -254,7 +254,7 @@ type Directory interface {
 	Glob(pattern string) ([]*DirEntry, error)
 
 	// Delete deletes the DirEntry for a name from the directory service.
-        // It does not delete the data it references; use Store.Delete for that.
+	// It does not delete the data it references; use Store.Delete for that.
 	Delete(name PathName) error
 
 	// Endpoint returns the network endpoint of the server.
