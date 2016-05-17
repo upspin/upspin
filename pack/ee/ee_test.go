36d6a308f0c4a58269e11ade58981f743bb89c8b
Eduardo Pinheiro <edpin@google.com>
2016-05-17 09:38:35 -0700

    pack/ee: re-enable sharing test.
    - Slight refactor given the previous way of sharing was through
      Metadata.Readers.
    
    All tests pass. No new lint or vet errors.
    
    Change-Id: Ibcd518106c1fadbb3926e81a7c28f2bde3774f85

--- pack/ee/ee_test.go
+++ pack/ee/ee_test.go
@@ -45,6 +45,13 @@ func unpackBlob(t *testing.T, ctx *upspin.Context, packer upspin.Packer, d *upsp
 	return clear[:m]
 }
 
+// shareBlob updates the packdata of a blob such that the public keys given are readers of the blob.
+func shareBlob(t *testing.T, ctx *upspin.Context, packer upspin.Packer, readers []upspin.PublicKey, packdata *[]byte) {
+	pd := make([]*[]byte, 1)
+	pd[0] = packdata
+	packer.Share(ctx, readers, pd)
+}
+
 func testPackAndUnpack(t *testing.T, ctx *upspin.Context, packer upspin.Packer, name upspin.PathName, text []byte) {
 	// First pack.
 	d := &upspin.DirEntry{}
@@ -151,8 +158,7 @@ func BenchmarkPack256(b *testing.B) { benchmarkPack(b, upspin.EEp256Pack) }
 func BenchmarkPack384(b *testing.B) { benchmarkPack(b, upspin.EEp384Pack) }
 func BenchmarkPack521(b *testing.B) { benchmarkPack(b, upspin.EEp521Pack) }
 
-// TODO: re-enable when sharing is working
-func XXXTestLoadingRemoteKeys(t *testing.T) {
+func TestSharing(t *testing.T) {
 	// dude@google.com is the owner of a file that is shared with bob@foo.com.
 	const (
 		dudesUserName upspin.UserName = "dude@google.com"
@@ -161,43 +167,39 @@ func XXXTestLoadingRemoteKeys(t *testing.T) {
 		bobsUserName  upspin.UserName = "bob@foo.com"
 		text                          = "bob, here's the secret file. Sincerely, The Dude."
 	)
-	dudesPrivKey := upspin.KeyPair{
+	dudesKeyPair := upspin.KeyPair{
 		Public:  upspin.PublicKey("p256\n104278369061367353805983276707664349405797936579880352274235000127123465616334\n26941412685198548642075210264642864401950753555952207894712845271039438170192\n"),
 		Private: upspin.PrivateKey("82201047360680847258309465671292633303992565667422607675215625927005262185934\n"),
 	}
-	bobsPrivKey := upspin.KeyPair{
+	bobsKeyPair := upspin.KeyPair{
 		Public:  upspin.PublicKey("p256\n22501350716439586308300487995594907386227865907589820632958610970814693581908\n104071495646780593180743128812641149143422089655848205222288250096821814372528"),
 		Private: upspin.PrivateKey("93177533964096447201034856864549483929260757048490326880916443359483929789924"),
 	}
 
 	// Set up Dude as the creator/owner.
 	ctx, packer := setup(dudesUserName, packing)
-	// Set up a mock user service that knows about Bob's and Dude's public keys.
+	// Set up a mock user service that knows about Dude's public keys (for checking signature during unpack).
 	mockUser := &dummyUser{
-		userToMatch: []upspin.UserName{bobsUserName, dudesUserName},
-		keyToReturn: []upspin.PublicKey{bobsPrivKey.Public, dudesPrivKey.Public},
+		userToMatch: []upspin.UserName{dudesUserName},
+		keyToReturn: []upspin.PublicKey{dudesKeyPair.Public},
 	}
-	f, err := factotum.New(dudesPrivKey)
+	f, err := factotum.New(dudesKeyPair)
 	if err != nil {
 		t.Fatal(err)
 	}
 	ctx.Factotum = f // Override setup to prevent reading keys from .ssh/
 	ctx.User = mockUser
 
-	// TODO: share with bob (make bob a reader).
-	// Setup the metadata such that Bob is a reader.
-	d := &upspin.DirEntry{}
-	d.Name = pathName
-	cipher := packBlob(t, ctx, packer, d, []byte(text))
-
-	// Interim check: dummyUser returned Bob's public key when asked.
-	if mockUser.returnedKeys != 1 {
-		t.Fatal("Packer failed to request Bob's public key")
+	d := &upspin.DirEntry{
+		Name: pathName,
 	}
+	cipher := packBlob(t, ctx, packer, d, []byte(text))
+	// Share with Bob
+	shareBlob(t, ctx, packer, []upspin.PublicKey{bobsKeyPair.Public}, &d.Metadata.Packdata)
 
 	// Now load Bob as the current user.
 	ctx.UserName = bobsUserName
-	f, err = factotum.New(bobsPrivKey)
+	f, err = factotum.New(bobsKeyPair)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -209,14 +211,13 @@ func XXXTestLoadingRemoteKeys(t *testing.T) {
 	}
 
 	// Finally, check that unpack looked up Dude's public key, to verify the signature.
-	if mockUser.returnedKeys != 2 {
+	if mockUser.returnedKeys != 1 {
 		t.Fatal("Packer failed to request dude's public key")
 	}
 }
 
-// TODO: re-enable when sharing is working
-func XXXTestLoadingRemoteKeyless(t *testing.T) {
-	// dudette@google.com is the owner of a file that is attempting to be shared with mia@foo.com, but mia has no key.
+func TestBadSharing(t *testing.T) {
+	// dudette@google.com is the owner of a file that is attempting to be shared with mia@foo.com, but share wasn't called.
 	const (
 		dudettesUserName upspin.UserName = "dudette@google.com"
 		packing                          = upspin.EEp256Pack
@@ -224,49 +225,49 @@ func XXXTestLoadingRemoteKeyless(t *testing.T) {
 		miasUserName     upspin.UserName = "mia@foo.com"
 		text                             = "mia, here's the secret file. sincerely, dudette."
 	)
-	dudettesPrivKey := upspin.KeyPair{
+	dudettesKeyPair := upspin.KeyPair{
 		Public:  upspin.PublicKey("p256\n104278369061367353805983276707664349405797936579880352274235000127123465616334\n26941412685198548642075210264642864401950753555952207894712845271039438170192"),
 		Private: upspin.PrivateKey("82201047360680847258309465671292633303992565667422607675215625927005262185934"),
 	}
-	var miasPublic upspin.PublicKey
+	miasKeyPair := upspin.KeyPair{
+		Public:  upspin.PublicKey("p256\n22501350716439586308300487995594907386227865907589820632958610970814693581908\n104071495646780593180743128812641149143422089655848205222288250096821814372528"),
+		Private: upspin.PrivateKey("93177533964096447201034856864549483929260757048490326880916443359483929789924"),
+	}
 
 	ctx, packer := setup(dudettesUserName, packing)
 	mockUser := &dummyUser{
 		userToMatch: []upspin.UserName{miasUserName, dudettesUserName},
-		keyToReturn: []upspin.PublicKey{miasPublic, dudettesPrivKey.Public},
+		keyToReturn: []upspin.PublicKey{miasKeyPair.Public, dudettesKeyPair.Public},
 	}
-	f, err := factotum.New(dudettesPrivKey) // Override setup to prevent reading keys from .ssh/
+	f, err := factotum.New(dudettesKeyPair) // Override setup to prevent reading keys from .ssh/
 	if err != nil {
 		t.Fatal(err)
 	}
 	ctx.Factotum = f
 	ctx.User = mockUser
 
-	// TODO: share with mia (make mia a reader)
-	d := &upspin.DirEntry{}
-	d.Name = pathName
-	cipher := make([]byte, packer.PackLen(ctx, []byte(text), d))
-	m, err := packer.Pack(ctx, cipher, []byte(text), d)
-	if err == nil || !strings.HasPrefix(err.Error(), "no known keys for user") {
-		t.Error("Pack: ", err)
+	d := &upspin.DirEntry{
+		Name: pathName,
 	}
-	cipher = cipher[:m]
+	cipher := packBlob(t, ctx, packer, d, []byte(text))
 
-	// Check that we didn't kid ourselves into wrapping for mia without a key.
-	p, ok := packer.(eep256)
-	if ok {
-		_, wrap, _, err := p.pdUnmarshal(d.Metadata.Packdata)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if len(wrap) != 1 {
-			t.Fatalf("Expected 1 wrapped key, got %d", len(wrap))
-		}
+	// Don't share with Mia (do nothing).
+
+	// Now load Mia as the current user.
+	ctx.UserName = miasUserName
+	f, err = factotum.New(miasKeyPair)
+	if err != nil {
+		t.Fatal(err)
 	}
+	ctx.Factotum = f
 
-	clear := unpackBlob(t, ctx, packer, d, cipher)
-	if string(clear) != text {
-		t.Errorf("Expected %s, got %s", text, clear)
+	clear := make([]byte, packer.UnpackLen(ctx, cipher, d))
+	_, err = packer.Unpack(ctx, clear, cipher, d)
+	if err == nil {
+		t.Fatal("Expected error, got none.")
+	}
+	if !strings.Contains(err.Error(), "no wrapped key for me") {
+		t.Fatalf("Expected no key error, got %s", err)
 	}
 }
 
