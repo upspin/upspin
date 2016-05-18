9c132ce13ae1bb2722e773e74276006515fd9ab6
Rob Pike <r@golang.org>
2016-05-18 11:51:20 -0700

    cmd/storeserver: convert to use grpc
    Use grpc rather than Go's net/rpc, for easier interoperability.
    This requires bringing in protocol buffers, which is annoying
    but pragmatic. Because protobufs are not in charge of all our
    types, this also involves some conversion code (protobufs can't
    handle types from non-proto definitions).
    
    We will later convert user and directory's remotes as well.
    
    Change-Id: I8acc0ed96c272b3b71bc5667782caaa728632433

--- store/proto/proto.go
+++ store/proto/proto.go
@@ -1,54 +1,50 @@
 // Package proto contains the definitions shared between RPC store server and client,
 // one pair for each remote call.
-// TODO: Maybe move to gprc?
 package proto
 
 import "upspin.googlesource.com/upspin.git/upspin"
 
-type GetRequest struct {
-	Reference upspin.Reference
+// To regenerate the protocol buffer output for this package, run
+//	go generate
+
+//go:generate protoc store.proto --go_out=plugins=grpc:.
+
+// UpspinLocations converts from slices of proto's Location struct to upspin's.
+// An unfortunate side-effect of not letting protobufs rule our types.
+func UpspinLocations(l []*Location) []upspin.Location {
+	if len(l) == 0 {
+		return nil
+	}
+	ulocs := make([]upspin.Location, len(l))
+	for i := range ulocs {
+		loc := l[i]
+		ulocs[i] = upspin.Location{
+			Endpoint: upspin.Endpoint{
+				Transport: upspin.Transport(loc.Endpoint.Transport),
+				NetAddr:   upspin.NetAddr(loc.Endpoint.NetAddr),
+			},
+			Reference: upspin.Reference(loc.Reference),
+		}
+	}
+	return ulocs
 }
 
-type GetResponse struct {
-	Data      []byte
-	Locations []upspin.Location
-}
-
-type PutRequest struct {
-	Data []byte
-}
-
-type PutResponse struct {
-	Reference upspin.Reference
-}
-
-type DeleteRequest struct {
-	Reference upspin.Reference
-}
-
-type DeleteResponse struct {
-}
-
-// Types for the methods of upspin.Service.
-// TODO: Put somewhere shared?
-
-type ConfigureRequest struct {
-	Options []string
-}
-
-type ConfigureResponse struct {
-}
-
-type EndpointRequest struct {
-}
-
-type EndpointResponse struct {
-	Endpoint upspin.Endpoint
-}
-
-type ServerUserNameRequest struct {
-}
-
-type ServerUserNameResponse struct {
-	UserName string
+// Locations converts from slices of upspin's Location struct to proto's.
+// An unfortunate side-effect of not letting protobufs rule our types.
+func Locations(ul []upspin.Location) []*Location {
+	if len(ul) == 0 {
+		return nil
+	}
+	locs := make([]*Location, len(ul))
+	for i := range locs {
+		loc := ul[i]
+		locs[i] = &Location{
+			Endpoint: &Endpoint{
+				Transport: int32(loc.Endpoint.Transport),
+				NetAddr:   string(loc.Endpoint.NetAddr),
+			},
+			Reference: string(loc.Reference),
+		}
+	}
+	return locs
 }
