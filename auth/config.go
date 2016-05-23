07185e7f810749073ad41a0ba92bb7c9e437ebe4
Eduardo Pinheiro <edpin@google.com>
2016-05-23 13:14:41 -0700

    grpcauth: first step to productionizing authentication for gRPC servers.
    [Note: this is far from done. storeserver is not yet fully functional.
     However, the final CL will be huge (it's already 500+ lines as it is),
     so I thought this would be a good place to checkpoint and ask for feedback,
     and then continue the implementation and testing in subsequent CLs.]
    
    - This builds on what Eric did for dirserver.Authenticate.
    - It implements a server-side Authenticate method that will be shared by
      all upspin servers using grpc.
    - It refactors code so that eventually most of the authentication will
      be shared between HTTP and gRPC servers.
    
    All tests pass. No new lint or vet errors.
    
    Change-Id: I268b44e5f49bb349c06005509314faf2c687658c

---
+++ auth/config.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"time"
+
+	"upspin.googlesource.com/upspin.git/upspin"
+)
+
+// Config holds the configuration parameters for an instance of Handler.
+type Config struct {
+	// Lookup looks up user keys.
+	Lookup func(userName upspin.UserName) ([]upspin.PublicKey, error)
+
+	// AllowUnauthenticatedConnections allows unauthenticated connections, making it the caller's
+	// responsibility to check Handler.IsAuthenticated.
+	AllowUnauthenticatedConnections bool
+
+	// TimeFunc returns the current time. If nil, time.Now() will be used. Mostly only used for testing.
+	TimeFunc func() time.Time
+}
---
+++ auth/session.go
@@ -0,0 +1,95 @@
+package auth
+
+import (
+	"time"
+
+	"upspin.googlesource.com/upspin.git/cache"
+	"upspin.googlesource.com/upspin.git/upspin"
+)
+
+// Session contains information about the connection and the authenticated user, if any.
+type Session interface {
+	// User returns the user name present in the session. It may be empty. Note: user, even when present,
+	// might not be authenticated.
+	User() upspin.UserName
+
+	// IsAuthenticated reports whether the user in the session is authenticated. When true, a user is guaranteed
+	// to be returned by the User method.
+	IsAuthenticated() bool
+
+	// Err reports the status of the session.
+	Err() error
+
+	// Expires returns the expiration time of the session, in UTC.
+	Expires() time.Time
+}
+
+const sessionCacheSize = 1000 // max number of sessions to remember. Small values may limit parallelism.
+
+var sessionCache *cache.LRU // Caches <authToken, Session> Thread safe.
+
+// NewSession creates a new session with the given contents.
+func NewSession(user upspin.UserName, isAuth bool, expiration time.Time, authToken string, err error) Session {
+	session := &sessionImpl{
+		user:      user,
+		isAuth:    isAuth,
+		expires:   expiration,
+		authToken: authToken,
+		err:       err,
+	}
+	sessionCache.Add(authToken, session)
+	return session
+}
+
+// GetSession returns a session associated with an auth token, if one has been cached or nil otherwise.
+func GetSession(authToken string) Session {
+	session, ok := sessionCache.Get(authToken)
+	if !ok {
+		return nil
+	}
+	return session.(Session)
+}
+
+// ClearSession removes a session associated with a given auth token from the cache.
+func ClearSession(authToken string) {
+	sessionCache.Remove(authToken)
+}
+
+// resetSessions creates a new session cache. It is not thread safe. To be used for testing only.
+func resetSessions() {
+	sessionCache = cache.NewLRU(sessionCacheSize)
+}
+
+type sessionImpl struct {
+	user      upspin.UserName
+	isAuth    bool
+	authToken string
+	err       error
+	expires   time.Time
+}
+
+var _ Session = (*sessionImpl)(nil)
+
+// User implements Session.
+func (s *sessionImpl) User() upspin.UserName {
+	return s.user
+}
+
+// IsAuthenticated implements Session.
+func (s *sessionImpl) IsAuthenticated() bool {
+	return s.isAuth
+}
+
+// Err implements Session.
+func (s *sessionImpl) Err() error {
+	return s.err
+}
+
+// Expires implements Session.
+func (s *sessionImpl) Expires() time.Time {
+	return s.expires
+}
+
+func init() {
+	resetSessions()
+}
