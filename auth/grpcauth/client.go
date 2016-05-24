cfd5c778d7d9000b9248915f7b293a546df7e6c3
Eduardo Pinheiro <edpin@google.com>
2016-05-24 10:43:32 -0700

    grpcauth: Make storeserver work end-to-end with auth.
    - Added a Ping to the RPC server to implement the service Ping.
    - Checks auth on all calls that require a user.
    - Tested manually.
    
    Coming soon:
    - Automated tests.
    - A client that re-authenticates automatically.
    
    All tests pass. No new lint or vet errors.
    
    Change-Id: I98abb0af0fa8ef4894bcdb425aba71bfb42f31b5

---
+++ auth/grpcauth/client.go
@@ -0,0 +1,63 @@
+package grpcauth
+
+import (
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"upspin.googlesource.com/upspin.git/log"
+	"upspin.googlesource.com/upspin.git/upspin"
+)
+
+// NewGRPCClient returns new GRPC client connection connected securely (with TLS) to a GRPC server at the given address.
+// If allowUnauthenticatedConnections is true, the connection may not be secure.
+func NewGRPCClient(netAddr upspin.NetAddr, allowUnauthenticatedConnections bool) (*grpc.ClientConn, error) {
+	// TODO: These timeouts are arbitrary.
+	const (
+		longTimeout  = time.Second * 7
+		shortTimeout = time.Second * 3
+	)
+	// By default, wait until we get a connection. But if we're expecting TLS to fail, wait a little less.
+	timeOut := longTimeout
+	if allowUnauthenticatedConnections {
+		timeOut = shortTimeout
+	}
+	addr := string(netAddr)
+	conn, err := grpc.Dial(addr,
+		grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(nil, "")),
+		grpc.WithBlock(),
+		grpc.WithTimeout(timeOut))
+	if err != nil && allowUnauthenticatedConnections {
+		conn, err = grpc.Dial(addr,
+			grpc.WithInsecure(),
+			grpc.WithBlock(),
+			grpc.WithTimeout(longTimeout))
+		if err != nil {
+			log.Debug.Printf("grpcauth: did not connect even insecurely: %v", err)
+			return nil, err
+		}
+		log.Printf("grpcauth: connected insecurely.")
+	}
+	return conn, err
+}
+
+/* TODO: this is coming up soon.
+type AuthClientService struct {
+	authToken  string
+	clientConn grpc.ClientConn
+}
+
+func (ac *AuthClientService) Authenticate(ctx *upspin.Context) error {
+	req := &proto.AuthenticateRequest{
+		UserName: ctx.UserName,
+		Now:      time.Now().UTC().Format(time.ANSIC), // to discourage signature replay
+	}
+	sig, err := ctx.Factotum.UserSign([]byte(string(req.UserName) + " Authenticate " + req.Now))
+	if err != nil {
+		return err
+	}
+	req.Signature = sig
+	var resp proto.AuthenticateResponse
+}
+*/
