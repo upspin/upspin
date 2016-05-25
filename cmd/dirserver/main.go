46239e1f3cb7dd69c4a902c25a7109012fd8d609
Rob Pike <r@golang.org>
2016-05-25 12:16:16 -0700

    cmd/dirserver: convert to GRPC
    Convert dirserver to use GRPC, with authentication and proper user multiplexing.
    
    Change-Id: Ibbcdadab9c396ae7fa0a4adcd8086ad8c8b602a0

--- cmd/dirserver/main.go
+++ cmd/dirserver/main.go
@@ -3,32 +3,28 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"errors"
 	"flag"
 	"fmt"
-	"log"
-	"math/big"
 	"net"
-	"net/http"
-	"net/rpc"
 	"os"
-	"sync"
-	"time"
 
+	"upspin.googlesource.com/upspin.git/auth"
+	"upspin.googlesource.com/upspin.git/auth/grpcauth"
+	"upspin.googlesource.com/upspin.git/bind"
 	"upspin.googlesource.com/upspin.git/context"
-	"upspin.googlesource.com/upspin.git/directory/proto"
-	"upspin.googlesource.com/upspin.git/factotum"
+	"upspin.googlesource.com/upspin.git/endpoint"
+	"upspin.googlesource.com/upspin.git/log"
 	"upspin.googlesource.com/upspin.git/upspin"
+	"upspin.googlesource.com/upspin.git/upspin/proto"
 
-	// TODO: Other than the directory implementations, most of these
-	// are only necessary because of InitContext.
+	gContext "golang.org/x/net/context"
+
+	// TODO: Which of these are actually needed?
 
 	// Load useful packers
 	_ "upspin.googlesource.com/upspin.git/pack/debug"
 	_ "upspin.googlesource.com/upspin.git/pack/ee"
 	_ "upspin.googlesource.com/upspin.git/pack/plain"
-	"upspin.googlesource.com/upspin.git/path"
 
 	// Load required gcp services
 	_ "upspin.googlesource.com/upspin.git/directory/gcpdir"
@@ -47,244 +43,233 @@ import (
 )
 
 var (
-	port    = flag.Int("port", 8081, "TCP port number")
-	ctxfile = flag.String("context", os.Getenv("HOME")+"/upspin/rc.dirserver", "context file to use to configure server")
-)
-
-var (
-	mu     sync.Mutex
-	userID int
-	// users holds the set of already user-authenticated Servers.
-	users = make(map[upspin.UserName]*Server)
+	port         = flag.Int("port", 8081, "TCP port number")
+	ctxfile      = flag.String("context", os.Getenv("HOME")+"/upspin/rc.dirserver", "context file to use to configure server")
+	endpointFlag = flag.String("endpoint", "inprocess", "endpoint of remote service")
+	noAuth       = flag.Bool("noauth", false, "Disable authentication.")
+	certFile     = flag.String("cert", "/etc/letsencrypt/live/upspin.io/fullchain.pem", "Path to SSL certificate file")
+	certKeyFile  = flag.String("key", "/etc/letsencrypt/live/upspin.io/privkey.pem", "Path to SSL certificate key file")
 )
 
 type Server struct {
-	context *upspin.Context
-	userID  int
+	context  *upspin.Context
+	endpoint upspin.Endpoint
+	// Automatically handles authentication by implementing the Authenticate server method.
+	grpcauth.SecureServer
 }
 
 func main() {
-	log.SetFlags(0)
-	log.SetPrefix("dirserver: ")
-
 	flag.Parse()
+	log.Connect("google.com:upspin", "storeserver")
 
 	ctxfd, err := os.Open(*ctxfile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ctxfd.Close()
-	ctx, err := context.InitContext(ctxfd)
+	context, err := context.InitContext(ctxfd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	endpoint, err := endpoint.Parse(*endpointFlag)
+	if err != nil {
+		log.Fatalf("endpoint parse error: %v", err)
+	}
+
+	config := auth.Config{
+		Lookup: auth.PublicUserKeyService(),
+		AllowUnauthenticatedConnections: *noAuth,
+	}
+	grpcSecureServer, err := grpcauth.NewSecureServer(config, *certFile, *certKeyFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	s := &Server{
-		context: ctx,
+		context:      context,
+		SecureServer: grpcSecureServer,
+		endpoint:     *endpoint,
 	}
 
-	rpc.Register(s)
-	// TODO: FIGURE OUT HTTPS
-	rpc.HandleHTTP()
+	proto.RegisterDirectoryServer(grpcSecureServer.GRPCServer(), s)
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatal("listen error:", err)
 	}
-	log.Fatal(http.Serve(listener, nil))
+	grpcSecureServer.Serve(listener)
 }
 
-// Authenticate must be done before any other methods. It authenticates
-// the calling user.
-func (s *Server) Authenticate(req *proto.AuthenticateRequest, resp *proto.AuthenticateResponse) (err error) {
-	log.Printf("Authenticate %q %q", req.UserName, req.Now)
-	// Must be a valid name.
-	parsed, err := path.Parse(upspin.PathName(req.UserName))
+var (
+	// Empty structs we can allocate just once.
+	putResponse       proto.DirectoryPutResponse
+	deleteResponse    proto.DirectoryDeleteResponse
+	configureResponse proto.ConfigureResponse
+)
+
+// dirFor returns a directory service bound to the user specified in the context.
+func (s *Server) dirFor(ctx gContext.Context) (upspin.Directory, error) {
+	// Validate that we have a session. If not, it's an auth error.
+	session, err := s.GetSessionFromContext(ctx)
 	if err != nil {
-		log.Fatalf("Authenticate %q: %v", req.UserName, err)
-		return err
+		return nil, err
 	}
-
-	// Time should be sane.
-	reqNow, err := time.Parse(time.ANSIC, req.Now)
-	if err != nil {
-		log.Fatalf("time failed to parse: %q", req.Now)
-		return err
-	}
-	now := time.Now()
-	if reqNow.After(now.Add(30*time.Second)) || reqNow.Before(now.Add(-45*time.Second)) {
-
-		log.Printf("timestamp is far wrong, but proceeding anyway")
-	}
-
-	// Signature should verify.
-	_, keys, err := s.context.User.Lookup(req.UserName)
-	if err != nil {
-		return err
-	}
-	err = verifySignature(keys, []byte(string(req.UserName)+" DirectoryAuthenticate "+req.Now), req.Signature.R, req.Signature.S)
-	if err != nil {
-		log.Printf("bad signature, but proceeding anyway: %s", err)
-	}
-
-	mu.Lock()
-	defer mu.Unlock()
-
-	userServer := users[req.UserName]
-	if userServer != nil {
-		// Already have a server for this user.
-		resp.ID = userServer.userID
-		return nil
-	}
-
-	userID++
-
-	// Create a new server for this user and context.
-	ctx := *s.context
-	ctx.UserName = parsed.User()
-	userServer = &Server{
-		context: &ctx,
-		userID:  userID,
-	}
-
-	// Register the new server under its user-qualified name.
-	err = rpc.RegisterName(fmt.Sprintf("Server_%d", userID), userServer)
-	if err != nil {
-		log.Printf("Authenticate %q RPC registration failed: %v", parsed.User(), err)
-		return err
-	}
-
-	// All is well. Install it.
-	users[req.UserName] = userServer
-
-	resp.ID = userID
-	return nil
+	context := *s.context
+	context.UserName = session.User()
+	return bind.Directory(&context, s.endpoint)
 }
 
-// verifySignature verifies that the hash was signed by one of the keys.
-func verifySignature(keys []upspin.PublicKey, hash []byte, r, s *big.Int) error {
-	for _, k := range keys {
-		ecdsaPubKey, _, err := factotum.ParsePublicKey(k)
-		if err != nil {
-			return err
-		}
-		if ecdsa.Verify(ecdsaPubKey, hash, r, s) {
-			return nil
-		}
-	}
-	return fmt.Errorf("no keys verified signature")
-}
-
-var ErrUnauthenticated = errors.New("user not authenticated")
-
-func (s *Server) Lookup(req *proto.LookupRequest, resp *proto.LookupResponse) (err error) {
+func (s *Server) Lookup(ctx gContext.Context, req *proto.DirectoryLookupRequest) (*proto.DirectoryLookupResponse, error) {
 	log.Printf("Lookup %q", req.Name)
-	if s.userID == 0 {
-		err = ErrUnauthenticated
-	} else {
-		resp.Entry, err = s.context.Directory.Lookup(req.Name)
+
+	dir, err := s.dirFor(ctx)
+	if err != nil {
+		return nil, err
 	}
+	entry, err := dir.Lookup(upspin.PathName(req.Name))
 	if err != nil {
 		log.Printf("Lookup %q failed: %v", req.Name, err)
+		return nil, err
 	}
-	return err
-}
-
-func (s *Server) Put(req *proto.PutRequest, resp *proto.PutResponse) error {
-	log.Printf("Put %q", req.Entry.Name)
-	var err error
-	if s.userID == 0 {
-		err = ErrUnauthenticated
-	} else {
-		err = s.context.Directory.Put(req.Entry)
-	}
+	b, err := entry.Marshal()
 	if err != nil {
-		log.Printf("Put %q failed: %v", req.Entry.Name, err)
+		return nil, err
 	}
-	return err
+
+	resp := &proto.DirectoryLookupResponse{
+		Entry: b,
+	}
+	return resp, err
 }
 
-func (s *Server) MakeDirectory(req *proto.MakeDirectoryRequest, resp *proto.MakeDirectoryResponse) (err error) {
-	log.Printf("MakeDirectory %q", req.Name)
-	if s.userID == 0 {
-		err = ErrUnauthenticated
-	} else {
-		resp.Location, err = s.context.Directory.MakeDirectory(req.Name)
+func (s *Server) Put(ctx gContext.Context, req *proto.DirectoryPutRequest) (*proto.DirectoryPutResponse, error) {
+	log.Printf("Put")
+
+	entry, err := proto.UpspinDirEntry(req.Entry)
+	if err != nil {
+		return nil, err
 	}
+	log.Printf("Put %q", entry.Name)
+	dir, err := s.dirFor(ctx)
+	if err != nil {
+		return nil, err
+	}
+	err = dir.Put(entry)
+	if err != nil {
+		log.Printf("Put %q failed: %v", entry.Name, err)
+	}
+	return &putResponse, err
+}
+
+func (s *Server) MakeDirectory(ctx gContext.Context, req *proto.DirectoryMakeDirectoryRequest) (*proto.DirectoryMakeDirectoryResponse, error) {
+	log.Printf("MakeDirectory %q", req.Name)
+
+	dir, err := s.dirFor(ctx)
+	if err != nil {
+		return nil, err
+	}
+	loc, err := dir.MakeDirectory(upspin.PathName(req.Name))
 	if err != nil {
 		log.Printf("MakeDirectory %q failed: %v", req.Name, err)
+		return nil, err
 	}
-	return err
+	locSlice := []upspin.Location{loc}
+	resp := &proto.DirectoryMakeDirectoryResponse{
+		Location: proto.Locations(locSlice)[0], // Clumsy but easy (and rare).
+	}
+	return resp, err
 }
 
-func (s *Server) Glob(req *proto.GlobRequest, resp *proto.GlobResponse) (err error) {
+func (s *Server) Glob(ctx gContext.Context, req *proto.DirectoryGlobRequest) (*proto.DirectoryGlobResponse, error) {
 	log.Printf("Glob %q", req.Pattern)
-	if s.userID == 0 {
-		err = ErrUnauthenticated
-	} else {
-		resp.Entries, err = s.context.Directory.Glob(req.Pattern)
+
+	dir, err := s.dirFor(ctx)
+	if err != nil {
+		return nil, err
 	}
+	entries, err := dir.Glob(req.Pattern)
 	if err != nil {
 		log.Printf("Glob %q failed: %v", req.Pattern, err)
+		return nil, err
 	}
-	return err
+	data, err := proto.DirEntryBytes(entries)
+	resp := &proto.DirectoryGlobResponse{
+		Entries: data,
+	}
+	return resp, err
 }
 
-func (s *Server) Delete(req *proto.DeleteRequest, resp *proto.DeleteResponse) error {
+func (s *Server) Delete(ctx gContext.Context, req *proto.DirectoryDeleteRequest) (*proto.DirectoryDeleteResponse, error) {
 	log.Printf("Delete %q", req.Name)
-	var err error
-	if s.userID == 0 {
-		err = ErrUnauthenticated
-	} else {
-		err = s.context.Directory.Delete(req.Name)
+
+	dir, err := s.dirFor(ctx)
+	if err != nil {
+		return nil, err
 	}
+	err = dir.Delete(upspin.PathName(req.Name))
 	if err != nil {
 		log.Printf("Delete %q failed: %v", req.Name, err)
 	}
-	return err
+	return &deleteResponse, err
 }
 
-func (s *Server) WhichAccess(req *proto.WhichAccessRequest, resp *proto.WhichAccessResponse) (err error) {
+func (s *Server) WhichAccess(ctx gContext.Context, req *proto.DirectoryWhichAccessRequest) (*proto.DirectoryWhichAccessResponse, error) {
 	log.Printf("WhichAccess %q", req.Name)
-	if s.userID == 0 {
-		err = ErrUnauthenticated
-	} else {
-		resp.Name, err = s.context.Directory.WhichAccess(req.Name)
+
+	dir, err := s.dirFor(ctx)
+	if err != nil {
+		return nil, err
 	}
+	name, err := dir.WhichAccess(upspin.PathName(req.Name))
 	if err != nil {
 		log.Printf("WhichAccess %q failed: %v", req.Name, err)
 	}
-	return err
+	resp := &proto.DirectoryWhichAccessResponse{
+		Name: string(name),
+	}
+	return resp, err
 }
 
-func (s *Server) Configure(req *proto.ConfigureRequest, resp *proto.ConfigureResponse) error {
+// Configure implements upspin.Service
+func (s *Server) Configure(ctx gContext.Context, req *proto.ConfigureRequest) (*proto.ConfigureResponse, error) {
 	log.Printf("Configure %q", req.Options)
-	var err error
-	if s.userID == 0 {
-		err = ErrUnauthenticated
-	} else {
-		err = s.context.Directory.Configure(req.Options...)
+	dir, err := s.dirFor(ctx)
+	if err != nil {
+		return nil, err
 	}
+	err = dir.Configure(req.Options...)
 	if err != nil {
 		log.Printf("Configure %q failed: %v", req.Options, err)
 	}
-	return err
+	return &configureResponse, err
 }
 
-func (s *Server) Endpoint(req *proto.EndpointRequest, resp *proto.EndpointResponse) error {
+// Endpoint implements upspin.Service
+func (s *Server) Endpoint(ctx gContext.Context, req *proto.EndpointRequest) (*proto.EndpointResponse, error) {
 	log.Print("Endpoint")
-	if s.userID == 0 {
-		log.Printf("Endpoint failed: %v", ErrUnauthenticated)
-		return ErrUnauthenticated
+	dir, err := s.dirFor(ctx)
+	if err != nil {
+		return nil, err
 	}
-	resp.Endpoint = s.context.Directory.Endpoint()
-	return nil
+	endpoint := dir.Endpoint()
+	resp := &proto.EndpointResponse{
+		Endpoint: &proto.Endpoint{
+			Transport: int32(endpoint.Transport),
+			NetAddr:   string(endpoint.NetAddr),
+		},
+	}
+	return resp, nil
 }
 
-func (s *Server) ServerUserName(req *proto.ServerUserNameRequest, resp *proto.ServerUserNameResponse) error {
+// ServerUserName implements upspin.Service
+func (s *Server) ServerUserName(ctx gContext.Context, req *proto.ServerUserNameRequest) (*proto.ServerUserNameResponse, error) {
 	log.Print("ServerUserName")
-	if s.userID == 0 {
-		log.Printf("ServerUserName failed: %v", ErrUnauthenticated)
-		return ErrUnauthenticated
+	dir, err := s.dirFor(ctx)
+	if err != nil {
+		return nil, err
 	}
-	resp.UserName = s.context.Directory.ServerUserName()
-	return nil
+	userName := dir.ServerUserName()
+	resp := &proto.ServerUserNameResponse{
+		UserName: string(userName),
+	}
+	return resp, nil
 }
