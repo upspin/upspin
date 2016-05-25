e136c33f4f29ed6cd0ccf04b2d070f49e9fabf09
Eduardo Pinheiro <edpin@google.com>
2016-05-25 16:05:19 -0700

    bind: Add Release function to close services and release resources.
    - If one calls bind.Bind they now have to call bind.Release when they're
      done with the Service.
    
    All tests pass. No new lint or vet errors.
    
    Change-Id: I3fd4fed47ce50b648d903feaa77dad3ae97fe5e4

--- bind/bind.go
+++ bind/bind.go
@@ -7,7 +7,6 @@ import (
 	"sync"
 	"time"
 
-	"upspin.googlesource.com/upspin.git/cache"
 	"upspin.googlesource.com/upspin.git/upspin"
 )
 
@@ -23,8 +22,9 @@ type dialedService struct {
 	lastPing time.Time
 }
 
+type dialCache map[dialKey]*dialedService
+
 const (
-	cacheSize             = 20
 	pingFreshnessDuration = time.Minute * 15
 )
 
@@ -35,10 +35,10 @@ var (
 	storeMap     = make(map[upspin.Transport]upspin.Store)
 
 	// These caches hold <dialKey, *dialedService> for each respective service type.
-	// They are thread safe.
-	userBoundCache      = cache.NewLRU(cacheSize)
-	directoryBoundCache = cache.NewLRU(cacheSize)
-	storeBoundCache     = cache.NewLRU(cacheSize)
+	userDialCache      = make(dialCache)
+	directoryDialCache = make(dialCache)
+	storeDialCache     = make(dialCache)
+	reverseLookup      = make(map[upspin.Service]dialKey)
 )
 
 // RegisterUser registers a User interface for the transport.
@@ -85,7 +85,7 @@ func User(cc *upspin.Context, e upspin.Endpoint) (upspin.User, error) {
 	if !ok {
 		return nil, fmt.Errorf("User service with transport %q not registered", e.Transport)
 	}
-	x, err := reachableService(cc, e, userBoundCache, u)
+	x, err := reachableService(cc, e, userDialCache, u)
 	if err != nil {
 		return nil, err
 	}
@@ -100,7 +100,7 @@ func Store(cc *upspin.Context, e upspin.Endpoint) (upspin.Store, error) {
 	if !ok {
 		return nil, fmt.Errorf("Store service with transport %q not registered", e.Transport)
 	}
-	x, err := reachableService(cc, e, storeBoundCache, s)
+	x, err := reachableService(cc, e, storeDialCache, s)
 	if err != nil {
 		return nil, err
 	}
@@ -115,23 +115,49 @@ func Directory(cc *upspin.Context, e upspin.Endpoint) (upspin.Directory, error) 
 	if !ok {
 		return nil, fmt.Errorf("Directory service with transport %q not registered", e.Transport)
 	}
-	x, err := reachableService(cc, e, directoryBoundCache, d)
+	x, err := reachableService(cc, e, directoryDialCache, d)
 	if err != nil {
 		return nil, err
 	}
 	return x.(upspin.Directory), nil
 }
 
+// Release closes the service and releases all resources associated with it.
+func Release(service upspin.Service) error {
+	mu.Lock()
+	defer mu.Unlock()
+
+	key, ok := reverseLookup[service]
+	if !ok {
+		return errors.New("service not found")
+	}
+	switch service.(type) {
+	case upspin.Directory:
+		delete(directoryDialCache, key)
+	case upspin.Store:
+		delete(storeDialCache, key)
+	case upspin.User:
+		delete(userDialCache, key)
+	default:
+		return errors.New("invalid service type")
+	}
+	service.Close()
+	delete(reverseLookup, service)
+	return nil
+}
+
 // reachableService finds a bound and reachable service in the cache or dials a fresh one and saves it in the cache.
-func reachableService(cc *upspin.Context, e upspin.Endpoint, cache *cache.LRU, dialer upspin.Dialer) (upspin.Service, error) {
+func reachableService(cc *upspin.Context, e upspin.Endpoint, cache dialCache, dialer upspin.Dialer) (upspin.Service, error) {
 	key := dialKey{
 		context:  *cc,
 		endpoint: e,
 	}
-	s, found := cache.Get(key)
+	mu.Lock()
+	defer mu.Unlock()
+
+	ds, found := cache[key]
 	var service upspin.Service
 	if found {
-		ds := s.(*dialedService)
 		service = ds.service
 		now := time.Now()
 		if ds.lastPing.Add(pingFreshnessDuration).After(now) {
@@ -152,10 +178,10 @@ func reachableService(cc *upspin.Context, e upspin.Endpoint, cache *cache.LRU, d
 	if !service.Ping() {
 		return nil, errors.New("Ping failed")
 	}
-	// TODO: there's no Close/Stop after a Dial. When there is, we can remove services from this cache at that point.
-	cache.Add(key, &dialedService{
+	cache[key] = &dialedService{
 		service:  service,
 		lastPing: time.Now(),
-	})
+	}
+	reverseLookup[service] = key
 	return service, nil
 }
