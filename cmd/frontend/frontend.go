885fcd9cea19bdc9535f019e72275edc4df7eb63
Andrew Gerrand <adg@golang.org>
2016-05-27 15:29:42 +1000

    cmd/frontend: add go-import HTML meta tag for requests made by 'go get'
    Change-Id: I4578b48d86fd44074920e6073d2f6ff44c9c29d9

--- cmd/frontend/frontend.go
+++ cmd/frontend/frontend.go
@@ -1,8 +1,11 @@
-// A simple static file server serving on port 443 with SSL with a redirector on port 80 to 443.
+// A simple static file server serving on port 443 with SSL with a redirector
+// on port 80 to 443. It also serves meta tags to instruct "go get" where to
+// find the upspin source repository.
 package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -16,5 +19,19 @@ func main() {
 	go func() {
 		log.Fatal(http.ListenAndServe(":80", http.RedirectHandler("https://upspin.io", http.StatusMovedPermanently)))
 	}()
-	log.Fatal(http.ListenAndServeTLS(":443", *sslCertificateFile, *sslCertificateKeyFile, http.FileServer(http.Dir("/var/www/public_root"))))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServeTLS(":443", *sslCertificateFile, *sslCertificateKeyFile, nil))
+}
+
+const (
+	sourceBase = "upspin.io"
+	sourceRepo = "https://upspin.googlesource.com/upspin"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("go-get") == "1" {
+		fmt.Fprintf(w, `<meta name="go-import" content="%v git %v">`, sourceBase, sourceRepo)
+		return
+	}
+	http.FileServer(http.Dir("/var/www/public_root")).ServeHTTP(w, r)
 }
