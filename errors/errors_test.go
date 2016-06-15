a1f97d1da351351fce470118aee35a103d70979c
Rob Pike <r@golang.org>
2016-06-14 20:11:31 -0700

    errors: new package
    This is a proposal for a new errors package to be used in all of Upspin.
    The idea is that it carries information about the path, the user, the
    operation attempted, and what sort of error it is (permission, I/O, etc.).
    The constructer, E, makes it simple to fill in whatever fields make sense,
    and also to cascade errors when needed.
    
    For discussion.
    
    Change-Id: I81099c7046e7d2be78541fbac12f00ec6f260a9c
    Reviewed-on: https://upspin-review.git.corp.google.com/2260
    Reviewed-by: Eduardo Pinheiro <edpin@google.com>
    Reviewed-by: Andrew Gerrand <adg@google.com>

---
+++ errors/errors_test.go
@@ -0,0 +1,37 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package errors_test
+
+import (
+	"fmt"
+
+	"upspin.io/errors"
+	"upspin.io/upspin"
+)
+
+func ExampleError() {
+	path := upspin.PathName("jane@doe.com/file")
+	user := upspin.UserName("joe@blow.com")
+	err := fmt.Errorf("network unreachable")
+
+	// Single error.
+	e1 := errors.E(path, "Get", errors.IO, err)
+	fmt.Println("\nSimple error:")
+	fmt.Println(e1)
+
+	// Nested error.
+	fmt.Println("\nNested error:")
+	e2 := errors.E(path, user, "Read", errors.Other, e1)
+	fmt.Println(e2)
+
+	// Output:
+	//
+	// Simple error:
+	// jane@doe.com/file: Get: I/O error: network unreachable
+	//
+	// Nested error:
+	// jane@doe.com/file, for joe@blow.com: Read:
+	//	jane@doe.com/file: Get: I/O error: network unreachable
+}
