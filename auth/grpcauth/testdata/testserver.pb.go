e8ab94d2c2900ad7940cfa33bd8556ded15b1429
Eduardo Pinheiro <edpin@google.com>
2016-06-26 10:38:57 -0700

    grpcauth: upgrade to grpc3, fix race
    - go generate to upgrade the proto to grpc3.
    - go test -race found a "fake" race -- it was synchronized by channels
      so it wasn't a real race but we didn't really need the peek inside anyway.
    - Added two methods for checking and setting network activity so we can
      prevent too many application-level pings (to be used in a subsequent CL).
    
    All tests pass. No new lint errors.
    
    Change-Id: I1663056326e504ab8af0850f42852ab278fdc8c7

--- auth/grpcauth/testdata/testserver.pb.go
+++ auth/grpcauth/testdata/testserver.pb.go
@@ -15,9 +15,7 @@ It has these top-level messages:
 package prototest
 
 import proto "github.com/golang/protobuf/proto"
-import fmt "fmt"
-import math "math"
-import proto1 "upspin.io/upspin/proto"
+import proto1 "upspin.io/upspin/proto/upspin.pb"
 
 import (
 	context "golang.org/x/net/context"
@@ -25,44 +23,30 @@ import (
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
-var _ = proto.Marshal
-var _ = fmt.Errorf
-var _ = math.Inf
-
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the proto package it is being compiled against.
-const _ = proto.ProtoPackageIsVersion1
-
-type DoATrumpRequest struct {
-	PeopleDemand string `protobuf:"bytes,1,opt,name=people_demand,json=peopleDemand" json:"people_demand,omitempty"`
-}
-
-func (m *DoATrumpRequest) Reset()                    { *m = DoATrumpRequest{} }
-func (m *DoATrumpRequest) String() string            { return proto.CompactTextString(m) }
-func (*DoATrumpRequest) ProtoMessage()               {}
-func (*DoATrumpRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
-
-type DoATrumpResponse struct {
-	TrumpResponse string `protobuf:"bytes,1,opt,name=trump_response,json=trumpResponse" json:"trump_response,omitempty"`
-}
-
-func (m *DoATrumpResponse) Reset()                    { *m = DoATrumpResponse{} }
-func (m *DoATrumpResponse) String() string            { return proto.CompactTextString(m) }
-func (*DoATrumpResponse) ProtoMessage()               {}
-func (*DoATrumpResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
-
-func init() {
-	proto.RegisterType((*DoATrumpRequest)(nil), "prototest.DoATrumpRequest")
-	proto.RegisterType((*DoATrumpResponse)(nil), "prototest.DoATrumpResponse")
-}
-
-// Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
 var _ grpc.ClientConn
 
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion2
+// Reference imports to suppress errors if they are not otherwise used.
+var _ = proto.Marshal
+
+type DoATrumpRequest struct {
+	PeopleDemand string `protobuf:"bytes,1,opt,name=people_demand" json:"people_demand,omitempty"`
+}
+
+func (m *DoATrumpRequest) Reset()         { *m = DoATrumpRequest{} }
+func (m *DoATrumpRequest) String() string { return proto.CompactTextString(m) }
+func (*DoATrumpRequest) ProtoMessage()    {}
+
+type DoATrumpResponse struct {
+	TrumpResponse string `protobuf:"bytes,1,opt,name=trump_response" json:"trump_response,omitempty"`
+}
+
+func (m *DoATrumpResponse) Reset()         { *m = DoATrumpResponse{} }
+func (m *DoATrumpResponse) String() string { return proto.CompactTextString(m) }
+func (*DoATrumpResponse) ProtoMessage()    {}
+
+func init() {
+}
 
 // Client API for TestService service
 
@@ -119,58 +103,40 @@ func RegisterTestServiceServer(s *grpc.Server, srv TestServiceServer) {
 	s.RegisterService(&_TestService_serviceDesc, srv)
 }
 
-func _TestService_Authenticate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _TestService_Authenticate_Handler(srv interface{}, ctx context.Context, buf []byte) (proto.Message, error) {
 	in := new(proto1.AuthenticateRequest)
-	if err := dec(in); err != nil {
+	if err := proto.Unmarshal(buf, in); err != nil {
 		return nil, err
 	}
-	if interceptor == nil {
-		return srv.(TestServiceServer).Authenticate(ctx, in)
+	out, err := srv.(TestServiceServer).Authenticate(ctx, in)
+	if err != nil {
+		return nil, err
 	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/prototest.TestService/Authenticate",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(TestServiceServer).Authenticate(ctx, req.(*proto1.AuthenticateRequest))
-	}
-	return interceptor(ctx, in, info, handler)
+	return out, nil
 }
 
-func _TestService_Ping_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _TestService_Ping_Handler(srv interface{}, ctx context.Context, buf []byte) (proto.Message, error) {
 	in := new(proto1.PingRequest)
-	if err := dec(in); err != nil {
+	if err := proto.Unmarshal(buf, in); err != nil {
 		return nil, err
 	}
-	if interceptor == nil {
-		return srv.(TestServiceServer).Ping(ctx, in)
+	out, err := srv.(TestServiceServer).Ping(ctx, in)
+	if err != nil {
+		return nil, err
 	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/prototest.TestService/Ping",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(TestServiceServer).Ping(ctx, req.(*proto1.PingRequest))
-	}
-	return interceptor(ctx, in, info, handler)
+	return out, nil
 }
 
-func _TestService_DoATrump_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _TestService_DoATrump_Handler(srv interface{}, ctx context.Context, buf []byte) (proto.Message, error) {
 	in := new(DoATrumpRequest)
-	if err := dec(in); err != nil {
+	if err := proto.Unmarshal(buf, in); err != nil {
 		return nil, err
 	}
-	if interceptor == nil {
-		return srv.(TestServiceServer).DoATrump(ctx, in)
+	out, err := srv.(TestServiceServer).DoATrump(ctx, in)
+	if err != nil {
+		return nil, err
 	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/prototest.TestService/DoATrump",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(TestServiceServer).DoATrump(ctx, req.(*DoATrumpRequest))
-	}
-	return interceptor(ctx, in, info, handler)
+	return out, nil
 }
 
 var _TestService_serviceDesc = grpc.ServiceDesc{
@@ -191,23 +157,4 @@ var _TestService_serviceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams: []grpc.StreamDesc{},
-}
-
-var fileDescriptor0 = []byte{
-	// 238 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0x12, 0x28, 0x49, 0x2d, 0x2e,
-	0x29, 0x4e, 0x2d, 0x2a, 0x4b, 0x2d, 0xd2, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x04, 0x53,
-	0x20, 0x61, 0x29, 0xe5, 0xd2, 0x82, 0xe2, 0x82, 0xcc, 0x3c, 0xbd, 0xcc, 0x7c, 0x7d, 0x08, 0x4b,
-	0x1f, 0x2c, 0x07, 0xe5, 0x40, 0xd4, 0x2b, 0x99, 0x71, 0xf1, 0xbb, 0xe4, 0x3b, 0x86, 0x14, 0x95,
-	0xe6, 0x16, 0x04, 0xa5, 0x16, 0x96, 0x02, 0xf5, 0x09, 0x29, 0x73, 0xf1, 0x16, 0xa4, 0xe6, 0x17,
-	0xe4, 0xa4, 0xc6, 0xa7, 0xa4, 0xe6, 0x26, 0xe6, 0xa5, 0x48, 0x30, 0x2a, 0x30, 0x6a, 0x70, 0x06,
-	0xf1, 0x40, 0x04, 0x5d, 0xc0, 0x62, 0x4a, 0x96, 0x5c, 0x02, 0x08, 0x7d, 0xc5, 0x05, 0xf9, 0x79,
-	0xc5, 0xa9, 0x42, 0xaa, 0x5c, 0x7c, 0x25, 0x20, 0x81, 0xf8, 0x22, 0xa8, 0x08, 0x54, 0x27, 0x6f,
-	0x09, 0xb2, 0x32, 0xa3, 0x4b, 0x8c, 0x5c, 0xdc, 0x21, 0x40, 0x8b, 0x82, 0x81, 0xee, 0xce, 0x4c,
-	0x4e, 0x15, 0xf2, 0xe4, 0xe2, 0x71, 0x2c, 0x2d, 0xc9, 0x48, 0xcd, 0x2b, 0xc9, 0x4c, 0x4e, 0x2c,
-	0x49, 0x15, 0x92, 0x82, 0x38, 0x4d, 0x0f, 0x59, 0x10, 0xea, 0x36, 0x29, 0x69, 0xac, 0x72, 0x10,
-	0x83, 0x95, 0x18, 0x84, 0x0c, 0xb9, 0x58, 0x02, 0x32, 0xf3, 0xd2, 0x85, 0x84, 0xa0, 0xca, 0x40,
-	0x1c, 0x98, 0x56, 0x61, 0x14, 0x31, 0xb8, 0x16, 0x57, 0x2e, 0x0e, 0x98, 0x47, 0x60, 0x36, 0x83,
-	0x42, 0x4f, 0x0f, 0x2d, 0x54, 0x60, 0x36, 0xa3, 0xc9, 0xc1, 0x8c, 0x49, 0x62, 0x03, 0xcb, 0x1a,
-	0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0x1e, 0x26, 0x84, 0x38, 0x92, 0x01, 0x00, 0x00,
 }
