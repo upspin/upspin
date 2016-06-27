8941fb918a28eba780aee2cb6ba893b1b1272a6f
Eduardo Pinheiro <edpin@google.com>
2016-06-27 09:57:41 -0700

    storage: add registration and configuration
    - Simplifies service configuration.
    - In preparation for adding new backend types (mySQL, Postgres, Mongo, etc).
    - Isolates backends that are not necessary from being included (GCS is
      now its own package.
    
    All tests pass. No new lint or vet errors.
    
    Change-Id: Id0869f4e71ba7c4bedc490cecb439d6cd6a2c542
    Reviewed-on: https://upspin-review.git.corp.google.com/2444
    Reviewed-by: David Presotto <p@google.com>

--- cloud/storage/storage.go
+++ cloud/storage/storage.go
@@ -6,6 +6,14 @@
 // stable storage such as a database.
 package storage
 
+import (
+	"strings"
+	"time"
+
+	"upspin.io/errors"
+	"upspin.io/upspin"
+)
+
 // Storage is a low-level storage interface for services to store their data permanently.
 type Storage interface {
 	// PutLocalFile copies a local file to storage using ref as its
@@ -38,10 +46,96 @@ type Storage interface {
 	// with a ref.
 	Delete(ref string) error
 
-	// Connect connects with the storage backend. It must be called only once.
-	Connect() error
+	// Dial dials the storage backend with servers and options as given by opts.
+	// It is called only once, but not directly, use storage.Dial instead.
+	Dial(opts *Opts) error
 
-	// Disconnect disconnects from the storage backend.
-	// It must be called only once and only after Connect has succeeded.
-	Disconnect()
+	// Close closes the the connection to the storage backend and releases all resources used.
+	// It must be called only once and only after storage.Dial has succeeded.
+	Close()
+}
+
+var registration = make(map[string]Storage)
+
+// Opts holds configuration options for the storage backend.
+// It is meant to be used by implementations of Storage.
+type Opts struct {
+	Opts    map[string]string // key-value pair
+	Timeout time.Duration
+	Addrs   []upspin.NetAddr
+}
+
+// DialOpts is a daisy-chaining mechanism for setting options to a backend during Dial.
+type DialOpts func(*Opts) error
+
+// Register registers a new Storage under a name. It is typically used in init functions.
+func Register(name string, storage Storage) error {
+	if _, exists := registration[name]; exists {
+		return errors.E("Register", errors.Exist)
+	}
+	registration[name] = storage
+	return nil
+}
+
+// WithNetAddr sets a network host:port pair as the network address to dial.
+// Multiple calls can be made to register a pool of servers.
+func WithNetAddr(netAddr upspin.NetAddr) DialOpts {
+	return func(o *Opts) error {
+		o.Addrs = append(o.Addrs, netAddr)
+		return nil
+	}
+}
+
+// WithTimeout sets a maximum duration for dialing.
+func WithTimeout(timeout time.Duration) DialOpts {
+	return func(o *Opts) error {
+		o.Timeout = timeout
+		return nil
+	}
+}
+
+// WithOptions parses a string in the format "key1=value1,key2=value2,..." where keys and values
+// are specific to each storage backend. Neither key nor value may contain the characters "," or "=".
+// Use WithKeyValue repeatedly if these characters need to be used.
+func WithOptions(options string) DialOpts {
+	return func(o *Opts) error {
+		pairs := strings.Split(options, ",")
+		for _, p := range pairs {
+			kv := strings.Split(p, "=")
+			if len(kv) != 2 {
+				return errors.E("WithOptions", errors.Syntax, errors.Errorf("error parsing option %s", kv))
+			}
+			o.Opts[kv[0]] = kv[1]
+		}
+		return nil
+	}
+}
+
+// WithKeyValue sets a key-value pair as option. If called multiple times with the same key, the last one wins.
+func WithKeyValue(key, value string) DialOpts {
+	return func(o *Opts) error {
+		o.Opts[key] = value
+		return nil
+	}
+}
+
+// Dial dials the named storage backend using the dial options opts.
+func Dial(name string, opts ...DialOpts) (Storage, error) {
+	s, found := registration[name]
+	if !found {
+		return nil, errors.E("Dial", errors.NotExist, errors.Str("storage backend type not registered"))
+	}
+	dOpts := &Opts{
+		Opts: make(map[string]string),
+	}
+	var err error
+	for _, o := range opts {
+		if o != nil {
+			err = o(dOpts)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return s, s.Dial(dOpts)
 }
