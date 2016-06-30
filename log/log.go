c484f848e8c5b8b7d1bacd0a125c4420c83771bd
Eduardo Pinheiro <edpin@google.com>
2016-06-30 15:35:55 -0700

    log: add flag for setting the log level
    Fixes #13
    
    All tests pass. No new lint or vet errors.
    
    Change-Id: I6b48782d9c796f9b87b87c76acaaeb9794384e87

--- log/log.go
+++ log/log.go
@@ -10,6 +10,7 @@ package log
 // 2) it mimics Go's log package and can be used as a drop-in replacement for it.
 
 import (
+	"fmt"
 	goLog "log"
 	"os"
 
@@ -36,125 +37,139 @@ type Logger interface {
 	Fatalf(format string, v ...interface{})
 }
 
-// Level is the level of logging.
-type Level int
+// level represents the level of logging.
+type level int
 
 // Different levels of logging.
 const (
-	Ldebug = Level(logging.Debug)
-	Linfo  = Level(logging.Info)
-	Lerror = Level(logging.Error)
+	debug level = iota
+	info
+	errors
+	disabled
 )
 
 // Pre-allocated Loggers at each logging level.
 var (
-	Debug = newLogger(Ldebug)
-	Info  = newLogger(Linfo)
-	Error = newLogger(Lerror)
+	Debug = newLogger(debug, logging.Debug)
+	Info  = newLogger(info, logging.Info)
+	Error = newLogger(errors, logging.Error)
 
-	currentLevel  Level = Linfo
-	defaultClient *logging.Client
+	currentLevel  level
+	client        *logging.Client
 	defaultLogger Logger = goLog.New(os.Stderr, "", goLog.Ldate|goLog.Ltime|goLog.LUTC|goLog.Lmicroseconds)
 )
 
 type logger struct {
-	level  logging.Level
-	client *logging.Client
+	level      level
+	cloudLevel logging.Level
 }
 
 var _ Logger = (*logger)(nil)
 
-// New creates a new logger at a given level, possibly backed by a Google Cloud Logging instance assigned to a
-// project ID and logName if projectID is not empty and is a valid Google Cloud project ID.
-func New(level Level, projectID, logName string) (Logger, error) {
-	var client *logging.Client
-	var err error
-	if projectID != "" {
-		client, err = newClient(projectID, logName)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return &logger{
-		level:  logging.Level(level),
-		client: client,
-	}, nil
-}
-
 // Printf writes a formated message to the log.
 func (l *logger) Printf(format string, v ...interface{}) {
-	if l.level < logging.Level(currentLevel) {
+	if l.level < currentLevel {
 		return // Don't log at lower levels.
 	}
-	if l.client != nil {
-		l.client.Logger(l.level).Printf(format, v...)
-	} else if defaultClient != nil {
-		defaultClient.Logger(l.level).Printf(format, v...)
+	if client != nil {
+		client.Logger(l.cloudLevel).Printf(format, v...)
 	}
 	defaultLogger.Printf(format, v...)
 }
 
 // Print writes a message to the log.
 func (l *logger) Print(v ...interface{}) {
-	if l.level < logging.Level(currentLevel) {
+	if l.level < currentLevel {
 		return // Don't log at lower levels.
 	}
-	if l.client != nil {
-		l.client.Logger(l.level).Print(v...)
-	} else if defaultClient != nil {
-		defaultClient.Logger(l.level).Print(v...)
+	if client != nil {
+		client.Logger(l.cloudLevel).Print(v...)
 	}
 	defaultLogger.Print(v...)
 }
 
 // Println writes a line to the log.
 func (l *logger) Println(v ...interface{}) {
-	if l.level < logging.Level(currentLevel) {
+	if l.level < currentLevel {
 		return // Don't log at lower levels.
 	}
-	if l.client != nil {
-		l.client.Logger(l.level).Println(v...)
-	} else if defaultClient != nil {
-		defaultClient.Logger(l.level).Println(v...)
+	if client != nil {
+		client.Logger(l.cloudLevel).Println(v...)
 	}
 	defaultLogger.Println(v...)
 }
 
 // Fatal writes a message to the log and aborts, regardless of the current log level.
 func (l *logger) Fatal(v ...interface{}) {
-	// Fatal always logs.
-	if l.client != nil {
-		l.client.Logger(l.level).Print(v...)
-	} else if defaultClient != nil {
-		defaultClient.Logger(l.level).Print(v...)
+	if client != nil {
+		client.Logger(l.cloudLevel).Print(v...)
 	}
 	defaultLogger.Fatal(v...)
 }
 
 // Fatalf writes a formated message to the log and aborts, regardless of the current log level.
 func (l *logger) Fatalf(format string, v ...interface{}) {
-	// Fatalf always logs.
-	if l.client != nil {
-		l.client.Logger(l.level).Printf(format, v...)
-	} else if defaultClient != nil {
-		defaultClient.Logger(l.level).Printf(format, v...)
+	if client != nil {
+		client.Logger(l.cloudLevel).Printf(format, v...)
 	}
 	defaultLogger.Fatalf(format, v...)
 }
 
-// SetLevel sets the current logging level. Lower levels than current will not be logged.
-func SetLevel(level Level) {
-	currentLevel = level
+// String returns the name of the logger.
+func (l *logger) String() string {
+	return toString(l.level)
 }
 
-// CurrentLevel returns the current logging level.
-func CurrentLevel() Level {
-	return currentLevel
+func toString(level level) string {
+	switch level {
+	case info:
+		return "info"
+	case debug:
+		return "debug"
+	case errors:
+		return "error"
+	case disabled:
+		return "disabled"
+	}
+	return "unknown"
+}
+
+// Level returns the level of logging currently.
+func Level() string {
+	return toString(currentLevel)
+}
+
+func toLevel(level string) (level, error) {
+	switch level {
+	case "info":
+		return info, nil
+	case "debug":
+		return debug, nil
+	case "error":
+		return errors, nil
+	case "disabled":
+		return disabled, nil
+	}
+	return disabled, fmt.Errorf("invalid log level %q", level)
+}
+
+// SetLevel sets the current level of logging.
+func SetLevel(level string) error {
+	l, err := toLevel(level)
+	if err != nil {
+		return err
+	}
+	currentLevel = l
+	return nil
 }
 
 // At returns whether the level will be logged currently.
-func At(level Level) bool {
-	return currentLevel <= level
+func At(level string) bool {
+	l, err := toLevel(level)
+	if err != nil {
+		return false
+	}
+	return currentLevel <= l
 }
 
 // Printf writes a formated message to the log.
@@ -186,7 +201,7 @@ func Fatalf(format string, v ...interface{}) {
 // instance writing to a given logName.
 func Connect(projectID, logName string) error {
 	var err error
-	defaultClient, err = newClient(projectID, logName)
+	client, err = newClient(projectID, logName)
 	if err != nil {
 		return err
 	}
@@ -203,8 +218,9 @@ func newClient(projectID, logName string) (*logging.Client, error) {
 }
 
 // newLogger instantiates an implicit Logger using the default client.
-func newLogger(level Level) Logger {
+func newLogger(level level, cloudLevel logging.Level) Logger {
 	return &logger{
-		level: logging.Level(level),
+		level:      level,
+		cloudLevel: cloudLevel,
 	}
 }
