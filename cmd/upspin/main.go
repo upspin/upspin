a61f2b9394a435aaebfc71d761d33d9555a829ac
[Eric Grosse] <[ehg@google.com]>
2016-06-30 13:23:08 -0700

    user/gcp: Add debug flag to upspin, logging to gcp configuration.
    Reverted change to gcp.go, but will keep
    the debug flag in upspin cmd until edpin generalizes.
    
    Change-Id: I96e9106036d128f46b86150db77c38e88c17be67
    Reviewed-on: https://upspin-review.git.corp.google.com/2541
    Reviewed-by: Rob Pike <r@google.com>
    Reviewed-by: Eduardo Pinheiro <edpin@google.com>

--- cmd/upspin/main.go
+++ cmd/upspin/main.go
@@ -15,6 +15,7 @@ import (
 	"upspin.io/bind"
 	"upspin.io/client"
 	"upspin.io/context"
+	"upspin.io/log"
 	"upspin.io/path"
 	"upspin.io/upspin"
 
@@ -30,11 +31,19 @@ import (
 	_ "upspin.io/user/transports"
 )
 
-var op string // The subcommand we are running.
+var (
+	op        string // The subcommand we are running.
+	debugFlag = flag.Bool("d", false, "turn on fine-grain debugging")
+)
 
 func main() {
 	flag.Usage = usage
 	flag.Parse()
+
+	if *debugFlag {
+		log.SetLevel(log.Ldebug)
+		// TODO(edpin)  Apply to all commands.
+	}
 
 	if len(flag.Args()) < 1 {
 		usage()
