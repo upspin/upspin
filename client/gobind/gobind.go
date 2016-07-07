e8eb9fe949ed595d78098e8806e3f134bcede222
Eduardo Pinheiro <edpin@google.com>
2016-07-07 11:59:56 -0700

    client/gobind: add Go bindings for the Client
    - Adds a wrapped client and utility functions for configuring
    and creating a Client for other languages to use, most notably
    Java for Android.
    
    Notes:
    1) Just declaring a struct does not mean it will be
    instantiatable in Java. We need to create the New funcs too.
    2) Slices of any kind except byte cannot be returned, even
    inside structs in the current implementation of gomobile.
    3) We cannot export any upspin.* type since the upspin package
    contains uint64, which Java can't handle.
    4) This is WIP, only read-only methods of Client are exported
    for now. And only Remote is supported (likely the only we'll ever
    need for a mobile client, but perhaps we'll need others for
    other types of uses that are not mobile). TBD
    
    Tested with a Java mobile client for Android.
    
    Change-Id: Ic89fff0107e285fdde248d3015ebd7951ce3554e

---
+++ client/gobind/gobind.go
@@ -0,0 +1,132 @@
+// Package gobind provides experimental go bindings for a simplified
+// upspin Client and related data structures in a such a way that languages
+// such as Java and Objective-C can handle and gomobile can export.
+// Currently, gomobile cannot export slices other than byte, so slices are
+// converted to linked lists when necessary. Unsigned types are not supported
+// either, so they're converted to their signed respectives and truncations
+// are silently ignored.
+// This package is experimental and is NOT an official upspin.Client
+// implementation. Its definition may change or break without warning.
+package gobind
+
+// To regenerate the .aar archive for Android Java, run:
+//	go generate
+
+//go:generate gomobile bind -target android upspin.io/client/gobind
+
+import (
+	"upspin.io/client"
+	"upspin.io/context"
+	"upspin.io/factotum"
+	"upspin.io/log"
+	"upspin.io/upspin"
+
+	// Load everything we need.
+	_ "upspin.io/directory/transports"
+	_ "upspin.io/pack/ee"
+	_ "upspin.io/pack/plain"
+	_ "upspin.io/store/transports"
+	_ "upspin.io/user/transports"
+)
+
+// DirEntry represents the most relevant pieces of an upspin.DirEntry for clients.
+type DirEntry struct {
+	Name         string
+	IsDir        bool
+	Size         int64
+	LastModified int64
+	Writer       string
+	Next         *DirEntry
+}
+
+// ClientConfig is a setup data structure that configures the Client
+// for a given user with keys and server endpoints.
+type ClientConfig struct {
+	// UserName is the upspin.UserName.
+	UserName string
+
+	// PublicKey is the user's upspin.PublicKey.
+	PublicKey string
+
+	// PrivateKey is the user's private key.
+	PrivateKey string
+
+	// UserNetAddr is the upspin.NetAddr of an upspin.Remote User endpoint.
+	UserNetAddr string
+
+	// StoreNetAddr is the upspin.NetAddr of an upspin.Remote Store endpoint.
+	StoreNetAddr string
+
+	// DirNetAddr is the upspin.NetAddr of an upspin.Remote Directory endpoint.
+	DirNetAddr string
+}
+
+// NewClientConfig returns a new ClientConfig.
+func NewClientConfig() *ClientConfig {
+	return new(ClientConfig)
+}
+
+// Client is a wrapped upspin.Client.
+type Client struct {
+	c upspin.Client
+}
+
+// Glob returns a linked list of DirEntry listing the results of the Glob operation.
+func (c *Client) Glob(pattern string) (*DirEntry, error) {
+	des, err := c.c.Glob(pattern)
+	if err != nil {
+		return nil, err
+	}
+	var first *DirEntry
+	var last *DirEntry
+	for i, de := range des {
+		dirEntry := &DirEntry{
+			Name:         string(de.Name),
+			IsDir:        de.IsDir(),
+			Size:         int64(de.Metadata.Size),
+			LastModified: int64(de.Metadata.Time),
+			Writer:       string(de.Metadata.Writer),
+		}
+		if i > 0 {
+			last.Next = dirEntry
+		} else {
+			first = dirEntry
+		}
+		last = dirEntry
+	}
+	return first, nil
+}
+
+// Get returns the contents of a path.
+func (c *Client) Get(path string) ([]byte, error) {
+	return c.c.Get(upspin.PathName(path))
+}
+
+// NewClient returns a new Client for a given user's configuration.
+func NewClient(config *ClientConfig) (*Client, error) {
+	ctx := context.New().SetUserName(upspin.UserName(config.UserName)).SetPacking(upspin.EEPack)
+	f, err := factotum.New(upspin.PublicKey(config.PublicKey), config.PrivateKey)
+	if err != nil {
+		log.Error.Printf("Error creating factotum: %s", err)
+		return nil, err
+	}
+	ctx.SetFactotum(f)
+	se := upspin.Endpoint{
+		Transport: upspin.Remote,
+		NetAddr:   upspin.NetAddr(config.StoreNetAddr),
+	}
+	ctx.SetStoreEndpoint(se)
+	de := upspin.Endpoint{
+		Transport: upspin.Remote,
+		NetAddr:   upspin.NetAddr(config.DirNetAddr),
+	}
+	ctx.SetDirectoryEndpoint(de)
+	ue := upspin.Endpoint{
+		Transport: upspin.Remote,
+		NetAddr:   upspin.NetAddr(config.UserNetAddr),
+	}
+	ctx.SetUserEndpoint(ue)
+	return &Client{
+		c: client.New(ctx),
+	}, nil
+}
