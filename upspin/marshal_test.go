973946f3793f89150d1ded84e36af1b4335be228
Rob Pike <r@golang.org>
2016-07-25 13:53:18 +1000

    upspin: update to new multiblock DirEntry structure
    Also add new DirBlock structure, and its marshaling and unmarshaling routines.
    
    Remove unused types Packdata and Metadata.
    
    The Packer interface is unchanged but needs to be redone soon.
    We can hold on to it for now, as long as we use only PlainPack.
    (But PlainPack will need fixing, not done here.)
    
    This will break the build.
    
    Change-Id: I2438994cd9105f29684277952cacceb9358d0459
    Reviewed-on: https://upspin-review.googlesource.com/2711
    Reviewed-by: Andrew Gerrand <adg@google.com>

--- upspin/marshal_test.go
+++ upspin/marshal_test.go
@@ -10,21 +10,44 @@ import (
 )
 
 var dirEnt = DirEntry{
-	Name: "u@foo.com/a/directory", // so IsDir is not the zero value.
+	Name:    "u@foo.com/a/directory",
+	Packing: EEPack,
+	Time:    123456,
+	Blocks: []DirBlock{
+		dirBlock1,
+		dirBlock2,
+	},
+	WrappedKeys: []byte{1, 2, 3, 4},
+	Signature:   []byte{5, 6, 7, 8, 9},
+	Attr:        AttrDirectory, // Just so it's not zero; this is not a semantically valid entry.
+	Sequence:    1234,
+	Writer:      "u@foo.com",
+}
+
+var dirBlock1 = DirBlock{
 	Location: Location{
 		Endpoint: Endpoint{
-			Transport: GCP,
+			Transport: Remote,
 			NetAddr:   "foo.com:1234",
 		},
-		Reference: "Chubb",
+		Reference: "Cinder",
 	},
-	Metadata: Metadata{
-		Attr:     AttrDirectory,
-		Sequence: 1234,
-		Size:     12345,
-		Time:     123456,
-		Packdata: []byte{1, 2, 3},
+	Offset:    0,
+	Size:      1024,
+	Signature: []byte("sign"),
+}
+
+var dirBlock2 = DirBlock{
+	Location: Location{
+		Endpoint: Endpoint{
+			Transport: InProcess,
+			NetAddr:   "foo.com:1234",
+		},
+		Reference: "Ice",
 	},
+	Offset:    1024,
+	Size:      4096,
+	Signature: []byte("nature"),
 }
 
 func TestDirEntMarshal(t *testing.T) {
@@ -43,6 +66,27 @@ func TestDirEntMarshal(t *testing.T) {
 	if !reflect.DeepEqual(&dirEnt, &new) {
 		t.Errorf("bad result. expected:")
 		t.Errorf("%+v\n", &dirEnt)
+		t.Errorf("got:")
+		t.Errorf("%+v\n", &new)
+	}
+}
+
+func TestDirBlockMarshal(t *testing.T) {
+	data, err := dirBlock1.Marshal()
+	if err != nil {
+		t.Fatalf("Marshal: %v", err)
+	}
+	var new DirBlock
+	remaining, err := new.Unmarshal(data)
+	if err != nil {
+		t.Fatalf("Unmarshal: %v", err)
+	}
+	if len(remaining) != 0 {
+		t.Errorf("data remains after unmarshal")
+	}
+	if !reflect.DeepEqual(&dirBlock1, &new) {
+		t.Errorf("bad result. expected:")
+		t.Errorf("%+v\n", &dirBlock1)
 		t.Errorf("got:")
 		t.Errorf("%+v\n", &new)
 	}
