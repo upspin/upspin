6e95001e0172118380924d0754f02f1deef9d93e
Andrew Gerrand <adg@golang.org>
2016-07-27 06:23:27 +1000

    upspin, pack/*: change packer interface to streaming model
    This introduces a new Packer interface that supports writing
    and reading data per-block.
    
    Change-Id: Ie5e7a663d41bfed20557af9f6dd3c483371b9ae1
    Reviewed-on: https://upspin-review.googlesource.com/2730
    Reviewed-by: Rob Pike <r@google.com>

--- pack/ee/ee.go
+++ pack/ee/ee.go
@@ -98,14 +98,14 @@ func (ee ee) Packing() upspin.Packing {
 }
 
 func (ee ee) PackLen(ctx upspin.Context, cleartext []byte, d *upspin.DirEntry) int {
-	if err := pack.CheckPackMeta(ee, &d.Metadata); err != nil {
+	if err := pack.CheckPacking(ee, d); err != nil {
 		return -1
 	}
 	return len(cleartext)
 }
 
 func (ee ee) UnpackLen(ctx upspin.Context, ciphertext []byte, d *upspin.DirEntry) int {
-	if err := pack.CheckUnpackMeta(ee, &d.Metadata); err != nil {
+	if err := pack.CheckPacking(ee, d); err != nil {
 		return -1
 	}
 	return len(ciphertext)
@@ -115,50 +115,130 @@ func (ee ee) String() string {
 	return "ee"
 }
 
-func (ee ee) Pack(ctx upspin.Context, ciphertext, cleartext []byte, d *upspin.DirEntry) (int, error) {
+func (ee ee) Pack(ctx upspin.Context, d *upspin.DirEntry) (upspin.BlockPacker, error) {
 	const Pack = "Pack"
-	if err := pack.CheckPackMeta(ee, &d.Metadata); err != nil {
-		return 0, errors.E(Pack, errors.Invalid, d.Name, err)
+	if err := pack.CheckPacking(ee, d); err != nil {
+		return nil, errors.E(Pack, errors.Invalid, d.Name, err)
 	}
-	if len(ciphertext) < len(cleartext) {
-		return 0, errors.E(Pack, errors.Invalid, d.Name, errTooShort)
-	}
-	ciphertext = ciphertext[:len(cleartext)]
+
+	// TODO(adg): support append; for now assume a new file.
+	d.Blocks = nil
 
 	// Pick fresh file encryption key.
 	dkey := make([]byte, aesKeyLen)
 	_, err := rand.Read(dkey)
 	if err != nil {
-		return 0, errors.E(Pack, d.Name, err)
+		return nil, errors.E(Pack, d.Name, err)
 	}
-	cipherLen, err := ee.encrypt(ciphertext, cleartext, dkey)
+
+	// Set up the stream cipher.
+	if len(dkey) != aesKeyLen {
+		return nil, errors.E(Pack, errKeyLength)
+	}
+	block, err := aes.NewCipher(dkey)
 	if err != nil {
-		return 0, errors.E(Pack, d.Name, err)
+		return nil, errors.E(Pack, err)
+	}
+	iv := make([]byte, aes.BlockSize)
+	// iv=0 is ok because we're certain that dkey is random and not reused
+	stream := cipher.NewCTR(block, iv)
+
+	return &blockPacker{
+		ctx:    ctx,
+		entry:  d,
+		dkey:   dkey,
+		cipher: stream,
+	}, nil
+}
+
+type blockPacker struct {
+	ctx    upspin.Context
+	entry  *upspin.DirEntry
+	dkey   []byte
+	cipher cipher.Stream
+
+	buf []byte // re-used by Pack
+}
+
+// checkPreviousLocation checks whether the previous block's Location field
+// was set, to prevent misuse of the API.
+func (bp *blockPacker) checkPreviousLocation() error {
+	if bs := bp.entry.Blocks; len(bs) > 0 {
+		if i := len(bs) - 1; bs[i].Location == (upspin.Location{}) {
+			return errors.E("Pack", bp.entry.Name, errors.Errorf("location not set for block %v", i))
+		}
+	}
+	return nil
+}
+
+func (bp *blockPacker) Pack(cleartext []byte) (ciphertext []byte, err error) {
+	if err := bp.checkPreviousLocation(); err != nil {
+		return nil, err
+	}
+
+	// (re-)allocate shared buffer if necessary.
+	if bp.buf == nil || len(bp.buf) < len(cleartext) {
+		bp.buf = make([]byte, len(cleartext))
+	}
+	ciphertext = bp.buf[:len(cleartext)]
+
+	// Encrypt.
+	bp.cipher.XORKeyStream(ciphertext, cleartext)
+
+	// Compute size, offset, and checksum.
+	size := int64(len(ciphertext))
+	offs := int64(0)
+	if bs := bp.entry.Blocks; len(bs) > 0 {
+		offs = bs[len(bs)-1].Offset + size
 	}
 	b := sha256.Sum256(ciphertext)
-	cipherSum := b[:]
+	sum := b[:]
 
-	// Sign ciphertext.
-	sig, err := ctx.Factotum().FileSign(path.Clean(d.Name), d.Metadata.Time, dkey, cipherSum)
-	if err != nil {
-		return 0, errors.E(Pack, d.Name, err)
+	// Create and append new DirBlock record.
+	block := upspin.DirBlock{
+		Size:     size,
+		Offset:   offs,
+		Packdata: sum,
+	}
+	bp.entry.Blocks = append(bp.entry.Blocks, block)
+
+	return ciphertext, nil
+}
+
+func (bp *blockPacker) SetLocation(l upspin.Location) {
+	bs := bp.entry.Blocks
+	bs[len(bs)-1].Location = l
+}
+
+func (bp *blockPacker) Close() error {
+	// Zero out encryption key when we're done.
+	defer zeroSlice(&bp.dkey)
+
+	const Pack = "Pack"
+	if err := bp.checkPreviousLocation(); err != nil {
+		return err
 	}
 
-	// Wrap for myself.
+	name := bp.entry.Name
+	ctx := bp.ctx
+
+	// Wrap keys.
 	wrap := make([]wrappedKey, 2)
+
+	// First, wrap for myself.
 	p, _, err := factotum.ParsePublicKey(ctx.Factotum().PublicKey())
 	if err != nil {
-		return 0, errors.E(Pack, d.Name, err)
+		return errors.E(Pack, name, err)
 	}
-	wrap[0], err = ee.aesWrap(p, dkey)
+	wrap[0], err = aesWrap(p, bp.dkey)
 	if err != nil {
-		return 0, errors.E(Pack, d.Name, err)
+		return errors.E(Pack, name, err)
 	}
 
 	// Also wrap for owner, if different.
-	parsed, err := path.Parse(d.Name)
+	parsed, err := path.Parse(name)
 	if err != nil {
-		return 0, errors.E(Pack, d.Name, err)
+		return errors.E(Pack, name, err)
 	}
 	owner := parsed.User()
 	if owner == ctx.UserName() {
@@ -166,11 +246,11 @@ func (ee ee) Pack(ctx upspin.Context, ciphertext, cleartext []byte, d *upspin.Di
 	} else {
 		userConn, err := bind.KeyServer(ctx, ctx.KeyEndpoint())
 		if err != nil {
-			return 0, errors.E(Pack, owner, err)
+			return errors.E(Pack, name, owner, err)
 		}
 		u, err := userConn.Lookup(owner)
 		if err != nil {
-			return 0, errors.E(Pack, owner, err)
+			return errors.E(Pack, name, owner, err)
 		}
 		ownerKey := u.PublicKey
 		if ownerKey == ctx.Factotum().PublicKey() {
@@ -179,91 +259,155 @@ func (ee ee) Pack(ctx upspin.Context, ciphertext, cleartext []byte, d *upspin.Di
 		} else {
 			p, _, err = factotum.ParsePublicKey(ownerKey)
 			if err != nil {
-				return 0, errors.E(Pack, d.Name, err)
+				return errors.E(Pack, name, owner, err)
 			}
-			wrap[1], err = ee.aesWrap(p, dkey)
+			wrap[1], err = aesWrap(p, bp.dkey)
 			if err != nil {
-				return 0, errors.E(Pack, d.Name, err)
+				return errors.E(Pack, name, owner, err)
 			}
 		}
 	}
 
-	// Serialize packer metadata.
-	err = ee.pdMarshal(&d.Metadata.Packdata, sig, sig0, wrap, cipherSum)
+	// Compute checksum of block hashes.
+	sum := blockSum(bp.entry.Blocks)
+
+	// Compute entry signature.
+	sig, err := ctx.Factotum().FileSign(path.Clean(name), bp.entry.Time, bp.dkey, sum)
 	if err != nil {
-		return 0, errors.E(Pack, d.Name, err)
+		return errors.E("Pack", err)
 	}
-	return cipherLen, err
+
+	return pdMarshal(&bp.entry.Packdata, sig, upspin.Signature{}, wrap, sum)
 }
 
-func (ee ee) Unpack(ctx upspin.Context, cleartext, ciphertext []byte, d *upspin.DirEntry) (int, error) {
+func blockSum(bs []upspin.DirBlock) []byte {
+	hash := sha256.New()
+	for i := range bs {
+		hash.Write(bs[i].Packdata)
+	}
+	return hash.Sum(nil)
+}
+
+func (ee ee) Unpack(ctx upspin.Context, d *upspin.DirEntry) (upspin.BlockUnpacker, error) {
 	const Unpack = "Unpack"
-	if err := pack.CheckUnpackMeta(ee, &d.Metadata); err != nil {
-		return 0, errors.E(Unpack, errors.Invalid, d.Name, err)
+	if err := pack.CheckPacking(ee, d); err != nil {
+		return nil, errors.E(Unpack, errors.Invalid, d.Name, err)
 	}
-	if len(cleartext) < len(ciphertext) {
-		return 0, errors.E(Unpack, errors.Invalid, d.Name, errTooShort)
-	}
-	cleartext = cleartext[:len(ciphertext)]
 
-	// Retrieve file decryption key.
-	dkey := make([]byte, aesKeyLen)
-	sig, sig2, wrap, _, err := ee.pdUnmarshal(d.Metadata.Packdata)
+	sig, sig2, wrap, hash, err := pdUnmarshal(d.Packdata)
 	if err != nil {
-		return 0, errors.E(Unpack, d.Name, err)
+		return nil, errors.E(Unpack, d.Name, err)
 	}
 
-	writer := d.Metadata.Writer
+	// Check that our stored+signed block checksum matches the sum of the actual blocks.
+	if got, want := blockSum(d.Blocks), hash; !bytes.Equal(got, want) {
+		return nil, errors.E(Unpack, d.Name, errors.Str("checksum mismatch"))
+	}
+
+	// Fetch writer public key.
+	writer := d.Writer
 	if len(writer) == 0 {
-		return 0, errors.E(Unpack, d.Name, errWriter)
+		return nil, errors.E(Unpack, d.Name, errWriter)
 	}
 	writerRawPubKey, err := publicKey(ctx, writer)
 	if err != nil {
-		return 0, errors.E(Unpack, writer, err)
+		return nil, errors.E(Unpack, writer, err)
 	}
 	writerPubKey, writerCurveName, err := factotum.ParsePublicKey(writerRawPubKey)
 	if err != nil {
-		return 0, errors.E(Unpack, writer, err)
+		return nil, errors.E(Unpack, writer, err)
 	}
 
-	// Now get my own keys
-	me := ctx.UserName() // Recipient of the file is me (the user in the context)
+	// Fetch my own keys, as I am the recipient of the file.
+	me := ctx.UserName()
 	rawPublicKey, err := publicKey(ctx, me)
 	if err != nil {
-		return 0, errors.E(Unpack, d.Name, err)
+		return nil, errors.E(Unpack, d.Name, err)
 	}
 
+	// Pull the decryption key out of the wrapped keys.
+	dkey := make([]byte, aesKeyLen)
 	// For quick lookup, hash my public key and locate my wrapped key in the metadata.
 	rhash := factotum.KeyHash(rawPublicKey)
-	b := sha256.Sum256(ciphertext)
-	cipherSum := b[:]
 	for _, w := range wrap {
 		if !bytes.Equal(rhash, w.keyHash) {
 			continue
 		}
-		// Decode my wrapped key using my private key
+		// Decode my wrapped key using my private key.
 		dkey, err = ee.aesUnwrap(ctx.Factotum(), w)
 		if err != nil {
-			log.Printf("unwrap failed: %v", err)
-			return 0, errors.E(Unpack, d.Name, err)
+			return nil, errors.E(Unpack, d.Name, me, err)
+		}
+		if len(dkey) != aesKeyLen {
+			return nil, errors.E(Unpack, d.Name, errKeyLength)
 		}
 		// Verify that this was signed with the writer's old or new public key.
-		vhash := factotum.VerHash(writerCurveName, path.Clean(d.Name), d.Metadata.Time, dkey, cipherSum)
+		vhash := factotum.VerHash(writerCurveName, path.Clean(d.Name), d.Time, dkey, hash)
 		if !ecdsa.Verify(writerPubKey, vhash, sig.R, sig.S) &&
 			sig2.R.Sign() != 0 && !ecdsa.Verify(writerPubKey, vhash, sig2.R, sig2.S) {
 			// Only check sig2 if non-zero and sig failed, likely because writerPubKey is rotating.
-			log.Println("verify failed")
-			return 0, errors.E(Unpack, d.Name, errVerify)
+			return nil, errors.E(Unpack, d.Name, writer, errVerify)
 		}
-		// dkey is safe, so we decrypt the whole blob.
-		return ee.decrypt(cleartext, ciphertext, dkey)
+		// Set up stream cipher.
+		block, err := aes.NewCipher(dkey)
+		if err != nil {
+			return nil, errors.E(Unpack, d.Name, err)
+		}
+		iv := make([]byte, aes.BlockSize)
+		stream := cipher.NewCTR(block, iv)
+		// We're OK to start decrypting blocks.
+		return &blockUnpacker{
+			ctx:    ctx,
+			entry:  d,
+			cipher: stream,
+			block:  -1,
+		}, nil
 	}
-	return 0, errors.E(Unpack, d.Name, errNoWrappedKey)
+	return nil, errors.E(Unpack, d.Name, me, errors.Str("could not find wrapped key"))
+}
+
+type blockUnpacker struct {
+	ctx    upspin.Context
+	entry  *upspin.DirEntry
+	block  int // index into entry.Blocks
+	cipher cipher.Stream
+
+	buf []byte // re-used by Unpack
+}
+
+func (bp *blockUnpacker) NextBlock() (upspin.DirBlock, bool) {
+	bp.block++
+	bs := bp.entry.Blocks
+	if bp.block >= len(bs) {
+		return upspin.DirBlock{}, false
+	}
+	b := bs[bp.block]
+	return b, true
+}
+
+func (bp *blockUnpacker) Unpack(ciphertext []byte) (cleartext []byte, err error) {
+	// Validate checksum.
+	b := sha256.Sum256(ciphertext)
+	sum := b[:]
+	if got, want := sum, bp.entry.Blocks[bp.block].Packdata; !bytes.Equal(got, want) {
+		return nil, errors.E("Unpack", bp.entry.Name, errors.Str("checksum mismatch"))
+	}
+
+	// (re-)allocate shared buffer if necessary.
+	if bp.buf == nil || len(bp.buf) < len(ciphertext) {
+		bp.buf = make([]byte, len(ciphertext))
+	}
+	cleartext = bp.buf[:len(ciphertext)]
+
+	// Decrypt.
+	bp.cipher.XORKeyStream(cleartext, ciphertext)
+
+	return cleartext, nil
 }
 
 // ReaderHashes returns SHA-256 hashes of the public keys able to decrypt the associated ciphertext.
 func (ee ee) ReaderHashes(packdata []byte) (readers [][]byte, err error) {
-	_, _, wrap, _, err := ee.pdUnmarshal(packdata)
+	_, _, wrap, _, err := pdUnmarshal(packdata)
 	if err != nil {
 		return nil, errors.E("ReaderHashes", errors.Invalid, err)
 	}
@@ -302,7 +446,7 @@ func (ee ee) Share(ctx upspin.Context, readers []upspin.PublicKey, packdata []*[
 		// Extract dkey and existing wrapped keys from packdata.
 		var dkey []byte
 		alreadyWrapped := make(map[keyHashArray]*wrappedKey)
-		sig, sig2, wrap, cipherSum, err := ee.pdUnmarshal(*d)
+		sig, sig2, wrap, cipherSum, err := pdUnmarshal(*d)
 		if err != nil {
 			log.Printf("eePack: pdUnmarshal failed in Share: %v", err)
 			for jj := j; j < len(packdata); jj++ {
@@ -338,7 +482,7 @@ func (ee ee) Share(ctx upspin.Context, readers []upspin.PublicKey, packdata []*[
 			}
 			pw, ok := alreadyWrapped[hash[i]]
 			if !ok { // then need to wrap
-				w, err := ee.aesWrap(pubkey[i], dkey)
+				w, err := aesWrap(pubkey[i], dkey)
 				if err != nil {
 					continue
 				}
@@ -353,8 +497,8 @@ func (ee ee) Share(ctx upspin.Context, readers []upspin.PublicKey, packdata []*[
 		wrap = wrap[:nwrap]
 
 		// Rebuild packdata[j] from existing sig and new wrapped keys.
-		dst := make([]byte, ee.packdataLen(nwrap))
-		if ee.pdMarshal(&dst, sig, sig2, wrap, cipherSum) != nil {
+		dst := make([]byte, packdataLen(nwrap))
+		if pdMarshal(&dst, sig, sig2, wrap, cipherSum) != nil {
 			packdata[j] = nil // Tell caller this packdata was skipped.
 		} else {
 			*packdata[j] = dst
@@ -368,12 +512,12 @@ func (ee ee) Name(ctx upspin.Context, d *upspin.DirEntry, newName upspin.PathNam
 	if d.IsDir() {
 		return errors.E(Name, d.Name, errors.IsDir, "cannot rename directory")
 	}
-	if err := pack.CheckUnpackMeta(ee, &d.Metadata); err != nil {
+	if err := pack.CheckPacking(ee, d); err != nil {
 		return errors.E(Name, errors.Invalid, d.Name, err)
 	}
 
 	dkey := make([]byte, aesKeyLen)
-	sig, sig2, wrap, cipherSum, err := ee.pdUnmarshal(d.Metadata.Packdata)
+	sig, sig2, wrap, cipherSum, err := pdUnmarshal(d.Packdata)
 	if err != nil {
 		return errors.E(Name, errors.Invalid, d.Name, err)
 	}
@@ -428,7 +572,7 @@ func (ee ee) Name(ctx upspin.Context, d *upspin.DirEntry, newName upspin.PathNam
 	}
 
 	// Verify that this was signed with the owner's old or new public key.
-	vhash := factotum.VerHash(ownerCurveName, path.Clean(d.Name), d.Metadata.Time, dkey, cipherSum)
+	vhash := factotum.VerHash(ownerCurveName, path.Clean(d.Name), d.Time, dkey, cipherSum)
 	if !ecdsa.Verify(ownerPubKey, vhash, sig.R, sig.S) &&
 		sig2.R.Sign() != 0 && !ecdsa.Verify(ownerPubKey, vhash, sig2.R, sig2.S) {
 		// Only check sig2 if non-zero and sig failed, likely because ownerPubKey is rotating.
@@ -447,14 +591,14 @@ func (ee ee) Name(ctx upspin.Context, d *upspin.DirEntry, newName upspin.PathNam
 	}
 
 	// Compute new signature.
-	sig, err = ctx.Factotum().FileSign(newName, d.Metadata.Time, dkey, cipherSum)
+	sig, err = ctx.Factotum().FileSign(newName, d.Time, dkey, cipherSum)
 	if err != nil {
 		return errors.E(Name, d.Name, err)
 	}
 
 	// Serialize packer metadata. We do not reallocate Packdata since the new data
 	// should be the same size or smaller.
-	if err := ee.pdMarshal(&d.Metadata.Packdata, sig, sig0, wrap, cipherSum); err != nil {
+	if err := pdMarshal(&d.Packdata, sig, sig0, wrap, cipherSum); err != nil {
 		return errors.E(Name, d.Name, err)
 	}
 	d.Name = newName
@@ -463,7 +607,7 @@ func (ee ee) Name(ctx upspin.Context, d *upspin.DirEntry, newName upspin.PathNam
 }
 
 // aesWrap implements NIST 800-56Ar2; see also RFC6637 §8.
-func (ee ee) aesWrap(R *ecdsa.PublicKey, dkey []byte) (w wrappedKey, err error) {
+func aesWrap(R *ecdsa.PublicKey, dkey []byte) (w wrappedKey, err error) {
 	// Step 1.  Create shared Diffie-Hellman secret.
 	// v, V=vG  ephemeral key pair
 	// S = vR   shared point
@@ -549,12 +693,13 @@ func (ee ee) aesUnwrap(f upspin.Factotum, w wrappedKey) (dkey []byte, err error)
 	return
 }
 
-func (ee ee) pdMarshal(dst *[]byte, sig, sig2 upspin.Signature, wrap []wrappedKey, cipherSum []byte) error {
+func pdMarshal(dst *[]byte, sig, sig2 upspin.Signature, wrap []wrappedKey, cipherSum []byte) error {
 	// sig2 is a signature with another owner key, to enable smoother key rotation
-	n := ee.packdataLen(len(wrap))
+	n := packdataLen(len(wrap))
 	if len(*dst) < n {
 		*dst = make([]byte, n)
 	}
+	// TODO(adg): drop this from the packdata, it's now in DirEntry.Packing.
 	(*dst)[0] = byte(upspin.EEPack)
 	n = 1
 	n += pdPutBytes((*dst)[n:], sig.R.Bytes())
@@ -575,7 +720,7 @@ func (ee ee) pdMarshal(dst *[]byte, sig, sig2 upspin.Signature, wrap []wrappedKe
 	return nil // err impossible for now but the night is young
 }
 
-func (ee ee) pdUnmarshal(pd []byte) (sig, sig2 upspin.Signature, wrap []wrappedKey, hash []byte, err error) {
+func pdUnmarshal(pd []byte) (sig, sig2 upspin.Signature, wrap []wrappedKey, hash []byte, err error) {
 	if pd[0] != byte(upspin.EEPack) {
 		return sig0, sig0, nil, nil, errors.Errorf("expected packing %d, got %d", upspin.EEPack, pd[0])
 	}
@@ -649,37 +794,8 @@ func pdGetBytes(dst *[]byte, src []byte) int {
 	return k + vlen
 }
 
-func (ee ee) encrypt(ciphertext, cleartext, dkey []byte) (int, error) {
-	if len(dkey) != aesKeyLen {
-		return 0, errKeyLength
-	}
-	block, err := aes.NewCipher(dkey)
-	if err != nil {
-		return 0, err
-	}
-	iv := make([]byte, aes.BlockSize)
-	// iv=0 is ok because we're CERTAIN that dkey is random and not reused
-	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(ciphertext, cleartext)
-	return len(cleartext), nil
-}
-
-func (ee ee) decrypt(cleartext, ciphertext, dkey []byte) (int, error) {
-	if len(dkey) != aesKeyLen {
-		return 0, errKeyLength
-	}
-	block, err := aes.NewCipher(dkey)
-	if err != nil {
-		return 0, err
-	}
-	iv := make([]byte, aes.BlockSize)
-	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(cleartext, ciphertext)
-	return len(ciphertext), nil
-}
-
 // packdataLen returns n big enough for packing, sig.R, sig.S, nwrap, {keyHash, encrypted, nonce, X, y}
-func (ee ee) packdataLen(nwrap int) int {
+func packdataLen(nwrap int) int {
 	return 1 + 2*marshalBufLen + (1+5*nwrap)*binary.MaxVarintLen64 +
 		nwrap*(sha256.Size+(aesKeyLen+gcmTagSize)+gcmStandardNonceSize+2*marshalBufLen) +
 		sha256.Size + 1
@@ -713,4 +829,11 @@ func publicKey(ctx upspin.Context, user upspin.UserName) (upspin.PublicKey, erro
 		return "", errors.E(user, errors.NotExist, errNoKnownKeysForUser)
 	}
 	return u.PublicKey, nil
+}
+
+// zeroSlice replaces the contents of the given slice with zeroes.
+func zeroSlice(b *[]byte) {
+	for i := range *b {
+		(*b)[i] = 0
+	}
 }
