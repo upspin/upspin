d65d8270938dbf37a6f08a6fb4b5f987fedf99c4
Andrew Gerrand <adg@golang.org>
2016-07-27 14:18:22 +1000

    pack: add internal CheckLocationSet helper to validate BlockPacker API
    Change-Id: I26db62f9e76160e8ec1b429ae736e587f9466091

---
+++ pack/internal/checkloc.go
@@ -0,0 +1,21 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package internal
+
+import (
+	"upspin.io/errors"
+	"upspin.io/upspin"
+)
+
+// CheckLocationSet checks whether the previous block's Location field
+// was set, to prevent misuse of the BlockPacker API.
+func CheckLocationSet(d *upspin.DirEntry) error {
+	if bs := d.Blocks; len(bs) > 0 {
+		if i := len(bs) - 1; bs[i].Location == (upspin.Location{}) {
+			return errors.E("Pack", d.Name, errors.Errorf("location not set for block %v", i))
+		}
+	}
+	return nil
+}
