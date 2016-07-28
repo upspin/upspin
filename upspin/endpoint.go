c58ab168c278f4f1f055dab73e325ab6c78cb558
Eduardo Pinheiro <edpin@google.com>
2016-07-28 10:26:29 -0700

    cmd/upspin: pretty print user record
    - JSON treats the transport in endpoint as numeric but we have a parser
      for endpoint that is much more human-friendly.
    - This makes the output look like this:
    {
    	"Name": "edpin@google.com",
    	"Dirs": [
    			"remote,localhost:5581",
    			"remote,dir.upspin.io:443"
    		],
    }
    
    instead of the endpoints being:
    
    	"Dirs":[
    		{"Transport":3,
    		 "NetAddr":"dir.test.upspin.io:443"
    		}
    	]
    
    Also fixed a vet error and added a transport case (we use it, but never
    in string format, hence why this wasn't caught before).
    
    Affected tests pass. No new lint or vet errors.
    
    Change-Id: Icd22eabdd66fe2f55d5d4451f66238ba3ff73d63

--- upspin/endpoint.go
+++ upspin/endpoint.go
@@ -5,6 +5,7 @@
 package upspin
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 )
@@ -25,23 +26,66 @@ func ParseEndpoint(v string) (*Endpoint, error) {
 			return nil, fmt.Errorf("remote endpoint %q requires a netaddr", v)
 		}
 		return &Endpoint{Transport: Remote, NetAddr: NetAddr(elems[1])}, nil
+	case "https":
+		if len(elems) < 2 {
+			return nil, fmt.Errorf("https endpoint %q requires a netaddr", v)
+		}
+		return &Endpoint{Transport: HTTPS, NetAddr: NetAddr(elems[1])}, nil
 	case "unassigned":
 		return &Endpoint{Transport: Unassigned}, nil
 	}
+
 	return nil, fmt.Errorf("unknown transport type in endpoint %q", v)
 }
 
-// String converts an endpoint to a string.
-func (ep Endpoint) String() string {
+// toString converts an endpoint to a string.
+func (ep Endpoint) toString() (string, error) {
 	switch ep.Transport {
 	case GCP:
-		return fmt.Sprintf("gcp,%s", string(ep.NetAddr))
+		return fmt.Sprintf("gcp,%s", string(ep.NetAddr)), nil
 	case InProcess:
-		return "inprocess"
+		return "inprocess", nil
 	case Remote:
-		return fmt.Sprintf("remote,%s", string(ep.NetAddr))
+		return fmt.Sprintf("remote,%s", string(ep.NetAddr)), nil
+	case HTTPS:
+		return fmt.Sprintf("https,%s", string(ep.NetAddr)), nil
 	case Unassigned:
-		return "unassigned"
+		return "unassigned", nil
 	}
-	return fmt.Sprintf("unknown endpoint {%v, %v}", ep.Transport, ep.NetAddr)
+	// Note: can't use errors here.
+	return "", fmt.Errorf("unknown transport {%v, %v}", ep.Transport, ep.NetAddr)
+}
+
+// String converts an endpoint to a string.
+// TODO: possibly remove this everywhere if MarshalJSON is enough.
+func (ep Endpoint) String() string {
+	str, err := ep.toString()
+	if err != nil {
+		return err.Error()
+	}
+	return str
+}
+
+// MarshalJSON implements json.Marshaler.
+func (ep *Endpoint) MarshalJSON() ([]byte, error) {
+	str, err := ep.toString()
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(str)
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (ep *Endpoint) UnmarshalJSON(data []byte) error {
+	var str string
+	err := json.Unmarshal(data, &str)
+	if err != nil {
+		return err
+	}
+	parsedEndPt, err := ParseEndpoint(str)
+	if err != nil {
+		return err
+	}
+	*ep = *parsedEndPt
+	return nil
 }
