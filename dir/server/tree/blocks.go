4f9f4b3b4c74f868bbd718b739635e111229af67
Eduardo Pinheiro <edpin@google.com>
2016-08-04 17:38:58 -0700

    dir/server/merkle: implement Put and Flush
    Adds the following capabilities to the Tree:
    - Create a root and track it (via the log).
    - Put a new dir or file node.
    - Flush
    - Append to the Log.
    - Split blocks into blockSize pieces.
    
    Does not yet implement:
    - Log -- except an in-memory version for testing, which
      mostly validates the interface already.
    - Lookup -- though, it should be easy now since the tree
      loads from Store.
    - Delete.
    - Recover from crashes or poor shutdown.
    - Merging deleted blocks that can fit in blockSize (since
      Delete is not implemented yet).
    
    Adds a test that covers 74% of the file -- all lines except
    error conditions and a few other things like unimplemented
    methods and block splitting.
    
    Note: The root is not just a Reference (or even just a Location).
    The reason it needs to be a DirEntry is because the root can grow
    beyond the block size and thus need multiple Refs to build it.
    This won't make easy backups any more complicated, since backups
    are DirEntries themselves and can contain multiple Refs.
    
    No lint or vet errors.
    
    Change-Id: I8ddab3617c891285734f59a59c71d25ede72d5d5

---
+++ dir/server/tree/blocks.go
@@ -0,0 +1,205 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tree
+
+// This file implements block reading and writing.
+
+import (
+	"upspin.io/bind"
+	"upspin.io/errors"
+	"upspin.io/log"
+	"upspin.io/pack"
+	"upspin.io/path"
+	"upspin.io/upspin"
+)
+
+// TODO: move to package upspin or somewhere else more appropriate.
+const blockSize = 1024 * 1024 // 1MB
+
+// get gets the contents of a location as a blob.
+func (t *tree) get(loc *upspin.Location) ([]byte, error) {
+	const get = "get"
+	store, err := bind.StoreServer(t.context, loc.Endpoint)
+	if err != nil {
+		return nil, err
+	}
+	data, locs, err := store.Get(loc.Reference)
+	if err != nil {
+		return nil, errors.E(get, errors.Errorf("location: %v: %v", loc, err))
+	}
+	if data != nil && len(locs) > 0 {
+		return nil, errors.E(get, errInternalInconsistency)
+	}
+	if data != nil {
+		return data, nil
+	}
+	// TODO: this should do something akin to client.Get. It now only does one indirection.
+	if len(locs) > 0 {
+		return t.get(&locs[0])
+	}
+	// No error, no data, no indirection?
+	return nil, errors.E(get, errors.Errorf("location: %v: %v", loc, errInternalInconsistency))
+}
+
+// store stores a node to the StoreServer. It does not reset the dirty bit.
+// Children of n, if any, must not be dirty.
+func (t *tree) store(n *node) error {
+	const store = "store"
+
+	// Get our store server.
+	storeServer := t.context.StoreServer()
+
+	// Use our preferred packing
+	packer := t.packer
+
+	// Prepare the dirEntry
+	n.entry.Blocks = nil // if any blocks existed, their references are lost as we're packing dirEntry again.
+	n.entry.Packing = t.context.Packing()
+
+	// Start packing.
+	bp, err := packer.Pack(t.context, n.entry)
+	if err != nil {
+		return errors.E(store, err)
+	}
+
+	// Pack and store child nodes, keeping blocks at ~blockSize.
+	var data []byte
+	for _, child := range n.kids {
+		// TODO: also check whether there are any Blocks with empty locations that are non-empty dirs or files.
+		if child.dirty {
+			// We should write nodes from the bottom up, so this should never happen.
+			return errors.E(store, child.entry.Name, errInternalInconsistency)
+		}
+		log.Debug.Printf("%s: Saving child: %s", n.entry.Name, child.entry.Name)
+		block, err := child.entry.Marshal()
+		if err != nil {
+			return errors.E(store, err)
+		}
+		data = append(data, block...)
+
+		// Don't let blocks grow too much (but we never split a large DirEntry in the middle).
+		if len(data) >= blockSize {
+			// Flush now.
+			err = storeBlock(storeServer, bp, data)
+			if err != nil {
+				return errors.E(store, err)
+			}
+			data = data[0:0]
+		}
+	}
+	// Put remaining data.
+	if len(data) > 0 {
+		err = storeBlock(storeServer, bp, data)
+		if err != nil {
+			return errors.E(store, err)
+		}
+	}
+	err = bp.Close()
+	if err != nil {
+		return errors.E(store, err)
+	}
+	return nil
+}
+
+// storeBlock stores a single block of data to the StoreServer as part of a block packing operation.
+func storeBlock(store upspin.StoreServer, bp upspin.BlockPacker, data []byte) error {
+	// TODO(edpin): remove logging once debugging is done.
+	log.Debug.Print("Writing %d bytes of data", len(data))
+	cipher, err := bp.Pack(data)
+	if err != nil {
+		return err
+	}
+	ref, err := store.Put(cipher)
+	if err != nil {
+		return err
+	}
+	loc := upspin.Location{
+		Endpoint:  store.Endpoint(),
+		Reference: ref,
+	}
+	bp.SetLocation(loc)
+	return nil
+}
+
+// addChildren unmarshals a block of packed dirEntries into a node.
+func (t *tree) addChildren(n *node, block []byte) error {
+	const addChildren = "addChildren"
+	if n.kids == nil {
+		n.kids = make(map[string]*node)
+	}
+	if n.dirty {
+		log.Error.Printf("addChildren: trying to load a block from storage when the node %s is dirty.", n.entry.Name)
+		return errInternalInconsistency
+	}
+	if n.entry == nil || n.entry.Name == "" {
+		// Node is fubar. TODO: log details.
+		return errors.E(addChildren, errInternalInconsistency)
+	}
+	var dirs []upspin.DirEntry
+	for len(block) > 0 {
+		var dir upspin.DirEntry
+		remaining, err := dir.Unmarshal(block)
+		if err != nil {
+			return errors.E(addChildren, err)
+		}
+		dirs = append(dirs, dir)
+		block = remaining
+	}
+	// Load children for this node.
+	dePath, err := path.Parse(n.entry.Name)
+	if err != nil {
+		return errors.E(addChildren, err)
+	}
+	elemPos := dePath.NElem()
+	for _, dir := range dirs {
+		p, err := path.Parse(dir.Name)
+		if err != nil {
+			return errors.E(addChildren, err)
+		}
+		if p.NElem() <= elemPos {
+			// We should never have written a dirEntry whose path does not contain
+			// one more element than the parent.
+			return errors.E(addChildren, errInternalInconsistency)
+		}
+		elem := p.Elem(elemPos)
+		if _, exists := n.kids[elem]; exists {
+			// Trying to re-add an existing child. Something is amiss.
+			return errors.E(addChildren, errInternalInconsistency)
+		}
+		n.kids[elem] = &node{
+			entry: &dir,
+		}
+	}
+	return nil
+}
+
+// readDirEntry retrieves all the data for a dir entry.
+func (t *tree) readDirEntry(de *upspin.DirEntry) ([]byte, error) {
+	packer := pack.Lookup(de.Packing)
+	if packer == nil {
+		return nil, errors.Errorf("no packing %s registered", de.Packing)
+	}
+	u, err := packer.Unpack(t.context, de)
+	if err != nil {
+		return nil, err
+	}
+	var data []byte
+	for {
+		block, ok := u.NextBlock()
+		if !ok {
+			break
+		}
+		ciphertext, err := t.get(&block.Location)
+		if err != nil {
+			return nil, err
+		}
+		cleartext, err := u.Unpack(ciphertext)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, cleartext...)
+	}
+	return data, nil
+}
