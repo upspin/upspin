949d3069908cbaa2b86c44481df7492d00c5aec3
Eduardo Pinheiro <edpin@google.com>
2016-08-08 12:23:42 -0700

    errors: add Internal error
    For when servers crap out due to internal inconsistencies
    or programmer errors.
    
    Change-Id: I709095506303d060de1621a18af9f2fb4e33cf5e

--- errors/errors.go
+++ errors/errors.go
@@ -65,6 +65,7 @@ const (
 	IsDir                  // Item is a directory.
 	NotDir                 // Item is not a directory..
 	NotEmpty               // Directory not empty.
+	Internal               // Internal error or inconsistency.
 )
 
 func (k Kind) String() string {
@@ -89,6 +90,8 @@ func (k Kind) String() string {
 		return "item is not a directory"
 	case NotEmpty:
 		return "directory not empty"
+	case Internal:
+		return "internal error"
 	}
 	return "unknown error kind"
 }
