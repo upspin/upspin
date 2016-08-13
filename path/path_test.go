00b9df3ebf409f728af20dc030656850c42e07dd
Rob Pike <r@golang.org>
2016-08-13 11:55:22 +1000

    path: strengthen UserAndDomain to be more rigorous
    Still needs to deal with Unicode user names, which it will reject now.
    
    Update #26.
    
    Change-Id: I4f33de54dba0155c21f8d23f105f278d21762280

--- path/path_test.go
+++ path/path_test.go
@@ -79,7 +79,12 @@ func TestBadParse(t *testing.T) {
 	}
 }
 
-var userTests = []upspin.PathName{"a@b.co/", "a@b.co/a", "a@b.co/a/b/c", "a@b.co/a/b/c/d"}
+var userTests = []upspin.PathName{
+	"a@b.co/",
+	"a@b.co/a",
+	"a@b.co/a/b/c",
+	"a@b.co/a/b/c/d",
+}
 
 func TestUser(t *testing.T) {
 	const want = "a@b.co"
@@ -267,38 +272,52 @@ func TestUserAndDomain(t *testing.T) {
 		domain   string
 		errStr   string
 	}
-	var (
-		tests = []cases{
-			{upspin.UserName("me@here.com"), "me", "here.com", ""},
-			{upspin.UserName("@"), "", "", "syntax error: no user name"},
-			{upspin.UserName("a@bcom"), "", "", "syntax error: invalid domain name"},
-			{upspin.UserName("a@b@.com"), "", "", "syntax error: extra @ sign"},
-			{upspin.UserName("@bbc.com"), "", "", "syntax error: no user name"},
-			{upspin.UserName("abc.com@"), "", "", "syntax error: invalid domain name"},
-			{upspin.UserName("a@b.co"), "a", "b.co", ""},
-			{upspin.UserName("me@here/.com"), "", "", "syntax error: is a path name"},
-		}
-	)
+	hugeOKName := strings.Repeat("a", 254-9)
+	hugeBadName := strings.Repeat("b", 254-8)
+	hugeOKDomain := strings.Repeat("x", 63) + ".com"
+	hugeBadDomain := strings.Repeat("y", 64) + ".com"
+	var tests = []cases{
+		{upspin.UserName("me@here.com"), "me", "here.com", ""},
+		{upspin.UserName("me@HERE.com"), "me", "here.com", ""},                // Lower-case the domain.
+		{upspin.UserName(hugeOKName + "@foo.com"), hugeOKName, "foo.com", ""}, // Maximum accepted length, 254.
+		{upspin.UserName(hugeBadName + "@foo.com"), "", "", "name too long"},
+		{upspin.UserName("@"), "", "", "syntax error: missing user name"},
+		{upspin.UserName("user@" + hugeOKDomain), "user", hugeOKDomain, ""}, // Maximum domain element length, 63.
+		{upspin.UserName("user@" + hugeBadDomain), "", "", "invalid domain name element"},
+		{upspin.UserName("user@a..com"), "", "", "invalid domain name element"},
+		{upspin.UserName("a@bcom"), "", "", "domain name must contain a period"},
+		{upspin.UserName("a@b@.com"), "", "", "user name must contain one @ symbol"},
+		{upspin.UserName("a@%.com"), "", "", "bad symbol in domain name"},
+		{upspin.UserName("@bbc.com"), "", "", "missing user name"},
+		{upspin.UserName("abc.com@"), "", "", "missing domain name"},
+		{upspin.UserName("a@b.co"), "a", "b.co", ""},
+		{upspin.UserName("a@b.c"), "", "", "invalid domain name"},
+		{upspin.UserName("me@here/.com"), "", "", "bad symbol in domain name"},
+		{upspin.UserName("me@here.com."), "me", "here.com.", ""},
+		{upspin.UserName("me@here.com.."), "", "", "invalid domain name"},
+	}
 	for _, test := range tests {
 		u, d, err := UserAndDomain(test.userName)
 		if test.errStr == "" {
 			if err != nil {
-				t.Errorf("Expected no error, got %q", err)
+				t.Errorf("%q: Expected no error, got %q", test.userName, err)
+				continue
+			}
+			if u != test.user {
+				t.Errorf("%q: expected user %q, got %q", test.userName, test.user, u)
+			}
+			if d != test.domain {
+				t.Errorf("%q: expected domain %q, got %q", test.userName, test.domain, u)
 			}
 			continue
 		}
 		if err == nil {
-			t.Errorf("Expected %q error, got none", test.errStr)
-		}
-		if !strings.Contains(err.Error(), test.errStr) {
-			t.Errorf("Expected %q, got %q", test.errStr, err)
+			t.Errorf("%q: expected %q error, got none", test.userName, test.errStr)
 			continue
 		}
-		if u != test.user {
-			t.Errorf("Expected user %q, got %q", test.user, u)
-		}
-		if d != test.domain {
-			t.Errorf("Expected domain %q, got %q", test.domain, u)
+		if !strings.Contains(err.Error(), test.errStr) {
+			t.Errorf("%q: expected %q, got %q", test.userName, test.errStr, err)
+			continue
 		}
 	}
 }
