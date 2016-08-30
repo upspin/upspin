f4830a8e0d9971a8c08539b7aa93c645ac4dfb8c
Dave Presotto <p@google.com>
2016-08-29 21:21:46 -0700

    cmd/upspinfs: Fix some of the testing problems.
    - For now, disable build of sh_test in linux.
    - Move the body of upspinfs into a do() function so that it can be
      called directly from main() and from TestXXXs(). No separate build
      required.
    - Use a temporary directory in /tmp for the test mounts
    - Just output log to stderr (along with set -x in the shell script)
      to make debugging easier.
    
    Change-Id: I8f42edc2557a4a92bddb9cdd9c17695b1efa851b

--- cmd/upspinfs/main.go
+++ cmd/upspinfs/main.go
@@ -11,14 +11,10 @@ import (
 	"os"
 	"path/filepath"
 
-	"bazil.org/fuse"
-	"bazil.org/fuse/fs"
-
 	"upspin.io/context"
 	"upspin.io/flags"
 	"upspin.io/key/usercache"
 	"upspin.io/log"
-	"upspin.io/upspin"
 
 	_ "upspin.io/dir/transports"
 	_ "upspin.io/key/transports"
@@ -27,111 +23,36 @@ import (
 	_ "upspin.io/store/transports"
 )
 
-var (
-	testFlag = flag.String("test", "", "set up test context with specified user")
-)
-
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage: %s <mountpoint>\n", os.Args[0])
 	flag.PrintDefaults()
 	os.Exit(2)
 }
 
-func debug(msg interface{}) {
-	log.Debug.Printf("FUSE %v", msg)
-}
-
-func testSetup(name upspin.UserName) (ctx upspin.Context, err error) {
-	endpoint := upspin.Endpoint{
-		Transport: upspin.InProcess,
-		NetAddr:   "", // ignored
-	}
-	ctx = context.New().
-		SetUserName(name).
-		SetPacking(upspin.DebugPack).
-		SetKeyEndpoint(endpoint).
-		SetDirEndpoint(endpoint).
-		SetStoreEndpoint(endpoint)
-	publicKey := upspin.PublicKey(fmt.Sprintf("key for %s", name))
-	user := &upspin.User{
-		Name:      upspin.UserName(name),
-		Dirs:      []upspin.Endpoint{ctx.DirEndpoint()},
-		Stores:    []upspin.Endpoint{ctx.StoreEndpoint()},
-		PublicKey: publicKey,
-	}
-	err = ctx.KeyServer().Put(user)
-	return
-}
-
 func main() {
 	flag.Usage = usage
-	flags.Parse()
-
-	if log.Level() == "debug" {
-		fuse.Debug = debug
-	}
+	flags.Parse("context", "log")
 
 	if flag.NArg() != 1 {
 		usage()
 	}
+
+	// Normal setup, get context from file and push user cache onto context.
+	cf, err := os.Open(flags.Context)
+	if err != nil {
+		log.Debug.Fatal(err)
+	}
+	ctx, err := context.InitContext(cf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx = usercache.Global(ctx)
+
+	// Mount the file system and start serving.
 	mountpoint, err := filepath.Abs(flag.Arg(0))
 	if err != nil {
 		log.Fatal("can't determine absolute path to mount point %s: %s", flag.Arg(0), err)
 	}
-
-	var ctx upspin.Context
-	if *testFlag != "" {
-		// Special setup for testing.
-		ctx, err = testSetup(upspin.UserName(*testFlag))
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		// Normal setup, get context from file.
-		cf, err := os.Open(flags.Context)
-		if err != nil {
-			log.Debug.Fatal(err)
-		}
-		ctx, err = context.InitContext(cf)
-		if err != nil {
-			log.Fatal(err)
-		}
-		ctx = usercache.Global(ctx)
-	}
-
-	f := newUpspinFS(ctx, mountpoint)
-
-	c, err := fuse.Mount(
-		mountpoint,
-		fuse.FSName("upspin"),
-		fuse.Subtype("fs"),
-		fuse.LocalVolume(),
-		fuse.VolumeName(string(f.context.UserName())),
-		fuse.DaemonTimeout("240"),
-		//fuse.OSXDebugFuseKernel(),
-		fuse.NoAppleDouble(),
-		fuse.NoAppleXattr(),
-	)
-	if err != nil {
-		log.Debug.Fatal(err)
-	}
-	defer c.Close()
-
-	keyserver := ctx.KeyServer()
-	if u, err := keyserver.Lookup(upspin.UserName(*testFlag)); err != nil {
-		log.Fatal(err)
-	} else {
-		log.Debug.Printf("looked up %v %v", u, keyserver)
-	}
-
-	err = fs.Serve(c, f)
-	if err != nil {
-		log.Debug.Fatal(err)
-	}
-
-	// check if the mount process has an error to report
-	<-c.Ready
-	if err := c.MountError; err != nil {
-		log.Debug.Fatal(err)
-	}
+	done := do(ctx, mountpoint)
+	<-done
 }
--- cmd/upspinfs/sh_test.go
+++ cmd/upspinfs/sh_test.go
@@ -1,20 +1,79 @@
 // Copyright 2016 The Upspin Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+//
+// +build !linux
 
-package main_test
+package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"syscall"
 	"testing"
+
+	"upspin.io/context"
+	"upspin.io/upspin"
 )
 
+// testSetup creates a temporary user context with inprocess services.
+func testSetup(name string) (ctx upspin.Context, err error) {
+	endpoint := upspin.Endpoint{
+		Transport: upspin.InProcess,
+		NetAddr:   "", // ignored
+	}
+	ctx = context.New().
+		SetUserName(upspin.UserName(name)).
+		SetPacking(upspin.DebugPack).
+		SetKeyEndpoint(endpoint).
+		SetDirEndpoint(endpoint).
+		SetStoreEndpoint(endpoint)
+	publicKey := upspin.PublicKey(fmt.Sprintf("key for %s", name))
+	user := &upspin.User{
+		Name:      upspin.UserName(name),
+		Dirs:      []upspin.Endpoint{ctx.DirEndpoint()},
+		Stores:    []upspin.Endpoint{ctx.StoreEndpoint()},
+		PublicKey: publicKey,
+	}
+	err = ctx.KeyServer().Put(user)
+	return
+}
+
 func TestShell(t *testing.T) {
-	cmd := exec.Command("./test.sh")
+	mountpoint, err := ioutil.TempDir("/tmp", "upspinfstest")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	fmt.Printf("mountpoint is %s\n", mountpoint)
+
+	// Set up a user context.
+	ctx, err := testSetup("tester@google.com")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	// Create a fresh mountpoint.
+	syscall.Unmount(mountpoint, 0)
+	os.RemoveAll(mountpoint)
+	os.Mkdir(mountpoint, 0777)
+
+	// Mount the file system. It will be served in a separate go routine.
+	do(ctx, mountpoint)
+
+	// Run the tests.
+	cmd := exec.Command("./test.sh", mountpoint, "tester@google.com")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
+	err = cmd.Run()
+
+	// Unmount.
+	syscall.Unmount(mountpoint, 0)
+	os.RemoveAll(mountpoint)
+
+	// Report error.
+	if err != nil {
 		t.Fatal(err.Error())
 	}
 }
