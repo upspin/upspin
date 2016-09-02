f5cd5ab04bfb4055c85dbac66dfb7c0956188c93
Andrew Gerrand <adg@golang.org>
2016-09-02 15:13:46 +1000

    test/testenv: remove tree builder from the env setup
    The new testenv.Runner can do the same thing,
    but is more flexible and just as succinct.
    
    Change-Id: I79bde22cd43d48ba6370879eab1db3643f22238e

--- test/testenv/testenv.go
+++ test/testenv/testenv.go
@@ -10,7 +10,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"upspin.io/client"
 	"upspin.io/context"
@@ -27,15 +26,6 @@ import (
 	_ "upspin.io/store/remote"
 )
 
-// Entry is an entry in the Upspin namespace.
-type Entry struct {
-	// P is a path name in string format. Directories must end in "/".
-	P string
-
-	// C is the contents of P. C must be empty for directories.
-	C string
-}
-
 // Setup is a configuration structure that contains a directory tree and other optional flags.
 type Setup struct {
 	// OwnerName is the name of the directory tree owner.
@@ -47,24 +37,13 @@ type Setup struct {
 	// Packing is the desired packing for the tree.
 	Packing upspin.Packing
 
-	// Tree is the directory tree desired at the start of the test environment.
-	Tree Tree
-
-	// Some configuration options follow
-
 	// Verbose indicates whether we should print verbose debug messages.
 	Verbose bool
-
-	// IgnoreExistingDirectories does not report an error if the directories already exist from a previous run.
-	IgnoreExistingDirectories bool
 
 	// Cleanup, if present, is run at Exit to clean up any test state necessary.
 	// It may return an error, which is returned by Exit.
 	Cleanup func(e *Env) error
 }
-
-// Tree is a full directory tree with path names and their contents.
-type Tree []Entry
 
 // Env is the test environment. It contains a client which is the main piece that tests should use.
 type Env struct {
@@ -86,57 +65,19 @@ func New(setup *Setup) (*Env, error) {
 	env := &Env{
 		Setup: setup,
 	}
-	client, context, err := env.NewUser(setup.OwnerName)
+	ctx, err := env.NewUser(setup.OwnerName)
 	if err != nil {
-		return nil, err
+		return nil, errors.E(op, err)
 	}
-	env.Client = client
-	env.Context = context
-	err = makeRoot(context)
-	if err != nil {
-		return nil, err
-	}
-	// Generate the dir tree using the client.
-	for _, e := range setup.Tree {
-		if strings.HasSuffix(e.P, "/") {
-			if len(e.C) > 0 {
-				return nil, errors.E(op, errors.NotEmpty, errors.Str("directory entry must not have contents"))
-			}
-			dir := path.Join(upspin.PathName(setup.OwnerName), e.P)
-			entry, err := client.MakeDirectory(dir)
-			if err != nil {
-				if !setup.IgnoreExistingDirectories {
-					log.Printf("Tree: Error creating directory %s: %s", dir, err)
-					return nil, err
-				}
-			}
-			if setup.Verbose {
-				log.Printf("Tree: Created directory %#v", entry)
-			}
-		} else {
-			name := path.Join(upspin.PathName(setup.OwnerName), e.P)
-			entry, err := client.Put(name, []byte(e.C))
-			if err != nil {
-				log.Printf("Error creating file %s: %s", name, err)
-				return nil, errors.E(op, err)
-			}
-			if setup.Verbose {
-				log.Printf("Tree: Created file %#v", entry)
-			}
-		}
+	env.Context = ctx
+	env.Client = client.New(ctx)
+	if err := makeRootIfNotExist(ctx); err != nil {
+		return nil, errors.E(op, err)
 	}
 	if setup.Verbose {
 		log.Printf("Tree: All entries created.")
 	}
 	return env, nil
-}
-
-// E (short for Entry) is a helper function to return a new Entry.
-func E(pathName string, contents string) Entry {
-	return Entry{
-		P: pathName,
-		C: contents,
-	}
 }
 
 // Exit indicates the end of the test environment. It must only be called once. If Setup.Cleanup exists it is called.
@@ -158,88 +99,52 @@ func (e *Env) Exit() error {
 // NewUser creates a new client for a user.  The new user will not
 // have a root created. Callers should use the client to MakeDirectory if
 // necessary.
-func (e *Env) NewUser(userName upspin.UserName) (upspin.Client, upspin.Context, error) {
+func (e *Env) NewUser(userName upspin.UserName) (upspin.Context, error) {
 	const op = "testenv.NewUser"
-	var ctx upspin.Context
-	var err error
-	ctx = context.New().SetUserName(userName).SetPacking(e.Setup.Packing)
+	ctx := context.New().SetUserName(userName).SetPacking(e.Setup.Packing)
+
 	// Get keys for user.
 	user, _, err := path.UserAndDomain(userName)
 	if err != nil {
-		return nil, nil, errors.E(op, err)
+		return nil, errors.E(op, err)
 	}
 	f, err := factotum.New(repo("key/testdata/" + string(user)))
 	if err != nil {
-		return nil, nil, errors.E(op, userName, err)
+		return nil, errors.E(op, userName, err)
 	}
 	ctx.SetFactotum(f)
 
-	var client upspin.Client
+	// Set up endpoints.
+	inProcessEndpoint := upspin.Endpoint{
+		Transport: upspin.InProcess,
+		NetAddr:   "", // ignored
+	}
+	ctx.SetKeyEndpoint(inProcessEndpoint)
+
 	switch k := e.Setup.Kind; k {
 	case "remote":
-		client, err = remoteClient(ctx)
+		ctx.SetStoreEndpoint(upspin.Endpoint{
+			Transport: upspin.Remote,
+			NetAddr:   "store.test.upspin.io:443", // Test store server.
+		})
+		ctx.SetDirEndpoint(upspin.Endpoint{
+			Transport: upspin.Remote,
+			NetAddr:   "dir.test.upspin.io:443", // Test dir server.
+		})
 	case "inprocess":
-		client, err = inProcessClient(ctx)
+		ctx.SetStoreEndpoint(inProcessEndpoint)
+		ctx.SetDirEndpoint(inProcessEndpoint)
 	default:
-		return nil, nil, errors.E(op, errors.Invalid, errors.Errorf("bad server kind %q", k))
-	}
-	if err != nil {
-		return nil, nil, errors.E(op, err)
+		return nil, errors.E(op, errors.Invalid, errors.Errorf("bad server kind %q", k))
 	}
 
 	// Register user with key server.
 	err = registerUserWithKeyServer(userName, ctx)
 	if err != nil {
-		return nil, nil, errors.E(op, err)
+		return nil, errors.E(op, err)
 	}
 
-	return client, ctx, nil
-}
-
-// remoteClient returns a Client pointing to the remote test instances on
-// upspin.io given a Context partially initialized with a user and keys.
-func remoteClient(context upspin.Context) (upspin.Client, error) {
-	// Use a test GCP StoreServer...
-	endpointStore := upspin.Endpoint{
-		Transport: upspin.Remote,
-		NetAddr:   "store.test.upspin.io:443", // Test store server.
-	}
-	// ... and a test GCP directory ...
-	endpointDir := upspin.Endpoint{
-		Transport: upspin.Remote,
-		NetAddr:   "dir.test.upspin.io:443", // Test dir server.
-	}
-	// and an in-process test user.
-	endpointKey := upspin.Endpoint{
-		Transport: upspin.InProcess,
-		NetAddr:   "", // ignored
-	}
-	setContextEndpoints(context, endpointStore, endpointDir, endpointKey)
-	client := client.New(context)
-	return client, nil
-}
-
-// inProcessClient returns a Client pointing to in-process instances given a Context partially initialized
-// with a user and keys.
-func inProcessClient(context upspin.Context) (upspin.Client, error) {
-	// Use an in-process StoreServer...
-	endpointStore := upspin.Endpoint{
-		Transport: upspin.InProcess,
-		NetAddr:   "",
-	}
-	// ... and an in-process directory ...
-	endpointDir := upspin.Endpoint{
-		Transport: upspin.InProcess,
-		NetAddr:   "",
-	}
-	// and an in-process test user.
-	endpointKey := upspin.Endpoint{
-		Transport: upspin.InProcess,
-		NetAddr:   "",
-	}
-	setContextEndpoints(context, endpointStore, endpointDir, endpointKey)
-	client := client.New(context)
-	return client, nil
+	return ctx, nil
 }
 
 // registerUserWithKeyServer registers userName's context with the inProcess keyServer.
@@ -258,21 +163,14 @@ func registerUserWithKeyServer(userName upspin.UserName, context upspin.Context)
 	return nil
 }
 
-func makeRoot(context upspin.Context) error {
+func makeRootIfNotExist(context upspin.Context) error {
 	path := upspin.PathName(context.UserName()) + "/"
 	dir := context.DirServer(path)
-	// Make the root to be sure it's there.
 	_, err := dir.MakeDirectory(path)
 	if err != nil && !errors.Match(errors.E(errors.Exist), err) {
 		return err
 	}
 	return nil
-}
-
-func setContextEndpoints(context upspin.Context, store, dir, key upspin.Endpoint) {
-	context.SetStoreEndpoint(store)
-	context.SetDirEndpoint(dir)
-	context.SetKeyEndpoint(key)
 }
 
 // repo returns the local pathname of a file in the upspin repository.
