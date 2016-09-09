75768db045e9d2c11426213871ee591d082b9aec
Andrew Gerrand <adg@golang.org>
2016-09-09 11:42:34 +1000

    dir/remote: add debug logging
    Change-Id: Ifc13678725cdcf812d6f9feec2dbabad7b0401b3

--- dir/remote/remote.go
+++ dir/remote/remote.go
@@ -7,11 +7,14 @@
 package remote
 
 import (
+	"fmt"
+
 	gContext "golang.org/x/net/context"
 
 	"upspin.io/auth/grpcauth"
 	"upspin.io/bind"
 	"upspin.io/errors"
+	"upspin.io/log"
 	"upspin.io/upspin"
 	"upspin.io/upspin/proto"
 )
@@ -37,109 +40,122 @@ var _ upspin.DirServer = (*remote)(nil)
 
 // Glob implements upspin.DirServer.Glob.
 func (r *remote) Glob(pattern string) ([]*upspin.DirEntry, error) {
-	const op = "dir/remote.Glob"
+	op := opf("Glob", "%q", pattern)
+
 	gCtx, err := r.NewAuthContext()
 	if err != nil {
-		return nil, errors.E(op, err)
+		return nil, op.error(err)
 	}
 	req := &proto.DirGlobRequest{
 		Pattern: pattern,
 	}
 	resp, err := r.dirClient.Glob(gCtx, req)
 	if err != nil {
-		return nil, errors.E(op, errors.IO, err)
+		return nil, op.error(errors.IO, err)
 	}
 	r.LastActivity()
 
 	err = unmarshalError(resp.Error)
 	if err != nil && err != upspin.ErrFollowLink {
-		return nil, errors.E(op, err)
+		return nil, op.error(err)
 	}
 	entries, pErr := proto.UpspinDirEntries(resp.Entries)
 	if pErr != nil {
-		return nil, errors.E(op, errors.IO, pErr)
+		return nil, op.error(errors.IO, pErr)
 	}
-	return entries, err
+	return entries, op.error(err)
 }
 
 // MakeDirectory implements upspin.DirServer.MakeDirectory.
 func (r *remote) MakeDirectory(directoryName upspin.PathName) (*upspin.DirEntry, error) {
-	const op = "dir/remote.MakeDirectory"
+	op := opf("MakeDirectory", "%q", directoryName)
+
 	gCtx, err := r.NewAuthContext()
 	if err != nil {
-		return nil, errors.E(op, err)
+		return nil, op.error(err)
 	}
 	req := &proto.DirMakeDirectoryRequest{
 		Name: string(directoryName),
 	}
 	resp, err := r.dirClient.MakeDirectory(gCtx, req)
 	r.LastActivity()
-	return entryError(op, resp, err)
+	return op.entryError(resp, err)
+}
+
+func entryName(entry *upspin.DirEntry) string {
+	if entry == nil {
+		return "<nil>"
+	}
+	return fmt.Sprintf("%q", entry.Name)
 }
 
 // Put implements upspin.DirServer.Put.
 // Directories are created with MakeDirectory. Roots are anyway. TODO?.
 func (r *remote) Put(entry *upspin.DirEntry) (*upspin.DirEntry, error) {
-	const op = "dir/remote.Put"
+	op := opf("Put", "%s", entryName(entry))
+
 	gCtx, err := r.NewAuthContext()
 	if err != nil {
-		return nil, errors.E(op, err)
+		return nil, op.error(err)
 	}
 	b, err := entry.Marshal()
 	if err != nil {
-		return nil, errors.E(op, err)
+		return nil, op.error(err)
 	}
 	req := &proto.DirPutRequest{
 		Entry: b,
 	}
 	resp, err := r.dirClient.Put(gCtx, req)
 	r.LastActivity()
-	return entryError(op, resp, err)
+	return op.entryError(resp, err)
 }
 
 // WhichAccess implements upspin.DirServer.WhichAccess.
 func (r *remote) WhichAccess(pathName upspin.PathName) (*upspin.DirEntry, error) {
-	const op = "dir/remote.WhichAccess"
+	op := opf("WhichAccess", "%q", pathName)
+
 	gCtx, err := r.NewAuthContext()
 	if err != nil {
-		return nil, errors.E(op, err)
+		return nil, op.error(err)
 	}
 	req := &proto.DirWhichAccessRequest{
 		Name: string(pathName),
 	}
 	resp, err := r.dirClient.WhichAccess(gCtx, req)
 	r.LastActivity()
-	return entryError(op, resp, err)
+	return op.entryError(resp, err)
 }
 
 // Delete implements upspin.DirServer.Delete.
 func (r *remote) Delete(pathName upspin.PathName) (*upspin.DirEntry, error) {
-	const op = "dir/remote.Delete"
+	op := opf("Delete", "%q", pathName)
+
 	gCtx, err := r.NewAuthContext()
 	if err != nil {
-		return nil, errors.E(op, err)
+		return nil, op.error(err)
 	}
 	req := &proto.DirDeleteRequest{
 		Name: string(pathName),
 	}
 	resp, err := r.dirClient.Delete(gCtx, req)
 	r.LastActivity()
-	return entryError(op, resp, err)
+	return op.entryError(resp, err)
 }
 
 // Lookup implements upspin.DirServer.Lookup.
 func (r *remote) Lookup(pathName upspin.PathName) (*upspin.DirEntry, error) {
-	const op = "dir/remote.Lookup"
+	op := opf("Lookup", "%q", pathName)
+
 	gCtx, err := r.NewAuthContext()
 	if err != nil {
-		return nil, errors.E(op, err)
+		return nil, op.error(err)
 	}
 	req := &proto.DirLookupRequest{
 		Name: string(pathName),
 	}
 	resp, err := r.dirClient.Lookup(gCtx, req)
 	r.LastActivity()
-	return entryError(op, resp, err)
+	return op.entryError(resp, err)
 }
 
 // Endpoint implements upspin.StoreServer.Endpoint.
@@ -149,27 +165,29 @@ func (r *remote) Endpoint() upspin.Endpoint {
 
 // Configure implements upspin.Service.
 func (r *remote) Configure(options ...string) (upspin.UserName, error) {
-	const op = "dir/remote.Configure"
+	op := opf("Configure", "%v", options)
+
 	req := &proto.ConfigureRequest{
 		Options: options,
 	}
 	resp, err := r.dirClient.Configure(gContext.Background(), req)
 	if err != nil {
-		return "", errors.E(op, errors.IO, err)
+		return "", op.error(errors.IO, err)
 	}
-	return "", errors.UnmarshalError(resp.Error)
+	return "", op.error(errors.UnmarshalError(resp.Error))
 }
 
 // Dial implements upspin.Service.
 func (*remote) Dial(context upspin.Context, e upspin.Endpoint) (upspin.Service, error) {
-	const op = "dir/remote.Dial"
+	op := opf("Dial", "%v %v", context.UserName(), e)
+
 	if e.Transport != upspin.Remote {
-		return nil, errors.E(op, errors.Invalid, errors.Str("unrecognized transport"))
+		return nil, op.error(errors.Invalid, errors.Str("unrecognized transport"))
 	}
 
 	authClient, err := grpcauth.NewGRPCClient(context, e.NetAddr, grpcauth.KeepAliveInterval, grpcauth.Secure)
 	if err != nil {
-		return nil, errors.E(op, errors.IO, e, err)
+		return nil, op.error(errors.IO, e, err)
 	}
 
 	// The connection is closed when this service is released (see Bind.Release)
@@ -205,19 +223,50 @@ func unmarshalError(b []byte) error {
 	return err
 }
 
+func opf(method string, format string, args ...interface{}) *operation {
+	op := &operation{"dir/remote." + method, fmt.Sprintf(format, args...)}
+	log.Debug.Print(op)
+	return op
+}
+
+type operation struct {
+	op   string
+	args string
+}
+
+func (op *operation) String() string {
+	return fmt.Sprintf("%s: %s", op.op, op.args)
+}
+
+func (op *operation) error(args ...interface{}) error {
+	if len(args) == 0 {
+		panic("error called with zero args")
+	}
+	if len(args) == 1 {
+		if e, ok := args[0].(error); ok && e == upspin.ErrFollowLink {
+			return e
+		}
+		if args[0] == nil {
+			return nil
+		}
+	}
+	log.Debug.Print("%v failed: %v", op, errors.E(args...))
+	return errors.E(append([]interface{}{op.op}, args...))
+}
+
 // entryError performs the common operation of converting an EntryError
 // protocol buffer into a directory entry and error pair.
-func entryError(op string, p *proto.EntryError, err error) (*upspin.DirEntry, error) {
+func (op *operation) entryError(p *proto.EntryError, err error) (*upspin.DirEntry, error) {
 	if err != nil {
-		return nil, errors.E(op, errors.IO, err)
+		return nil, op.error(errors.IO, err)
 	}
 	err = unmarshalError(p.Error)
 	if err != nil && err != upspin.ErrFollowLink {
-		return nil, errors.E(op, err)
+		return nil, op.error(err)
 	}
 	entry, unmarshalErr := proto.UpspinDirEntry(p.Entry)
 	if unmarshalErr != nil {
-		return nil, errors.E(op, unmarshalErr)
+		return nil, op.error(unmarshalErr)
 	}
-	return entry, err
+	return entry, op.error(err)
 }
