e06e57b4f9e0cbcdb58768fc3ebe0ee5d312f18f
Eduardo Pinheiro <edpin@google.com>
2016-09-12 13:16:34 -0700

    factotum: split New into two versions
    This ensures the non-visibility of keys that Eric wants to achieve.
    
    This is mostly a no-op: just pulling the bulk of New into another
    function that is only used by factotum_mobile.go (and possibly other
    embedded platforms) to build a Factotum from keys instead of from a
    file system's directory.
    
    All tests pass.
    No new lint or vet errors.
    
    Change-Id: Idbf95b359a9e9f2cb9699a3a629501bbbe11aec3

---
+++ factotum/new.go
@@ -0,0 +1,42 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !mobile
+
+package factotum
+
+import (
+	"fmt"
+
+	"upspin.io/errors"
+	"upspin.io/upspin"
+)
+
+// New returns a new Factotum providing all needed private key operations,
+// loading keys from a directory (which looks for dir/*.upspinkey files).
+// Our desired end state is that Factotum is implemented on each platform by the
+// best local means of protecting private keys.  Please do not break the abstraction
+// by hand coding direct generation or use of private keys.
+func New(dir string) (upspin.Factotum, error) {
+	const op = "factotum.NewFromDir"
+	privBytes, err := readFile(op, dir, "secret.upspinkey")
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+	privBytes = stripCR(privBytes)
+	pubBytes, err := readFile(op, dir, "public.upspinkey")
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+	pubBytes = stripCR(pubBytes)
+
+	// Read older key pairs.
+	s2, err := readFile(op, dir, "secret2.upspinkey")
+	if err != nil && !errors.Match(errors.E(errors.NotExist), err) {
+		return nil, err
+	}
+	s2 = stripCR(s2)
+
+	return newFactotum(fmt.Sprintf("%s(%q)", op, dir), pubBytes, privBytes, s2)
+}
