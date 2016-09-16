35641261a000910372cad1a474f2ddae759c472e
Andrew Gerrand <adg@golang.org>
2016-09-16 18:14:39 +1000

    cmd/upbox: add command for rapidly spinning up test environments
    This is just a first step; for now, a glorified shell script.
    
    Change-Id: Ieb9d797a7cbf15582c97a695d528e83fc828ac6c

---
+++ cmd/upbox/generate_cert.go
@@ -0,0 +1,111 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file is adapted from $GOROOT/src/crypto/tls/generate_cert.go
+
+// Generate a self-signed X.509 certificate for a TLS server.
+
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func publicKey(priv interface{}) interface{} {
+	switch k := priv.(type) {
+	case *rsa.PrivateKey:
+		return &k.PublicKey
+	case *ecdsa.PrivateKey:
+		return &k.PublicKey
+	default:
+		return nil
+	}
+}
+
+func pemBlockForKey(priv interface{}) *pem.Block {
+	switch k := priv.(type) {
+	case *rsa.PrivateKey:
+		return &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(k)}
+	case *ecdsa.PrivateKey:
+		b, err := x509.MarshalECPrivateKey(k)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to marshal ECDSA private key: %v", err)
+			os.Exit(2)
+		}
+		return &pem.Block{Type: "EC PRIVATE KEY", Bytes: b}
+	default:
+		return nil
+	}
+}
+
+func generateCert(dir string) error {
+	priv, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		return fmt.Errorf("failed to generate key: %s", err)
+	}
+
+	notBefore := time.Now()
+	notAfter := notBefore.Add(24 * time.Hour)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return fmt.Errorf("failed to generate serial number: %s", err)
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"The Upspin Project"},
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+
+		DNSNames: []string{"localhost"},
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, publicKey(priv), priv)
+	if err != nil {
+		return fmt.Errorf("Failed to create certificate: %s", err)
+	}
+
+	certOut, err := os.Create(filepath.Join(dir, "cert.pem"))
+	if err != nil {
+		return fmt.Errorf("failed to open cert.pem for writing: %s", err)
+	}
+	if err := pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes}); err != nil {
+		return fmt.Errorf("failed to encode cert.pem: %s", err)
+	}
+	if err := certOut.Close(); err != nil {
+		return fmt.Errorf("failed to write cert.pem: %s", err)
+	}
+
+	keyOut, err := os.OpenFile(filepath.Join(dir, "key.pem"), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return fmt.Errorf("failed to open key.pem for writing:", err)
+	}
+	if err := pem.Encode(keyOut, pemBlockForKey(priv)); err != nil {
+		return fmt.Errorf("failed to encode key.pem: %s", err)
+	}
+	if err := keyOut.Close(); err != nil {
+		return fmt.Errorf("failed to write cert.pem: %s", err)
+	}
+
+	return nil
+}
---
+++ cmd/upbox/main.go
@@ -0,0 +1,194 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Command upbox builds and runs inprocess key, store, and directory servers
+// and provides an upspin shell acting as the test user bob@b.com.
+package main
+
+// TODO(adg): wait for each server to start up
+// TODO(adg): take username as an argument
+// TODO(adg): generate user secrets on startup
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	// TODO(adg): use flags package
+	logLevel = flag.String("log", "info", "log `level`")
+	basePort = flag.Int("port", 8000, "base `port` number for upspin servers")
+)
+
+func main() {
+	flag.Parse()
+	if err := do(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func do() error {
+	tmpDir, err := ioutil.TempDir("", "upbox")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tmpDir)
+
+	if err := generateCert(tmpDir); err != nil {
+		return err
+	}
+
+	port := *basePort
+	keyAddr := fmt.Sprintf("localhost:%d", port)
+	port++
+	storeAddr := fmt.Sprintf("localhost:%d", port)
+	port++
+	dirAddr := fmt.Sprintf("localhost:%d", port)
+
+	user := "bob@b.com"
+
+	secrets := filepath.Join(os.Getenv("GOPATH"), "src/upspin.io/key/testdata/bob")
+	if err := checkExists(secrets); err != nil {
+		return err
+	}
+
+	rcFile := filepath.Join(tmpDir, "rc")
+	rcContent := strings.Join([]string{
+		"username=" + user,
+		"keyserver=remote," + keyAddr,
+		"storeserver=remote," + storeAddr,
+		"dirserver=remote," + dirAddr,
+		"secrets=" + secrets,
+		"packing=ee",
+		"tlscerts=" + tmpDir,
+	}, "\n")
+	if err := ioutil.WriteFile(rcFile, []byte(rcContent), 0644); err != nil {
+		return err
+	}
+
+	// Build.
+	cmd := exec.Command("go", "install",
+		"upspin.io/cmd/keyserver",
+		"upspin.io/cmd/storeserver",
+		"upspin.io/cmd/dirserver",
+		"upspin.io/cmd/upspin",
+	)
+	cmd.Stdout = prefixWriter("build: ", os.Stdout)
+	cmd.Stderr = prefixWriter("build: ", os.Stderr)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("build error: %v", err)
+	}
+
+	server := func(name string, args ...string) *exec.Cmd {
+		cmd := exec.Command(name, append([]string{
+			"-context=" + rcFile,
+			"-log=" + *logLevel,
+			"-tls_cert=" + filepath.Join(tmpDir, "cert.pem"),
+			"-tls_key=" + filepath.Join(tmpDir, "key.pem"),
+		}, args...)...)
+		cmd.Stdout = prefixWriter(name+":\t", os.Stdout)
+		cmd.Stderr = prefixWriter(name+":\t", os.Stderr)
+		return cmd
+	}
+
+	// Start keyserver.
+	key := server("keyserver",
+		"-https="+keyAddr,
+		"-addr="+keyAddr, // is this necessary?
+		"-testuser="+user,
+	)
+	key.Env = []string{
+		"upspinusername=key@example.net",
+		"upspinsecrets=none",
+		"upspinpacking=plain",
+		"upspinstoreserver=unassigned,",
+		"upspindirserver=unassigned,",
+		"GOPATH=" + os.Getenv("GOPATH"),
+	}
+	if err := key.Start(); err != nil {
+		return fmt.Errorf("starting keyserver: %v", err)
+	}
+	defer kill(key)
+
+	// Start storeserver.
+	store := server("storeserver",
+		"-https="+storeAddr,
+		"-addr="+storeAddr, // is this necessary?
+	)
+	store.Env = []string{
+		"upspinusername=store@example.net",
+		"upspinsecrets=none",
+		"upspinpacking=plain",
+		"upspinstoreserver=inprocess,",
+		"upspindirserver=unassigned,",
+		"GOPATH=" + os.Getenv("GOPATH"),
+	}
+	if err := store.Start(); err != nil {
+		return fmt.Errorf("starting storeserver: %v", err)
+	}
+	defer kill(store)
+
+	// Start dirserver.
+	dir := server("dirserver",
+		"-https="+dirAddr,
+		"-addr="+dirAddr, // is this necessary?
+	)
+	dir.Env = []string{
+		"upspindirserver=inprocess,",
+		"GOPATH=" + os.Getenv("GOPATH"),
+	}
+	if err := dir.Start(); err != nil {
+		return fmt.Errorf("starting dirserver: %v", err)
+	}
+	defer kill(dir)
+
+	// Start upspin shell.
+	shell := exec.Command("upspin",
+		"-context="+rcFile,
+		"-log="+*logLevel,
+		"shell",
+	)
+	shell.Stdin = os.Stdin
+	shell.Stdout = os.Stdout
+	shell.Stderr = os.Stderr
+	return shell.Run()
+}
+
+func kill(cmd *exec.Cmd) {
+	if cmd.Process != nil {
+		cmd.Process.Kill()
+	}
+}
+
+func prefixWriter(prefix string, out io.Writer) io.Writer {
+	r, w := io.Pipe()
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			fmt.Fprintf(out, "%s%s\n", prefix, s.Bytes())
+		}
+	}()
+	return w
+}
+
+func checkExists(path ...string) error {
+	for _, p := range path {
+		_, err := os.Stat(p)
+		if os.IsNotExist(err) {
+			return fmt.Errorf("path does not exist: %v", err)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
