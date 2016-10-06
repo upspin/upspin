faa2a9180fcfb709dba3546b8ddb5177cf860769
Eduardo Pinheiro <edpin@google.com>
2016-10-06 11:15:23 -0700

    dir/server: fix Access race condition
    This is my understanding of what Dave said would fix
    the race.
    
    After much consideration, we decided a lock does not buy
    us anything in the access cache (other than the required
    one already in the LRU cache).
    
    All tests pass, including the remote integration tests
    (though, we did not explicitly create an Access file
    race in any test).
    
    No new lint or vet errors.
    
    Change-Id: Ie804c256e9400f66e7035cbbeb3bf508bacaef28

--- dir/server/access.go
+++ dir/server/access.go
@@ -15,6 +15,13 @@ import (
 	"upspin.io/upspin"
 )
 
+// accessEntry holds parsed Access files and a sequence number for their entries.
+// It is the unit stored in the access cache.
+type accessEntry struct {
+	sequence int64          // sequence number of the DirEntry parsed.
+	acc      *access.Access // parsed contents of the Access file.
+}
+
 // whichAccess implements DirServer.WhichAccess.
 func (s *server) whichAccess(p path.Parsed, opts ...options) (*upspin.DirEntry, error) {
 	o, ss := subspan("whichAccess", opts)
@@ -173,21 +180,34 @@ func (s *server) getAccess(entry *upspin.DirEntry, opts ...options) (*access.Acc
 	}
 
 	// Is it in the cache?
-	if acc, found := s.access.Get(entry.Name); found {
-		if a, ok := acc.(*access.Access); ok {
-			return a, nil
+	var accEntry *accessEntry
+	a, found := s.access.Get(entry.Name)
+	if found {
+		var ok bool
+		accEntry, ok = a.(*accessEntry)
+		if !ok {
+			return nil, errors.E(errors.Internal, errors.Str("invalid accessEntry"))
 		}
-		return nil, errors.E(errors.Internal, errors.Str("invalid accessCache entry"))
+		if entry.Sequence == accEntry.sequence {
+			return accEntry.acc, nil
+		}
+		// A race happened and we have a cached version that is
+		// different than the requested one. Fall through and load the
+		// requested one.
 	}
-	// Not in cache, load from the Store.
+	// Not in cache; load data from the Store.
+
 	// TODO: we should also reload any known Group files from their remote
 	// origin from time to time if they're not local to this server.
 	acc, err := s.loadAccess(entry, o)
 	if err != nil {
 		return nil, err
 	}
-	// Add to the cache.
-	s.access.Add(entry.Name, acc)
+	// Add or update cache.
+	s.access.Add(entry.Name, &accessEntry{
+		sequence: entry.Sequence,
+		acc:      acc,
+	})
 	return acc, nil
 }
 
