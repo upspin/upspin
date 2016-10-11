c2548b61474c61eccac10d59b0163685b99393dd
Eduardo Pinheiro <edpin@google.com>
2016-10-11 15:26:30 -0700

    cloud/mail: add package for sending email
    First implementation is with SendGrid.
    
    All tests pass.
    No new lint or vet errors.
    
    Change-Id: Ie5aaf888034f377e2fa36a66c08f65816fd6a870

---
+++ cloud/mail/mail.go
@@ -0,0 +1,22 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package mail defines an interface for sending mail that abstracts common
+// functionality between all mail providers such as SendGrid or MailGun.
+package mail
+
+// Mail sends email.
+type Mail interface {
+	// Send sends an email to a single recipient. The to address
+	// must be fully specified such as name@domain.com. The from address
+	// is just a name, the domain will be filled in as per the registered
+	// domain (see Domain). Fields subject, textBody and htmlBody are as
+	// expected. Subject is required and at least one of textBody or
+	// htmlBody must be present.
+	Send(to, from, subject, textBody, htmlBody string) error
+
+	// Domain returns the domain name of the server providing mail service.
+	// It is set during initialization and cannot be changed.
+	Domain() string
+}
---
+++ cloud/mail/sendgrid/sendgrid.go
@@ -0,0 +1,139 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package sendgrid sends email using SendGrid.
+package sendgrid
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"upspin.io/cloud/mail"
+	"upspin.io/errors"
+)
+
+// sendgrid implements cloud.mail.Mail using SendGrid as the underlying
+// substratum.
+type sendgrid struct {
+	apikey string
+	domain string
+}
+
+var _ mail.Mail = (*sendgrid)(nil)
+
+// New allocates a Mail type for sending email with SendGrid.
+func New(apikey, domain string) mail.Mail {
+	return &sendgrid{
+		apikey: apikey,
+		domain: domain,
+	}
+}
+
+// apiSend is the endpoint for requests. It's a var so tests can change it.
+var apiSend = "https://api.sendgrid.com/v3/mail/send"
+
+// Types below are Go JSON representations of SendGrid's API. The types are not
+// exported, but their fields are because json.marshal must be able to see them.
+
+// personalizations is a SendGrid's internal representation of the To and
+// Subject fields.
+type personalizations struct {
+	To      []addr
+	Subject string
+}
+
+// addr represents an email address with an optional recipient's name.
+type addr struct {
+	Email string
+	Name  string
+}
+
+// content represents the body of the email with a type of "text/plain" or
+// "text/html".
+type content struct {
+	Type  string
+	Value string
+}
+
+// message represents a message to send.
+// When more than one type of content is present plain must come before html.
+type message struct {
+	Personalizations []personalizations
+	From             addr
+	Content          []content
+}
+
+// Send implements cloud.mail.Mail.
+func (s *sendgrid) Send(to, from, subject, textBody, htmlBody string) error {
+	const op = "cloud/mail/sendgrid.Send"
+	if textBody == "" && htmlBody == "" {
+		return errors.E(op, errors.Invalid, errors.Str("text or html body must be filled"))
+	}
+	msg := message{
+		Personalizations: []personalizations{
+			{
+				To: []addr{
+					{
+						Email: to,
+					},
+				},
+				Subject: subject,
+			},
+		},
+		From: addr{
+			Email: fmt.Sprintf("%s@%s", from, s.domain),
+		},
+		// The order plain, html is important.
+		Content: []content{
+			{
+				Type:  "text/plain",
+				Value: textBody,
+			},
+			{
+				Type:  "text/html",
+				Value: htmlBody,
+			},
+		},
+	}
+	if textBody == "" {
+		msg.Content = msg.Content[1:]
+	}
+	if htmlBody == "" {
+		msg.Content = msg.Content[:1]
+	}
+
+	data, err := json.Marshal(&msg)
+	if err != nil {
+		return errors.E(op, errors.IO, err)
+	}
+	req, err := http.NewRequest("POST", apiSend, bytes.NewBuffer(data))
+	if err != nil {
+		return errors.E(op, errors.IO, err)
+	}
+	req.Header.Set("Authorization", "Bearer "+s.apikey)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return errors.E(op, errors.IO, err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusAccepted {
+		errStr, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return errors.E(op, errors.IO, err)
+		}
+		return errors.E(op, errors.IO, errors.Str(string(errStr)))
+	}
+
+	return nil
+}
+
+// Domain implements cloud.mail.Mail.
+func (s *sendgrid) Domain() string {
+	return s.domain
+}
