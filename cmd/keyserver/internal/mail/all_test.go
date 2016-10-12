ceedbad5246c9b00cd7f0a972c5cdd7e8ee6027b
Eduardo Pinheiro <edpin@google.com>
2016-10-12 12:41:04 -0700

    cmd/keyserver/internal/mail: add email parsing functions
    These functions will be used by the keyserver to
    process signup requests via email.
    
    Added test with real data as captured by manual
    testing.
    
    All tests pass.
    No new lint or vet errors.
    
    Change-Id: Ia21dbb44fef9e74965ed97e88966be7c435af9e9

---
+++ cmd/keyserver/internal/mail/all_test.go
@@ -0,0 +1,211 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mail
+
+import (
+	"bytes"
+	"net/http"
+	"strings"
+	"testing"
+
+	"upspin.io/errors"
+	"upspin.io/upspin"
+)
+
+func TestParseMail(t *testing.T) {
+	req := newRequest(t, emailPassesDKIM)
+	from, body, err := ParseMail(req.Form)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := "dude@gmail.com"
+	if from != expected {
+		t.Errorf("from = %q, want = %q", from, expected)
+	}
+	expected = "Test body!\n"
+	if body != expected {
+		t.Errorf("body = %q, want = %q", body, expected)
+	}
+}
+
+func TestParseMailFailDKIM_DomainMismatch(t *testing.T) {
+	req := newRequest(t, emailDKIMDomainMismatch)
+	_, _, err := ParseMail(req.Form)
+	expectedErr := errors.E(errors.Permission, errors.Str(`DKIM not present for domain "vanity.org"`))
+	if !errors.Match(expectedErr, err) {
+		t.Fatalf("err = %v, want = %s", err, expectedErr)
+	}
+}
+
+func TestParseMailFailDKIM_HardFailure(t *testing.T) {
+	failDKIM := strings.Replace(emailPassesDKIM, "@gmail.com : pass", "@gmail.com : fail", -1)
+	req := newRequest(t, failDKIM)
+	_, _, err := ParseMail(req.Form)
+	expectedErr := errors.E(errors.Permission, errors.Str(`DKIM failed for domain "gmail.com"`))
+	if !errors.Match(expectedErr, err) {
+		t.Fatalf("err = %v, want = %s", err, expectedErr)
+	}
+}
+
+func TestParseBody(t *testing.T) {
+	const valid = `
+Some garbage
+-----
+I'm foo@bar.com
+My public key is
+p256
+1063349993423423435345345345345345340
+3453453457828271720003453453245354698
+Signature: 1223234235345834578abc
+--- more garbage`
+
+	userName, pubKey, sig, err := ParseBody(valid)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := userName, upspin.UserName("foo@bar.com"); got != want {
+		t.Errorf("userName = %q, want = %q", got, want)
+	}
+	if got, want := pubKey, upspin.PublicKey("p256\n1063349993423423435345345345345345340\n3453453457828271720003453453245354698"); got != want {
+		t.Errorf("pubKey = %q, want = %q", got, want)
+	}
+	if got, want := sig, "1223234235345834578abc"; got != want {
+		t.Errorf("sig = %q, want = %q", got, want)
+	}
+}
+
+func newRequest(t *testing.T, data string) *http.Request {
+	req, err := http.NewRequest("POST", "some.url/incoming", bytes.NewReader([]byte(data)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "multipart/form-data; boundary=xYzZY")
+	err = req.ParseMultipartForm(1024)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return req
+}
+
+// These requests are real, abbreviated incoming messages with some of the
+// contents changed or simplified.
+const emailDKIMDomainMismatch = `--xYzZY
+Content-Disposition: form-data; name="headers"
+
+MIME-Version: 1.0
+Sender: zardoz@gmail.com
+Received: by 10.13.226.132 with HTTP; Tue, 11 Oct 2016 14:00:24 -0700 (PDT)
+From: Zardoz <zardoz@vanity.org>
+Date: Tue, 11 Oct 2016 14:00:24 -0700
+Subject: the rain in spain
+To: foo@key.upspin.io
+
+--xYzZY
+Content-Disposition: form-data; name="dkim"
+
+{@gmail.com : pass}
+--xYzZY
+Content-Disposition: form-data; name="to"
+
+foo@key.upspin.io
+--xYzZY
+Content-Disposition: form-data; name="html"
+
+<div dir="ltr">falls on my head</div>
+
+--xYzZY
+Content-Disposition: form-data; name="from"
+
+Zardoz <zardoz@vanity.org>
+--xYzZY
+Content-Disposition: form-data; name="text"
+
+falls on my head
+
+--xYzZY
+Content-Disposition: form-data; name="sender_ip"
+
+209.85.213.182
+--xYzZY
+Content-Disposition: form-data; name="envelope"
+
+{"to":["foo@key.upspin.io"],"from":"zardoz@gmail.com"}
+--xYzZY
+Content-Disposition: form-data; name="attachments"
+
+0
+--xYzZY
+Content-Disposition: form-data; name="subject"
+
+the rain in spain
+--xYzZY
+Content-Disposition: form-data; name="spam_score"
+
+0.011
+--xYzZY
+Content-Disposition: form-data; name="charsets"
+
+{"to":"UTF-8","html":"UTF-8","subject":"UTF-8","from":"UTF-8","text":"UTF-8"}
+--xYzZY
+Content-Disposition: form-data; name="SPF"
+
+pass
+--xYzZY--
+
+`
+
+const emailPassesDKIM = `--xYzZY
+Content-Disposition: form-data; name="headers"
+
+Received: by 10.13.227.130 with HTTP; Tue, 11 Oct 2016 13:45:32 -0700 (PDT)
+From: Some Dude <dude@gmail.com>
+Date: Tue, 11 Oct 2016 13:45:32 -0700
+Subject: subject!
+To: foo@key.upspin.io
+
+--xYzZY
+Content-Disposition: form-data; name="dkim"
+
+{@gmail.com : pass}
+--xYzZY
+Content-Disposition: form-data; name="to"
+
+foo@key.upspin.io
+--xYzZY
+Content-Disposition: form-data; name="html"
+
+<div dir="ltr">Test body!</div>
+
+--xYzZY
+Content-Disposition: form-data; name="from"
+
+Some Dude <dude@gmail.com>
+--xYzZY
+Content-Disposition: form-data; name="text"
+
+Test body!
+
+--xYzZY
+Content-Disposition: form-data; name="envelope"
+
+{"to":["foo@key.upspin.io"],"from":"dude@gmail.com"}
+--xYzZY
+Content-Disposition: form-data; name="attachments"
+
+0
+--xYzZY
+Content-Disposition: form-data; name="subject"
+
+subject!
+--xYzZY
+Content-Disposition: form-data; name="spam_score"
+
+0.022
+--xYzZY
+Content-Disposition: form-data; name="SPF"
+
+pass
+--xYzZY--
+`
---
+++ cmd/keyserver/internal/mail/parse.go
@@ -0,0 +1,153 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package mail provides email parsing utilities for keyserver.
+package mail
+
+import (
+	"bufio"
+	"fmt"
+	goMail "net/mail"
+	"net/url"
+	"strings"
+
+	"upspin.io/errors"
+	"upspin.io/upspin"
+	"upspin.io/user"
+)
+
+// Fields possibly present in email headers.
+const (
+	hdrDKIM = "dkim"
+	hdrFrom = "from"
+	hdrSPF  = "SPF"
+
+	text = "text"
+	pass = "pass"
+)
+
+// ParseMail parses the headers and body of an email message, returning the from
+// address and the email text body. ParseMail is only successful if the from
+// address passes DKIM and SPF checks. No validation is done on the email body.
+func ParseMail(data url.Values) (from string, body string, err error) {
+	const op = "cmd/keyserver/internal/mail.ParseMail"
+	from = data.Get(hdrFrom)
+	if from == "" {
+		return "", "", errors.E(op, errors.Invalid, errors.Str("no From: field in email header"))
+	}
+	// Perhaps from is encoded like this "Some dude <dude@gmail.com>".
+	addr, err := goMail.ParseAddress(from)
+	if err != nil {
+		return "", "", errors.E(op, errors.Invalid, err)
+	}
+	from = addr.Address
+
+	_, _, domain, err := user.Parse(upspin.UserName(from))
+	if err != nil {
+		return "", "", errors.E(op, err)
+	}
+	dkim := data.Get(hdrDKIM)
+	if dkim == "" {
+		return "", "", errors.E(op, errors.IO, errors.Str("no DKIM information present"))
+	}
+	dkim = strings.Trim(dkim, "{}")
+	fields := strings.Split(dkim, ":")
+	if len(fields) != 2 {
+		return "", "", errors.E(op, errors.Internal, errors.Errorf("DKIM information not parseable"))
+	}
+	dkimDomain := strings.TrimSpace(fields[0][1:]) // remove spaces and leading "@"
+	dkimStatus := strings.TrimSpace(fields[1])
+	// TODO: we could allow a whitelist of a few domains we trust, such as
+	// gmail.com.
+	if dkimDomain != domain {
+		return "", "", errors.E(op, errors.Permission, errors.Errorf("DKIM not present for domain %q", domain))
+	}
+	if dkimStatus != pass {
+		return "", "", errors.E(op, errors.Permission, errors.Errorf("DKIM failed for domain %q", domain))
+	}
+	spf := data.Get(hdrSPF)
+	if spf == "" {
+		return "", "", errors.E(op, errors.Permission, errors.Str("SPF information not present"))
+	}
+	if spf != pass {
+		return "", "", errors.E(op, errors.Permission, errors.Str("SPF failed"))
+	}
+
+	// Get the email body, in text format.
+	// TODO: maybe parse HTML if text is empty?
+	body = data.Get(text)
+	if text == "" {
+		return "", "", errors.E(op, errors.Invalid, errors.Str("empty email body"))
+	}
+	return
+}
+
+// States in the email parsing state machine.
+// Refer to this template to understand the states:
+/*
+   I’m foo@bar.com
+   My public key is
+   p256
+   1063349993423423435345345345345345340
+   3453453457828271720003453453245354698
+   Signature: ff2343ab34f43554d234e325ffac389000
+*/
+const (
+	email = iota
+	keyPreamble
+	key
+	signature
+)
+
+// ParseBody parses the contents of the email and returns the user name,
+// public key and signature of the contents. No validation is performed other
+// than the format of the email.
+func ParseBody(data string) (upspin.UserName, upspin.PublicKey, string, error) {
+	const op = "cmd/keyserver/internal/mail.ParseBody"
+	s := bufio.NewScanner(strings.NewReader(data))
+	state := email
+	keyLine := 0
+	var userName upspin.UserName
+	var pubKey string
+	var sig string
+Outer:
+	for s.Scan() {
+		switch state {
+		case email:
+			var value string
+			n, _ := fmt.Sscanf(s.Text(), "I'm %s", &value)
+			if n != 1 {
+				// Keep going until we find where it begins.
+				continue
+			}
+			userName = upspin.UserName(value)
+			state = keyPreamble
+		case keyPreamble:
+			_, err := fmt.Sscanf(s.Text(), "My public key is")
+			if err != nil {
+				return "", "", "", errors.E(op, errors.Invalid, errors.Str("missing public key preamble"))
+			}
+			state = key
+		case key:
+			pubKey = pubKey + s.Text()
+			if keyLine < 2 {
+				pubKey += "\n"
+			}
+			keyLine++
+			if keyLine == 3 {
+				state = signature
+			}
+		case signature:
+			n, err := fmt.Sscanf(s.Text(), "Signature: %s", &sig)
+			if n != 1 || err != nil {
+				return "", "", "", errors.E(op, errors.Invalid, errors.Str("missing signature"))
+			}
+			break Outer
+		}
+	}
+	if s.Err() != nil {
+		return "", "", "", errors.E(op, s.Err())
+	}
+	return userName, upspin.PublicKey(pubKey), sig, nil
+}
