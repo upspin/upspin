8b3435c903670c00454bf6d79a333cdd4509afd4
Andrew Gerrand <adg@golang.org>
2016-11-04 13:34:41 +1100

    serverutil,dir/inprocess: move Glob logic out of server
    The logic for Glob is more or less the same between dir/inprocess
    and dir/server, and future servers will also want to implement Glob.
    
    This change factors out the core logic of Glob and bases it upon
    DirServer.Lookup and a new function List, which lists the contents
    of a directory. With both Lookup and List doing permission checks,
    there's no need for permission checking in Glob.
    This way, the whole picture becomes a lot clearer.
    
    The new List function is not part of the DirServer interface,
    but we might consider putting it there.
    
    As-is, this change breaks a few tests. This is because it relies on a
    Lookup succeeding when the user only has List rights over the given
    path. I filed issue #95 to discuss this.
    I'll hold off on submitting this CL until that issue is resolved.
    
    Change-Id: I971579a12cdcf8aa3dc6e42179c97824d7acb2a2

---
+++ serverutil/glob_test.go
@@ -0,0 +1,144 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package serverutil
+
+import (
+	"strings"
+	"testing"
+
+	"upspin.io/errors"
+	"upspin.io/upspin"
+)
+
+var errLink = upspin.ErrFollowLink
+
+func TestGlob(t *testing.T) {
+	const (
+		user       = "user@example.com"
+		root       = user + "/"
+		dir        = user + "/dir"
+		file       = dir + "/file"
+		link       = dir + "/link"
+		linkTarget = "user@example.org/somewhere"
+		private    = dir + "/private"
+		public     = dir + "/public"
+		pubFile    = public + "/file"
+		pubDir     = public + "/dir"
+		pubDirFile = pubDir + "/file"
+	)
+	lookup := func(name upspin.PathName) (*upspin.DirEntry, error) {
+		t.Logf("lookup(%q)", name)
+		de := &upspin.DirEntry{
+			Name: name,
+			Attr: upspin.AttrDirectory,
+		}
+		switch name {
+		case root, dir, private, public, pubDir:
+			return de, nil
+		case file, pubFile, pubDirFile:
+			de.Attr = upspin.AttrNone
+			return de, nil
+		case link:
+			de.Attr = upspin.AttrLink
+			de.Link = linkTarget
+			return de, nil
+		default:
+			if strings.HasPrefix(string(name), link+"/") {
+				return &upspin.DirEntry{
+					Name: link,
+					Attr: upspin.AttrLink,
+					Link: linkTarget,
+				}, errLink
+			}
+			return nil, errNotExist
+		}
+		return nil, nil
+	}
+	ls := func(name upspin.PathName) ([]*upspin.DirEntry, error) {
+		t.Logf("ls(%q)", name)
+		switch name {
+		case root:
+			// Let's say we have read permission for the root,
+			// but not list.
+			return nil, errPermission
+		case dir:
+			// Return the entries deliberately out-of-order to make
+			// sure they are sorted.
+			return []*upspin.DirEntry{
+				{
+					Name: private,
+					Attr: upspin.AttrDirectory,
+				},
+				{
+					Name: public,
+					Attr: upspin.AttrDirectory,
+				},
+				{
+					Name: link,
+					Attr: upspin.AttrLink,
+					Link: linkTarget,
+				},
+				{
+					Name: file,
+				},
+			}, nil
+		case private:
+			return nil, errPermission
+		case public:
+			return []*upspin.DirEntry{
+				{
+					Name: pubDir,
+					Attr: upspin.AttrDirectory,
+				},
+				{
+					Name: pubFile,
+				},
+			}, nil
+		case pubDir:
+			return []*upspin.DirEntry{
+				{
+					Name: pubDirFile,
+				},
+			}, nil
+		default:
+			return nil, errNotExist
+		}
+	}
+
+	testGlob := func(pattern string, matchErr error, names ...upspin.PathName) {
+		t.Logf("Glob(%q)", pattern)
+		entries, err := Glob(pattern, lookup, ls)
+		if err != matchErr && !errors.Match(matchErr, err) {
+			t.Fatalf("Glob(%q): error: %v, want %v", pattern, err, matchErr)
+		}
+		if err := matchEntries(entries, names...); err != nil {
+			t.Fatalf("Glob(%q): %v", pattern, err)
+		}
+	}
+
+	testGlob(root, nil, root)
+	testGlob(root+"/*", errPermission)
+	testGlob(user+"/dir", nil, dir)
+	testGlob(user+"/dir/*", nil, file, link, private, public)
+	testGlob(user+"/dir/*/*", errLink, link, pubDir, pubFile)
+	testGlob(user+"/dir/*/file", errLink, link, pubFile)
+	testGlob(user+"/dir/p*/file", nil, pubFile)
+	testGlob(user+"/dir/p*/*", nil, pubDir, pubFile)
+	testGlob(user+"/dir/p*/*/*", nil, pubDirFile)
+	testGlob(user+"/dir/*/dir/*", errLink, link, pubDirFile)
+}
+
+func matchEntries(entries []*upspin.DirEntry, names ...upspin.PathName) error {
+	if len(entries) != len(names) {
+		return errors.Errorf("got %d entries, want %d", len(entries), len(names))
+	}
+	for i, e := range entries {
+		if e.Name != names[i] {
+			return errors.Errorf("entry %d named %q, want %q", i, e.Name, names[i])
+		}
+	}
+
+	return nil
+}
