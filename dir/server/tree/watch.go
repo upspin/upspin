765d6dfa7085bd42b89beef866919465b442a2aa
Eduardo Pinheiro <edpin@google.com>
2016-11-07 15:25:38 -0800

    dir/server/tree: add Watch
    Implements Watch on the Tree.
    
    A watcher hangs off of a node in the Tree that corresponds
    to the path to be watched. When updates happen to a node,
    its watchers and all ancestor node's watchers receive Events
    on their channels.
    
    Dead watchers are removed every time we traverse a path that
    contains watchers. A dead watcher is a watcher where the caller's
    done channel has been closed or one where an irrecoverable error
    has occurred.
    
    Currently, a goroutine is started for each watcher. The goroutine
    is necessary because the order requested may be far in the past and
    the operation may take long to complete. Once all Events have been
    caught up, the goroutine is no longer needed (there's a TODO to
    add a little synchronization to switch from background updates to
    real-time. However, we may choose to keep the watcher in a
    goroutine indefinitely as the receiver may take a very long time to
    read and we don't want to block the Tree indefinitely).
    
    Updates #89
    
    All tests pass with -race on.
    No new lint or vet errors.
    
    Change-Id: I0042c9cae9fbd2942b94371ff537f6598c8aa0b0

---
+++ dir/server/tree/watch.go
@@ -0,0 +1,336 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tree
+
+// TODOs:
+// - The watcher "tails" the log, starting from a given order number. It is
+//   done in a goroutine because the order can be very far from current state
+//   and we don't want to block the caller until all such state is sent on the
+//   Event channel. However, once the watcher has caught up with the current
+//   state of the Tree, there's no longer a need for a goroutine or for reading
+//   the log directly (and thus spend time in disk I/O, unmarshalling, etc). We
+//   can simply note that the end of file was reached, quit the goroutine and
+//   send events as they come in. This requires some extra synchronization code
+//   and ensuring that sending does not block the Tree (we can keep the
+//   goroutine if we don't want to impose a short timeout on the channel).
+
+import (
+	"strings"
+	"time"
+
+	"upspin.io/errors"
+	"upspin.io/log"
+	"upspin.io/path"
+	"upspin.io/upspin"
+)
+
+const (
+	// watcherTimeout is the timeout to send notifications on a watcher
+	// channel. It happens in a goroutine, so it's safe to hang for a while.
+	watcherTimeout = 1 * time.Minute
+)
+
+var errTimeout = errors.E(errors.IO, errors.Str("channel operation timed out"))
+
+// watcher holds together the done channel and the event channel for a given
+// watch point.
+type watcher struct {
+	// The path name this watcher watches.
+	path path.Parsed
+
+	// ch is the Event channel with the client. It is write-only.
+	events chan *upspin.Event
+
+	// done is the client's done channel. When it's closed, this watcher
+	// dies.
+	done <-chan struct{}
+
+	// hasWork is an internal channel that the Tree uses to tell the watcher
+	// goroutine to look for work at the end of the log.
+	hasWork chan bool
+
+	// log is a read-only cloned instance of the Tree's log that keeps track
+	// of this watcher's progress.
+	log *Log
+}
+
+// Watch implements upspin.DirServer.Watch.
+func (t *Tree) Watch(p path.Parsed, order int64, done <-chan struct{}) (<-chan *upspin.Event, error) {
+	const op = "dir/server/tree.Watch"
+
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	// Clone the logs so we can keep reading it while the current tree
+	// continues to be updated (we're about to unlock this tree).
+	cLog, err := t.log.Clone()
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+
+	// Create a watcher, but do not attach it to any node yet.
+	// TODO: limit number of watchers on any given node/tree?
+	ch := make(chan *upspin.Event)
+	w := &watcher{
+		path:    p,
+		events:  ch,
+		done:    done,
+		hasWork: make(chan bool, 1),
+		log:     cLog,
+	}
+
+	if order == -1 {
+		// Send the current state first. We must flush the tree so we
+		// know our logs are current (or we need to recover the tree
+		// from the logs).
+		err := t.flush()
+		if err != nil {
+			return nil, errors.E(op, err)
+		}
+
+		// Make a copy of the tree so we have an immutable tree in
+		// memory, at a fixed log position.
+		cIndex, err := t.logIndex.Clone()
+		if err != nil {
+			return nil, errors.E(op, err)
+		}
+		offset := t.log.LastOffset()
+		clone := &Tree{
+			user:     t.user,
+			context:  t.context,
+			packer:   t.packer,
+			log:      cLog,
+			logIndex: cIndex,
+		}
+		// Start sending the current state of the cloned tree and setup
+		// the watcher for this tree once the current state is sent.
+		go w.sendCurrentAndWatch(clone, t, p, offset)
+	} else {
+		// Set up the notification hook.
+		err = t.addWatcher(p, w)
+		if err != nil {
+			return nil, errors.E(op, err)
+		}
+
+		// Start the watcher.
+		go w.watch(order)
+	}
+
+	return w.events, nil
+}
+
+// addWatcher adds a watcher to the node at a given path location.
+// t.mu must be held.
+func (t *Tree) addWatcher(p path.Parsed, w *watcher) error {
+	n, _, err := t.loadPath(p)
+	if err != nil {
+		return err
+	}
+	n.watchers = append(n.watchers, w)
+	return nil
+}
+
+// sendCurrentAndWatch takes an original tree and its clone and sends the state
+// of the clone starting from the subtree rooted at p. The offset refers to the
+// last log offset saved by the original tree.
+// It must run in a goroutine. Errors are logged.
+func (w *watcher) sendCurrentAndWatch(clone, orig *Tree, p path.Parsed, offset int64) {
+	const op = "dir/server/tree.sendCurrentAndWatch"
+
+	n, _, err := clone.loadPath(p)
+	if err != nil {
+		log.Error.Printf("%s: %s", op, err)
+		return
+	}
+	fn := func(n *node, level int) error {
+		logEntry := &LogEntry{
+			Op:    Put,
+			Entry: n.entry,
+		}
+		err = w.sendEvent(logEntry, offset)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	err = clone.traverse(n, 0, fn)
+	if err != nil {
+		log.Error.Printf("%s: %s", op, err)
+		return
+	}
+
+	// Set up the notification hook on the original tree. We must lock it.
+	orig.mu.Lock()
+	err = orig.addWatcher(p, w)
+	orig.mu.Unlock()
+	if err != nil {
+		log.Error.Printf("%s: %s", op, err)
+		return
+	}
+	// Start the watcher (in this goroutine -- don't start a new one here).
+	w.watch(offset)
+}
+
+// sendEvent sends a single logEntry read from the log at offset position
+// to the event channel. If the channel blocks for longer than watcherTimeout,
+// the operation fails and the watcher is invalidated (marked for deletion).
+func (w *watcher) sendEvent(logEntry *LogEntry, offset int64) error {
+	if w.events == nil {
+		return errors.E(errors.Internal, errors.Str("send on a nil event channel"))
+	}
+	var event *upspin.Event
+	// Strip block information for directories. We avoid an extra copy
+	// if it's not a directory.
+	if logEntry.Entry.IsDir() {
+		entry := logEntry.Entry
+		entry.MarkIncomplete()
+		event = &upspin.Event{
+			Order:  offset,
+			Delete: logEntry.Op == Delete,
+			Entry:  &entry,
+		}
+	} else {
+		event = &upspin.Event{
+			Order:  offset,
+			Delete: logEntry.Op == Delete,
+			Entry:  &logEntry.Entry,
+		}
+	}
+	select {
+	case w.events <- event:
+		// Event was sent.
+	case <-time.After(watcherTimeout):
+		// TODO: time.After leaks. Use NewTimer.
+		// Oops. Client didn't read fast enough.
+		// Can't send an error since we timed out. Close the
+		// channel and log an error.
+		if w.events != nil {
+			// The watcher will be removed from the node's
+			// watcher slice on the next operation on the Tree.
+			close(w.events)
+			w.events = nil
+		}
+		log.Error.Printf("dir/server/tree.sendNotifications: %s", errTimeout)
+		return errTimeout
+	}
+	return nil
+}
+
+// sendEventFromLog sends notifications to the given watcher for all
+// descendant entries of a target path, reading from the given log starting at a
+// given offset until it reaches the end of the log. It returns the next offset
+// to read.
+func (w *watcher) sendEventFromLog(offset int64) (int64, error) {
+	curr := offset
+	for {
+		logs, next, err := w.log.ReadAt(1, curr)
+		if err != nil {
+			return next, err
+		}
+		if len(logs) != 1 {
+			// End of log.
+			return next, nil
+		}
+		curr = next
+		logEntry := logs[0]
+		path := logEntry.Entry.SignedName
+		if !isPrefixPath(path, w.path) {
+			// Not a log of interest.
+			continue
+		}
+		err = w.sendEvent(&logEntry, curr)
+		if err != nil {
+			return 0, err
+		}
+	}
+}
+
+// watch, which runs in a goroutine, reads from the log starting at a given
+// offset and sends notifications on the event channel until the end of the log
+// is reached. It waits to be notified of more work or until the client's
+// done channel is closed, in which case it terminates.
+func (w *watcher) watch(offset int64) {
+	for {
+		var err error
+		offset, err = w.sendEventFromLog(offset)
+		if err != nil {
+			log.Error.Printf("dir/server/tree.watch: %s", err)
+			if err != errTimeout {
+				w.events <- &upspin.Event{
+					Error: err,
+				}
+			}
+			return
+		}
+		select {
+		case <-w.done:
+			// Done channel was closed. Quit this goroutine.
+			if w.events != nil {
+				close(w.events)
+			}
+			return
+		case <-w.hasWork:
+			// Wake up and work from where we left off.
+		}
+	}
+}
+
+// removeDeadWatchers removes all watchers on a node that have closed their done
+// or Event channels.
+func removeDeadWatchers(n *node) {
+	curr := 0
+	for i := 0; i < len(n.watchers); i++ {
+		doneCh := n.watchers[i].done
+		// Any nil channel means this watcher is dead.
+		closed := n.watchers[i].events == nil || doneCh == nil
+		if !closed {
+			// If the done channel is ready, it's closed.
+			select {
+			case <-doneCh:
+				closed = true
+			default:
+			}
+		}
+		if closed {
+			// Remove this entry. If there are more, simply copy the
+			// next one over the ith entry. Otherwise just shrink
+			// the slice.
+			if i > curr {
+				n.watchers[curr] = n.watchers[i]
+			}
+			continue
+		}
+		curr++
+	}
+	n.watchers = n.watchers[:curr]
+}
+
+// notifyWatchers tells all watchers there are new entries in the log to be
+// processed.
+func notifyWatchers(watchers []*watcher) {
+	for _, w := range watchers {
+		select {
+		case w.hasWork <- true:
+			// OK, sent.
+		default:
+			// Watcher is busy. It will get to it eventually.
+		}
+	}
+}
+
+// isPrefixPath reports whether the path has a pathwise prefix.
+func isPrefixPath(name upspin.PathName, prefix path.Parsed) bool {
+	p := string(name)
+	pref := prefix.String()
+
+	if !strings.HasPrefix(p, pref) {
+		return false
+	}
+	if prefix.IsRoot() {
+		return true
+	}
+	// Make sure we are at a path element boundary.
+	return len(pref) == len(p) || p[len(pref)] == '/'
+}
