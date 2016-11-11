8bf725e163f1951fa33a512a49ae4b6a63f1664d
Andrew Gerrand <adg@golang.org>
2016-11-11 13:54:33 +1100

    cmd/keyserver,key/gcp: publish log for auditing purposes
    This change adds an audit log to the key/gcp package and a "/log" HTTP
    handler to the keyserver.
    
    It records both attempted Puts and successful Puts. "Attempted" in this
    case means that a valid, authenticated request was made, and we would
    try to put the record into storage. "Successful" means that the record
    was written to storage without any errors. We record both because it
    seems possible that the Cloud Storage API might possibly return an
    error, but still successfully have stored the key. If we only logged
    keys that were successfully stored then we might miss those unlikely
    cases.
    
    Rather than adding a Log method to all upspin.KeyServer implementations,
    I opted for only implementing it in key/gcp and exporting a Logger
    interface there. You can see how the keyserver command tests for the
    presence of that interface and uses that as a switch to enable the
    "/log" handler.
    
    Another approach would be to add the Log method to the upspin.KeyServer
    interface and have all KeyServer implementations do logging. This would
    also make GRPC clients of the KeyServer able to retrieve the logs. On
    the other hand, it adds a lot of complexity for seemingly minor gain.
    I implemented this approach in another CL, for contrast:
    	https://upspin-review.googlesource.com/c/6440/
    I think this change is better.
    
    Fixes #103
    
    Change-Id: I9fde3ec25be98a4b54ca07ef50e755cc7bef304f

---
+++ key/gcp/log.go
@@ -0,0 +1,137 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gcp
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	"upspin.io/cloud/storage"
+	"upspin.io/errors"
+	"upspin.io/upspin"
+)
+
+// Logger returns KeyServer logs for auditing purposes.
+// It is implemented by the KeyServer in this package,
+// but is not part of the upspin.KeyServer interface.
+type Logger interface {
+	Log() ([]byte, error)
+}
+
+type logger interface {
+	PutAttempt(actor upspin.UserName, u *upspin.User) error
+	PutSuccess(actor upspin.UserName, u *upspin.User) error
+	ReadAll() ([]byte, error)
+}
+
+// logRef is the name in Google Cloud Storage under which the log is stored.
+const logRef = "keyserver/log"
+
+type loggerImpl struct {
+	mu      sync.Mutex
+	log     []byte
+	storage storage.Storage
+}
+
+// PutAttempt records a KeyServer.Put attempt
+// by the given actor for the given user record.
+func (l *loggerImpl) PutAttempt(actor upspin.UserName, u *upspin.User) error {
+	if err := l.put("put attempt", actor, u); err != nil {
+		return errors.E("key/gcp.Logger.PutAttempt", err)
+	}
+	return nil
+}
+
+// PutSuccess records a successful KeyServer.Put
+// by the given actor for the given user record.
+func (l *loggerImpl) PutSuccess(actor upspin.UserName, u *upspin.User) error {
+	if err := l.put("put success", actor, u); err != nil {
+		return errors.E("key/gcp.Logger.PutSuccess", err)
+	}
+	return nil
+}
+
+var hashPrefix = []byte("SHA256:")
+
+func (l *loggerImpl) put(kind string, actor upspin.UserName, u *upspin.User) error {
+	content, err := json.Marshal(u)
+	if err != nil {
+		return err
+	}
+
+	now := time.Now().In(time.UTC)
+	record := []byte(fmt.Sprintf("%v: %s by %q: %s\n", now, kind, actor, content))
+
+	h := sha256.New()
+	h.Write([]byte(record))
+
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	// Include the previous record's hash in the new hash,
+	// but only if there is a previous record.
+	i := bytes.LastIndex(l.log, hashPrefix)
+	if i == -1 {
+		if len(l.log) > 0 {
+			return errors.Str("key log corrupt: non-empty but lacks previous record hash")
+		}
+		// No previous record; that's ok.
+	} else {
+		// Grab the hash hex, stripping the prefix and trailing newline.
+		prevHash := l.log[i+len(hashPrefix) : len(l.log)-1]
+		h.Write(prevHash)
+	}
+
+	if err := l.populate(); err != nil {
+		return err
+	}
+
+	l.log = append(l.log, record...)
+	l.log = append(l.log, fmt.Sprintf("%s%x\n", hashPrefix, h.Sum(nil))...)
+
+	_, err = l.storage.Put(logRef, l.log)
+	return err
+}
+
+// ReadAll returns the log bytes.
+func (l *loggerImpl) ReadAll() ([]byte, error) {
+	const op = "key/gcp.Logger.ReadAll"
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if err := l.populate(); err != nil {
+		return nil, errors.E(op, err)
+	}
+
+	// Make a copy of the log data to prevent tampering,
+	// however unlikely that may be.
+	data := append([]byte(nil), l.log...)
+	return data, nil
+}
+
+// populate reads log data from storage and populates l.log.
+// If l.log is already populated nothing happens.
+// l.mu must be held.
+func (l *loggerImpl) populate() error {
+	if l.log != nil {
+		return nil
+	}
+	data, err := l.storage.Download(logRef)
+	if errors.Match(errors.E(errors.NotExist), err) {
+		// The log doesn't exist yet.
+		// Make l.log non-nil so that we don't keep trying.
+		l.log = []byte{}
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	l.log = data
+	return nil
+}
