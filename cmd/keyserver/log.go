a6e0cb9add2f3eacf1a3b122e8b3a2ab0dc6fe15
Andrew Gerrand <adg@golang.org>
2016-11-11 13:54:33 +1100

    cmd/keyserver,key/gcp: publish log for auditing purposes
    This change adds an audit log to the key/gcp package and a "/log" HTTP
    handler to the keyserver.
    
    It records both attempted Puts and successful Puts. "Attempted" in this
    case means that a valid, authenticated request was made, and we would
    try to put the record into storage. "Successful" means that the record
    was written to storage without any errors. We record both because it
    seems possible that the Cloud Storage API might possibly return an
    error, but still successfully have stored the key. If we only logged
    keys that were successfully stored then we might miss those unlikely
    cases.
    
    Rather than adding a Log method to all upspin.KeyServer implementations,
    I opted for only implementing it in key/gcp and exporting a Logger
    interface there. You can see how the keyserver command tests for the
    presence of that interface and uses that as a switch to enable the
    "/log" handler.
    
    Another approach would be to add the Log method to the upspin.KeyServer
    interface and have all KeyServer implementations do logging. This would
    also make GRPC clients of the KeyServer able to retrieve the logs. On
    the other hand, it adds a lot of complexity for seemingly minor gain.
    I implemented this approach in another CL, for contrast:
    	https://upspin-review.googlesource.com/c/6440/
    I think this change is better.
    
    Fixes #103
    
    Change-Id: I9fde3ec25be98a4b54ca07ef50e755cc7bef304f
    Reviewed-on: https://upspin-review.googlesource.com/6441
    Reviewed-by: Eric Grosse <ehg@google.com>

---
+++ cmd/keyserver/log.go
@@ -0,0 +1,26 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"net/http"
+
+	"upspin.io/key/gcp"
+	"upspin.io/log"
+)
+
+type logHandler struct {
+	logger gcp.Logger
+}
+
+func (h logHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	data, err := h.logger.Log()
+	if err != nil {
+		log.Error.Printf("logHandler: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
---
+++ key/gcp/log_test.go
@@ -0,0 +1,19 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gcp
+
+import "upspin.io/upspin"
+
+type noopLogger struct{}
+
+func (noopLogger) PutAttempt(actor upspin.UserName, u *upspin.User) error {
+	return nil
+}
+func (noopLogger) PutSuccess(actor upspin.UserName, u *upspin.User) error {
+	return nil
+}
+func (noopLogger) ReadAll() ([]byte, error) {
+	return nil, nil
+}
