01e7a3831f4fc40dedf012c17efd2e77c7ee9543
Eduardo Pinheiro <edpin@google.com>
2016-11-17 15:31:46 -0800

    dir/server: add Watch
    Reads events from the Tree and passes them along to the
    DirServer's callers.
    
    Updates the Runner to more easily allow testing.
    
    Updates #89
    
    All tests pass.
    No new lint or vet errors.
    
    Change-Id: I5a96b1470d46e2c04631e3a9dfa98118df0dfd21

---
+++ test/watch_test.go
@@ -0,0 +1,101 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package test
+
+// This file tests the DirServer Watch API. It only works for the dir/server for
+// now since inprocess does not (yet?) support it.
+
+import (
+	"testing"
+
+	"upspin.io/errors"
+	"upspin.io/test/testenv"
+	"upspin.io/upspin"
+)
+
+func TestWatchCurrent(t *testing.T) {
+	ownerEnv, err := testenv.New(&testenv.Setup{
+		OwnerName: ownerName,
+		Packing:   upspin.PlainPack,
+		Kind:      "server",
+		Cleanup:   cleanup,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	readerEnv, err := testenv.New(&testenv.Setup{
+		OwnerName: readerName,
+		Packing:   upspin.PlainPack,
+		Kind:      "server",
+		Cleanup:   cleanup,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	const (
+		hasBlocks     = true
+		base          = ownerName + "/watch-test"
+		file          = base + "/testfile"
+		access        = base + "/Access"
+		accessContent = "*: " + ownerName
+	)
+
+	r := testenv.NewRunner()
+	r.AddUser(ownerEnv.Context)
+	r.AddUser(readerEnv.Context)
+
+	r.As(ownerName)
+	r.MakeDirectory(base)
+	r.Put(file, "something")
+	if r.Failed() {
+		t.Fatal(r.Diag())
+	}
+
+	r.DirWatch(base, -1)
+	if !r.GotEvent(base, !hasBlocks) {
+		t.Fatal(r.Diag())
+	}
+	if !r.GotEvent(file, hasBlocks) {
+		t.Fatal(r.Diag())
+	}
+
+	// Put an Access file; watch it appear on the channel.
+	r.Put(access, accessContent)
+	if !r.GotEvent(access, hasBlocks) {
+		t.Fatal(r.Diag())
+	}
+
+	// Reader does not have rights to set a watcher.
+	r.As(readerName)
+	r.DirWatch(base, -1)
+	if !r.Match(errors.E(errors.Private, upspin.PathName(base))) {
+		t.Fatal(r.Diag())
+	}
+
+	// Allow reader to list, but not read.
+	r.As(ownerName)
+	r.Put(access, "l: "+readerName+"\n*:"+ownerName)
+
+	r.As(readerName)
+	done := r.DirWatch(base, -1)
+	if r.Failed() {
+		t.Fatal(r.Diag())
+	}
+	if !r.GotEvent(base, !hasBlocks) {
+		t.Fatal(r.Diag())
+	}
+	if !r.GotEvent(access, !hasBlocks) { // No blocks returned!
+		t.Fatal(r.Diag())
+	}
+	if !r.GotEvent(file, !hasBlocks) { // No blocks returned!
+		t.Fatal(r.Diag())
+	}
+	close(done)
+	e, ok := <-r.Events
+	if ok {
+		t.Fatalf("Channel had more events: %v", e)
+	}
+}
