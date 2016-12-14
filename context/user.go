3d5cf676d8c028c3c07fb01a82f11e60a8ea77e2
Andrew Gerrand <adg@golang.org>
2016-12-15 09:50:03 +1100

    context: add User function to convert upspin.Context to upspin.User
    Change-Id: I5e87b363468daf21aa17ae104c5b2cba700c1008

---
+++ context/user.go
@@ -0,0 +1,22 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package context creates a client context from various sources.
+package context
+
+import "upspin.io/upspin"
+
+// User returns an upspin.User record for the user in the given context.
+func User(ctx upspin.Context) *upspin.User {
+	var key upspin.PublicKey
+	if f := ctx.Factotum(); f != nil {
+		key = f.PublicKey()
+	}
+	return &upspin.User{
+		Name:      ctx.UserName(),
+		Dirs:      []upspin.Endpoint{ctx.DirEndpoint()},
+		Stores:    []upspin.Endpoint{ctx.StoreEndpoint()},
+		PublicKey: key,
+	}
+}
