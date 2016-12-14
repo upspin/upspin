79b331c1e44757c5be78ee733af754523178e4d8
Andrew Gerrand <adg@golang.org>
2016-12-15 10:36:12 +1100

    context: remove superfluous comment
    Change-Id: Ib5ef40c93a90048db772f9855a2197f54bea55df

--- context/user.go
+++ context/user.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package context creates a client context from various sources.
 package context
 
 import "upspin.io/upspin"
