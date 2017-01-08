d4f0ef5effa389ae424e852e55eba8b2d31baf5f
Dave Presotto <presotto@gmail.com>
2017-01-07 16:19:57 -0800

    cmd/upspinfs: have it start the cache server if needed.
    I added a doc.go so that one doesn't have to guess how
    to use it. There are also scripts for mounting it on
    darwin and linux.
    
    Change-Id: I01db30030963a2fbff8f360d1c38dfdf0e700e78

---
+++ cmd/upspinfs/doc.go
@@ -0,0 +1,36 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !windows
+
+/*
+Command upspinfs is a FUSE interface for Upspin. It presents Upspin
+files as a locally mounted file system.
+
+If $HOME/upspin/rc specifies a cache server endpoint and cacheserver
+is not running, upspinfs will attempt to start it. Upspinfs will pass
+to cacheserver both its -log and -cache flags.
+
+Usage:
+	upspinfs [flags] mountpoint
+
+	where 'mountpoint' is an existing directory upon which to mount the Upspin namespace.
+
+The flags are:
+
+	-log=level
+ 		Set the log level to 'level'.
+	-cache=directory
+		Cache all files being read or written in 'directory'/fscache
+
+Examples:
+
+	% mkdir $HOME/ufs
+	% upspinfs $HOME/ufs &
+	% ls -l $HOME/ufs/tester@tester.com
+	% ...
+	% killall -9 upspinfs
+	% umount $HOME/ufs
+*/
+package main
