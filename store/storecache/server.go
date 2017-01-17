6b26b0d6e67a88328b4f1e821e2c2f05df8e5900
Rob Pike <r@golang.org>
2017-01-17 16:19:58 +1100

    store/*: rename context to config
    Also rename ctx to cfg
    
    This is the last one, except for some cleanups after more grepping.
    
    Change-Id: Ifd717d4e28393e8b2b2eec3de30ef23744fe570c
    Reviewed-on: https://upspin-review.googlesource.com/7053
    Reviewed-by: Andrew Gerrand <adg@google.com>

--- store/storecache/server.go
+++ store/storecache/server.go
@@ -17,7 +17,7 @@ import (
 
 // server implements upspin.Storeserver.
 type server struct {
-	ctx upspin.Config
+	cfg upspin.Config
 
 	// The on disk cache.
 	cache *storeCache
@@ -27,18 +27,18 @@ type server struct {
 }
 
 // New creates a new store cache that implements upspin.StoreServer.
-func New(ctx upspin.Config, cacheDir string, maxBytes int64) (upspin.StoreServer, error) {
+func New(cfg upspin.Config, cacheDir string, maxBytes int64) (upspin.StoreServer, error) {
 	c, err := newCache(path.Join(cacheDir, "storecache"), maxBytes)
 	if err != nil {
 		return nil, err
 	}
 	return &server{
-		ctx:   ctx,
+		cfg:   cfg,
 		cache: c,
 	}, nil
 }
 
-func (s *server) Dial(context upspin.Config, e upspin.Endpoint) (upspin.Service, error) {
+func (s *server) Dial(config upspin.Config, e upspin.Endpoint) (upspin.Service, error) {
 	s2 := *s
 	s.authority = e
 	return &s2, nil
@@ -53,7 +53,7 @@ func (s *server) Get(ref upspin.Reference) ([]byte, *upspin.Refdata, []upspin.Lo
 
 	op := logf("Get %q", ref)
 
-	data, locs, err := s.cache.get(s.ctx, ref, s.authority)
+	data, locs, err := s.cache.get(s.cfg, ref, s.authority)
 	if err != nil {
 		return nil, nil, nil, op.error(err)
 	}
@@ -72,7 +72,7 @@ func (s *server) Put(data []byte) (*upspin.Refdata, error) {
 
 	op := logf("Put %.30x...", data)
 
-	ref, err := s.cache.put(s.ctx, data, s.authority)
+	ref, err := s.cache.put(s.cfg, data, s.authority)
 	if err != nil {
 		return nil, op.error(err)
 	}
@@ -91,7 +91,7 @@ func (s *server) Delete(ref upspin.Reference) error {
 	}
 	op := logf("Delete %q", ref)
 
-	err := s.cache.delete(s.ctx, ref, s.authority)
+	err := s.cache.delete(s.cfg, ref, s.authority)
 	if err != nil {
 		return op.error(err)
 	}
