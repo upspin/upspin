3547e8ef95158aa58c0fa334c0f55f97dab54485
Eric Grosse <ehg@google.com>
2017-01-20 22:04:36 +0000

    Revert "cloud/https: Add Go 1.8 cipher suites."
    This reverts commit 2748bdec834936bd98e356e9e54a8f56aad5fb9a.
    
    Reason for revert: cdbuild doesn't have Go 1.8 yet.
    
    Change-Id: Ie8a0e67ecda3e31f8b101e779ed332eee3a2ed84

--- cloud/https/https.go
+++ cloud/https/https.go
@@ -164,14 +164,12 @@ func newDefaultTLSConfig(certFile string, certKeyFile string) (*tls.Config, erro
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 		},
 		MinVersion:               tls.VersionTLS12,
 		PreferServerCipherSuites: true, // Use our choice, not the client's choice
-		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256, tls.X25519},
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 		Certificates:             []tls.Certificate{cert},
 	}
 	tlsConfig.BuildNameToCertificate()
