fdd560202dba217932e54a0eb961e3773fca3f9f
[Eric Grosse] <[ehg@google.com]>
2017-01-23 12:13:58 -0800

    install-client.sh: Document how to build just client.
    "go install ./..." forces the initial user to fight dependencies
    too much while deciding if Upspin is worth their while.
    
    Also remove ~~sam~~ created accidentally.
    
    Change-Id: Icdfa95b9c2c2f226eea6ef4f422754dd9ac17cbf

---
+++ install-client.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Copyright 2017 The Upspin Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+# Build just the Upspin client tools, not the servers.
+# Run from the directory containing this script.
+set -e
+go get -u upspin.io/cmd/upspin upspin.io/cmd/upspinfs
+go install ./cmd/upspin ./cmd/upspinfs ./cmd/cacheserver
