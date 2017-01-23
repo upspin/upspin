4af57df1f9c6976e5b2290293ed00342ec708bde
Rob Pike <r@golang.org>
2017-01-24 10:23:06 +1100

    upspin: document that all methods of the package must return safe data
    The caller should not be able to modify internal data structures by changing
    the contents of items returned by these methods.
    
    Change-Id: Ia782ab2cd10d980475f967b6d3b0f517fd8fcb98

--- upspin/doc.go
+++ upspin/doc.go
@@ -3,46 +3,50 @@
 // license that can be found in the LICENSE file.
 
 /*
-Package upspin contains global interface and other definitions for the components of the system.
+Package upspin contains global interface and other definitions for the
+components of the system.
 
-Upspin provides a global hierarchical name space for accessing
-arbitrary user-defined data.  The interfaces for accessing and
-protecting data are general: Any service that implements the
-interfaces defined here, whether provided by the Upspin project
-itself or by an external agent that implements these interfaces as
-an alternate access method, contributes to the global name space.
+Upspin provides a global hierarchical name space for accessing arbitrary
+user-defined data. The interfaces for accessing and protecting data are
+general: Any service that implements the interfaces defined here, whether
+provided by the Upspin project itself or by an external agent that implements
+these interfaces as an alternate access method, contributes to the global name
+space.
 
-The name space itself is a single rooted tree. The root of the tree
-contains a subdirectory for each user, called the user root, that
-contains all directories and storage for that user. The user root
-is identified by the user's name, which has an email-like format.
-Thus the fully qualified name (called a path name) of an item looks
-analogous to
+The name space itself is a single rooted tree. The root of the tree contains a
+subdirectory for each user, called the user root, that contains all directories
+and storage for that user. The user root is identified by the user's name,
+which has an email-like format. Thus the fully qualified name (called a path
+name) of an item looks analogous to
 	user@name.com/file/name
 There is no leading slash in a path name.
 
 There are three fundamental services defined in Upspin.
 
-* The StoreServer is the ultimate location for user-provided data.  Every
-piece of user data stored in the system is identified by a Location,
-which identifies where it is stored and how to address it.
+* The StoreServer is the ultimate location for user-provided data. Every piece
+of user data stored in the system is identified by a Location, which identifies
+where it is stored and how to address it.
 
-* The DirServer implements the name space, binding hierarchical,
-Unix-like names to Locations of data stored in StoreServers.
-Directories also attach metadata to file names, which may include
-encryption/decryption keys or other access-control information.
+* The DirServer implements the name space, binding hierarchical, Unix-like
+names to Locations of data stored in StoreServers. Directories also attach
+metadata to file names, which may include encryption/decryption keys or other
+access-control information.
 
-* The KeyServer stores, for each registered user, the Location
-of the user root and any public encryption keys exported by that
-user.
+* The KeyServer stores, for each registered user, the Location of the user root
+and any public encryption keys exported by that user.
 
-Each of these three services is itself distributed, so for instance
-a user root may be hosted on or split across multiple DirServer
-instances, which coordinate to provide a consistent view of the user's
-data.
+Each of these three services is itself distributed, so for instance a user root
+may be hosted on or split across multiple DirServer instances, which coordinate
+to provide a consistent view of the user's data.
 
-Finally, the Client interface provides a coherent high-level file-like API that,
-internally, calls upon the other services to access and manage the data.
+Finally, the Client interface provides a coherent high-level file-like API
+that, internally, calls upon the other services to access and manage the data.
 Most applications using Upspin will talk to the Client interface alone.
+
+Many of the methods of the types in this package accept or return slices or
+pointers to data structures. It is a requirement that all implementations of
+these methods do not share data with the caller, that is, that data that can be
+modified safely by the method or the caller without affecting the memory of the
+caller or the method, respectively.
 */
 package upspin
