924ccef852f6eab2790844beee5d97e78e24efdb
Eduardo Pinheiro <edpin@google.com>
2017-01-23 14:59:13 -0800

    dir/server: ensure we're passing around copies, not references
    This prevents bugs where the Tree's internal structs are
    accidentally modified by the DirServer.
    
    Remove the explicit copies in the DirServer now that the Tree does not
    leak references.
    
    Adds test by adg@ to ensure no implementation of DirServer leaks
    references. The test fails without these fixes.
    
    Updates #106
    
    Tests pass.
    No new lint or vet errors.
    
    Change-Id: I17788a9404ef462231d1ee8ea3b9396546025d6c

---
+++ test/copy_test.go
@@ -0,0 +1,87 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package test
+
+import (
+	"testing"
+
+	"upspin.io/test/testenv"
+	"upspin.io/upspin"
+)
+
+// testCopyEntries tests that servers return copies of data instead of
+// references to their internal data structures.
+func testCopyEntries(t *testing.T, r *testenv.Runner) {
+	const (
+		dir           = ownerName + "/dir-entry-copy"
+		file          = dir + "/file"
+		content       = "file contents"
+		entryPackdata = "corrupted"
+		blockPackdata = "clobbered"
+	)
+
+	r.As(ownerName)
+	r.MakeDirectory(dir)
+	r.Put(file, content)
+	r.DirLookup(file)
+	if r.Failed() {
+		t.Fatal(r.Diag())
+	}
+	if r.Entry.Name != file {
+		t.Fatalf("entry name is %q, want %q", r.Entry.Name, file)
+	}
+	if len(r.Entry.Blocks) == 0 {
+		t.Fatal("expected 1 or more blocks, got zero")
+	}
+
+	// Mutating the entry that we received should not affect
+	// the copy that the server has.
+	r.Entry.Sequence = -20
+	if len(r.Entry.Packdata) >= 2 {
+		r.Entry.Packdata[0] = 0xde
+		r.Entry.Packdata[1] = 0xad
+	} else {
+		r.Entry.Packdata = []byte(entryPackdata)
+	}
+	if len(r.Entry.Blocks) > 0 && len(r.Entry.Blocks[0].Packdata) >= 2 {
+		r.Entry.Blocks[0].Packdata[0] = 0xbe
+		r.Entry.Blocks[0].Packdata[1] = 0xef
+	} else {
+		r.Entry.Blocks[0].Packdata = []byte(blockPackdata)
+	}
+
+	// Get the contents of dir and mess up the blocks. If the server
+	// sees the messed up data, it may crash or complain.
+	r.DirLookup(upspin.PathName(dir))
+	if r.Failed() {
+		t.Fatal(r.Diag())
+	}
+	// Clobber important fields.
+	r.Entry.Packing = upspin.EEIntegrityPack
+	r.Entry.Packdata = []byte(entryPackdata)
+	// Remove all blocks, so server can't find file unless this a copy.
+	r.Entry.Blocks = nil
+
+	// Read the file again. Verify it's all intact.
+	r.DirLookup(file)
+	if r.Failed() {
+		t.Fatal(r.Diag())
+	}
+	if r.Entry.Name != file {
+		t.Fatalf("entry name is %q, want %q", r.Entry.Name, file)
+	}
+	if r.Entry.Sequence == -20 {
+		t.Error("successfully clobbered sequence field")
+	}
+	if string(r.Entry.Packdata) == entryPackdata {
+		t.Error("successfully clobbered entry packdata field")
+	}
+	if len(r.Entry.Blocks) == 0 {
+		t.Fatal("expected 1 or more blocks, got zero")
+	}
+	if string(r.Entry.Blocks[0].Packdata) == blockPackdata {
+		t.Error("successfully clobbered block[0] packdata field")
+	}
+}
