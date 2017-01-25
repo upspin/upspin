153497ac4db1c064de432a47c8cd9f980d3dfd23
Rob Pike <r@golang.org>
2017-01-25 13:09:04 +1100

    test: fix handling of errors in watch-testing code
    Runner.Match clears the error, so testing for the error after a call to
    Match is fruitless. Rewrite the two uses in the Watch code to remember
    the error before calling Match.
    
    Bug discovered with new known-to-be-buggy Watch code in
    dir/inprocess not being caught by this test.
    
    Change-Id: I6f59a43987e3389a173127575f59b4238d6c7b8a

--- test/watch_test.go
+++ test/watch_test.go
@@ -32,12 +32,14 @@ func testWatchCurrent(t *testing.T, r *testenv.Runner) {
 	}
 
 	done := r.DirWatch(base, -1)
-	if r.Match(upspin.ErrNotSupported) {
-		t.Logf("Watch not supported for this DirServer.")
-		return
-	}
 	if r.Failed() {
-		t.Fatal(r.Diag())
+		// Match clears the error, so remember it.
+		err := r.Diag()
+		if r.Match(upspin.ErrNotSupported) {
+			t.Logf("Watch not supported for this DirServer.")
+			return
+		}
+		t.Fatal(err)
 	}
 	if !r.GotEvent(base, !hasBlocks) {
 		t.Fatal(r.Diag())
@@ -101,10 +103,14 @@ func testWatchErrors(t *testing.T, r *testenv.Runner) {
 		t.Fatal(r.Diag())
 	}
 
-	r.DirWatch(base, 3)
-	if r.Match(upspin.ErrNotSupported) {
-		t.Logf("Watch not supported in this DirServer")
-		return
+	r.DirWatch(base, -77) // -77 is an implausible order number.
+	if r.Failed() {
+		err := r.Diag()
+		if r.Match(upspin.ErrNotSupported) {
+			t.Logf("Watch not supported in this DirServer")
+			return
+		}
+		t.Fatal(err)
 	}
 	if !r.GotErrorEvent(errors.E(errors.IO)) {
 		t.Fatal(r.Diag())
