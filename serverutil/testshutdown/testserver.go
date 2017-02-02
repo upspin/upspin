34adfce0c2dd92706d41d33b0d81d73a02cfd617
Eduardo Pinheiro <edpin@google.com>
2017-02-01 16:32:43 -0800

    serverutil: tests for shutdown
    DO NOT REVIEW
    
    Adds tests for the shutdown process.
    This is tricky because one needs to send a signal and thus we need
    another process so we don't commit suicide.
    
    I ran this with -count=100 and it passed every time without flakiness.
    The sleeps are inevitable but each run takes 2 seconds on my machine.
    I'm happy to not run it when -short is requested, if that's better.
    
    Change-Id: I992490cf311c43bc721ab31bb8197f7ea9d14935

---
+++ serverutil/testshutdown/testserver.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"upspin.io/log"
+	"upspin.io/serverutil"
+)
+
+const magicPort = ":9781"
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		log.Fatalf("Must provide exit secret as sole argument")
+	}
+	secret := flag.Arg(0)
+
+	ln, err := net.Listen("tcp", magicPort)
+	if err != nil {
+		log.Fatalf("https: %v", err)
+	}
+	serverutil.RegisterShutdown(0, func() {
+		ln.Close()
+	})
+	serverutil.RegisterShutdown(1, func() {
+		fmt.Print(secret)
+	})
+	server := http.Server{
+		Addr: magicPort,
+	}
+	time.AfterFunc(1*time.Minute, func() {
+		log.Fatal("Server not shutdown after 1 minute.")
+	})
+	http.HandleFunc("/echo", echoHandler)
+	server.Serve(ln)
+	serverutil.Shutdown()
+}
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	toEcho, ok := values["str"]
+	if !ok {
+		w.Write([]byte("use /echo?str=<value>"))
+		return
+	}
+	if len(toEcho) != 1 {
+		w.Write([]byte("use /echo?str=<value>"))
+		return
+	}
+	w.Write([]byte(toEcho[0]))
+}
