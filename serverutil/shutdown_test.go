1232d4daa082d30f699da90d54b5e821b7b93074
Eduardo Pinheiro <edpin@google.com>
2017-02-01 16:32:43 -0800

    serverutil: tests for shutdown
    Adds tests for the shutdown process.
    This is tricky because one needs to send a signal and thus we need
    another process so we don't commit suicide.
    
    I ran this with -count=100 and it passed every time without flakiness.
    The sleeps are inevitable but each run takes 2 seconds on my machine.
    I'm happy not to run it when -short is requested, if that's better.
    
    No lint or vet errors.
    
    Change-Id: I992490cf311c43bc721ab31bb8197f7ea9d14935

---
+++ serverutil/shutdown_test.go
@@ -0,0 +1,105 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package serverutil
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"syscall"
+	"testing"
+	"time"
+	"upspin.io/log"
+)
+
+const magicPort = ":9781"
+
+// Test strategy:
+// Launch test binary, get the PID.
+// Check that it's running.
+// Send SIGTERM
+// Wait termination.
+// Check that it's no longer running.
+// Check side-effects to prove the shutdown routines ran.
+// Profit!
+func TestShutdown(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping shutdown test")
+	}
+	now := time.Now()
+
+	err := exec.Command("go", "install", "upspin.io/serverutil/testshutdown").Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	exitCode := fmt.Sprintf("exiting-now-%d-%d-%d", now.Nanosecond(), now.Second(), os.Getpid())
+	cmd := exec.Command("testshutdown", exitCode)
+	f, err := ioutil.TempFile("", "testshutdown")
+	if err != nil {
+		t.Fatal(err)
+	}
+	cmd.Stdout = f
+	err = cmd.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check that the server is up by making it return a secret message.
+	for tries := 0; ; tries++ {
+		testData := now.Format("04:05.000000000")
+		err = pingServer(t, testData)
+		if err == nil {
+			break
+		}
+		if err != nil && tries > 4 {
+			t.Fatal(err)
+		}
+		log.Error.Print(err)
+		time.Sleep(50 * time.Millisecond)
+	}
+
+	// Send SIGTERM.
+	err = syscall.Kill(cmd.Process.Pid, syscall.SIGTERM)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Wait for process to exit
+	err = cmd.Wait()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read and validate output.
+	output := make([]byte, len(exitCode))
+	_, err = f.ReadAt(output, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f.Close()
+	os.Remove(f.Name())
+	if string(output) != exitCode {
+		t.Fatalf("Expected %s, got %d", exitCode, output)
+	}
+}
+
+func pingServer(t *testing.T, ping string) error {
+	resp, err := http.Get("http://localhost" + magicPort + "/echo?str=" + ping)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(data) != ping {
+		return fmt.Errorf("expected %q, got %q", ping, data)
+	}
+	return nil
+}
