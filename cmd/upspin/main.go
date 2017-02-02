c756dd8a45e83ab182cebf8b42304ef0c4b75696
Rob Pike <r@golang.org>
2017-02-01 16:45:09 -0800

    cmd/upspin: save metrics in the upspin command only with a 'debug' build tag
    The user has no reason to save metrics unless debugging, and no ability
    to save them unless an admin for upspin-prod or upspin-test.
    
    Fixes #119
    
    Change-Id: I146b4bab316642ec071a09b3e99ca793557ba7ae

--- cmd/upspin/main.go
+++ cmd/upspin/main.go
@@ -16,7 +16,6 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
-	"time"
 
 	// We deliberately use native Go logs for this command-line tool
 	// as there is no need to report errors to GCP.
@@ -220,7 +219,7 @@ func newState(op string) *State {
 	s.client = client.New(cfg)
 	s.config = cfg
 	s.sharer = newSharer(s)
-	s.maybeEnableMetrics()
+	s.enableMetrics()
 	return s
 }
 
@@ -242,34 +241,7 @@ func (s *State) KeyServer() upspin.KeyServer {
 
 // end terminates any necessary state.
 func (s *State) cleanup() {
-	s.finishMetricsIfEnabled()
-}
-
-func (s *State) maybeEnableMetrics() {
-	gcloudProject := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
-	if strings.Contains(gcloudProject, "upspin-test") {
-		gcloudProject = "upspin-test"
-	} else if strings.Contains(gcloudProject, "upspin-prod") {
-		gcloudProject = "upspin-prod"
-	} else {
-		return
-	}
-	var err error
-	if s.metricsSaver, err = metric.NewGCPSaver(gcloudProject, "app", "cmd/upspin"); err == nil {
-		metric.RegisterSaver(s.metricsSaver)
-	} else {
-		log.Printf("saving metrics: %q", err)
-	}
-}
-
-func (s *State) finishMetricsIfEnabled() {
-	if s.metricsSaver == nil {
-		return
-	}
-	// Allow time for metrics to propagate.
-	for i := 0; metric.NumProcessed() > s.metricsSaver.NumProcessed() && i < 10; i++ {
-		time.Sleep(100 * time.Millisecond)
-	}
+	s.finishMetrics()
 }
 
 // hasGlobChar reports whether the string contains a Glob metacharacter.
---
+++ cmd/upspin/metric.go
@@ -0,0 +1,13 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !debug
+
+package main
+
+func (s *State) enableMetrics() {
+}
+
+func (s *State) finishMetrics() {
+}
