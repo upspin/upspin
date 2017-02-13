26f8824a87e63c19f4dca000acf790580ad8e7ff
Eduardo Pinheiro <edpin@google.com>
2017-02-13 09:41:34 -0800

    serverutil: properly test returned backoff values
    Added missing test values.
    
    No new lint or vet errors.
    
    Change-Id: Ie2723621091524450188c247bd7c456bbf7107db

--- serverutil/rate_test.go
+++ serverutil/rate_test.go
@@ -22,42 +22,46 @@ func TestRateLimiter(t *testing.T) {
 		a, b = "a", "b"
 	)
 	testCases := []struct {
-		key  string
-		sec  int
-		want bool
-		len  int
+		key        string
+		sec        int
+		pass       bool
+		backoffSec int
+		len        int
 	}{
-		{a, 0, true, 1}, // backoff 10s
+		{a, 0, true, 0, 1}, // backoff 10s
 
-		{a, 1, false, 1},
-		{a, 9, false, 1},
-		{a, 10, false, 1},
-		{a, 11, true, 1}, // backoff 20s
+		{a, 1, false, 9, 1},
+		{a, 9, false, 1, 1},
+		{a, 10, false, 0, 1},
+		{a, 11, true, 0, 1}, // backoff 20s
 
-		{b, 15, true, 2},
-		{"c", 24, true, 3},
-		{"d", 31, true, 4},
+		{b, 15, true, 0, 2},
+		{"c", 24, true, 0, 3},
+		{"d", 31, true, 0, 4},
 
-		{a, 22, false, 4},
-		{a, 31, false, 4},
-		{a, 32, true, 4}, // backoff 40s
+		{a, 22, false, 9, 4},
+		{a, 31, false, 0, 4},
+		{a, 32, true, 0, 4}, // backoff 40s
 
-		{b, 40, true, 4},
+		{b, 40, true, 0, 4},
 
-		{a, 200, true, 1}, // backoff 10s
+		{a, 200, true, 0, 1}, // backoff 10s
 
-		{a, 210, false, 1},
-		{a, 211, true, 1}, // backoff 20s
+		{a, 210, false, 0, 1},
+		{a, 211, true, 0, 1}, // backoff 20s
 
-		{a, 320, true, 1}, // backoff 10s
+		{a, 320, true, 0, 1}, // backoff 10s
 	}
-	for _, c := range testCases {
-		got, _ := r.pass(now.Add(time.Duration(c.sec)*time.Second), c.key)
-		if got != c.want {
-			t.Errorf("%d seconds for %q: got %v, want %v", c.sec, c.key, got, c.want)
+	for i, c := range testCases {
+		pass, backoff := r.pass(now.Add(time.Duration(c.sec)*time.Second), c.key)
+		if pass != c.pass {
+			t.Errorf("case %d: %d seconds for %q: got %v, want %v", i, c.sec, c.key, pass, c.pass)
+		}
+		if backoff != time.Duration(c.backoffSec)*time.Second {
+			t.Errorf("case %d: expected backoff = %d s, got = %v", i, c.backoffSec, backoff)
 		}
 		if got, want := len(r.m), c.len; got != want {
-			t.Errorf("%d seconds for %q: len(r.m) = %d, want %d", c.sec, c.key, got, want)
+			t.Errorf("case %d: %d seconds for %q: len(r.m) = %d, want %d", i, c.sec, c.key, got, want)
 		}
 	}
 
