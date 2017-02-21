010e20981fb90e86a0af8c45879c9677c3dd0abe
Andrew Gerrand <adg@golang.org>
2017-02-21 16:16:14 +1100

    cmd/upspin-deploy: add explanatory note to deploy scripts
    Change-Id: Ic998733e460823d54c1f7111aece1576b16830d6

--- cmd/upspin-deploy/deploy-prod.sh
+++ cmd/upspin-deploy/deploy-prod.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# This script deploys the Upspin servers running under upspin.io.
+
 go install && upspin-deploy -project=upspin-prod -domain=upspin.io -zone=us-central1-c -keyserver="" "$@"
--- cmd/upspin-deploy/deploy-test.sh
+++ cmd/upspin-deploy/deploy-test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# This script deploys the Upspin servers running under test.upspin.io.
+
 go install && upspin-deploy -project=upspin-test -domain=test.upspin.io -zone=us-central1-c -keyserver="" "$@"
