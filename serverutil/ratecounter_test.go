c63f3c1164da87637e97b0ed9680743c6018f4f8
Eduardo Pinheiro <edpin@edpin.com>
2017-02-21 22:43:32 -0800

    serverutil: add RateCounter
    A RateCounter counts events over time, averaged over a
    specified window. A RateCounter is an expvar, so it can
    be used in servers that want to export information.
    
    Motivation: watching logs for the keyserver does not give us
    a good indication whether or not its overloaded. A future CL
    will add a RateCounter to track requests per second over the last
    5, 10 and 60 seconds or so.
    
    No new lint or vet errors.
    
    Change-Id: Idf3b566174301c7936ca1eb5a6f73eecfe3c9ad5

---
+++ serverutil/ratecounter_test.go
@@ -0,0 +1,49 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package serverutil
+
+import (
+	"testing"
+	"time"
+)
+
+func TestRateCounter(t *testing.T) {
+	ready := make(chan bool)
+	onReady = func() {
+		ready <- true
+	}
+	defer func() {
+		onReady = func() {}
+	}()
+
+	tick := make(chan time.Time)
+	rc, err := newRateCounter(3, time.Second, tick)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, c := range []struct {
+		add  int64
+		want float64
+	}{
+		{3, 1.0}, // First second. [3, 0, 0] rate = 1
+		{6, 3.0}, // Second second. [3, 6, 0] rate = 3
+		{0, 3.0}, // Third second. [3, 6, 0] rate = 3
+		{0, 2.0}, // Fourth second. [0, 6, 0] rate = 2
+		{0, 0.0}, // Fifth second. [0, 0, 0] rate = 0
+	} {
+		rc.Add(c.add)
+		if got, want := rc.Rate(), c.want; got != want {
+			t.Errorf("rate = %f, want = %f", got, want)
+		}
+		tick <- time.Now()
+		<-ready
+	}
+
+	rc.Add(12)
+	if rc.String() != "4 ops/s" {
+		t.Errorf("got = %s", rc)
+	}
+}
