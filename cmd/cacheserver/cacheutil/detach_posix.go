14b0b37be737a28d2271e119b55a76175ec5882e
Dave Presotto <presotto@gmail.com>
2017-02-25 20:45:10 -0800

    cmd/cacheserver/cacheutil: when cacheserver is started by other progs, detach it from the process group
    This makes the cacheserver less dependent on programs that start it. It
    already outlasted its parent but this also detaches if from signals that
    kill the parent. It makes it more likely that writebacks will complete.
    
    Change-Id: I53eeff9bdee16d214234d9af2acbcfdc44ca8b7c
    Reviewed-on: https://upspin-review.googlesource.com/8034
    Reviewed-by: Andrew Gerrand <adg@golang.org>
    Reviewed-by: Rob Pike <r@golang.org>

---
+++ cmd/cacheserver/cacheutil/detach_posix.go
@@ -0,0 +1,20 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !windows
+
+package cacheutil
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func init() {
+	detach = func(cmd *exec.Cmd) {
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Setpgid: true,
+		}
+	}
+}
