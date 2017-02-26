462a18f5fa424af4319efaa2fcb640ce809869cd
Dave Presotto <presotto@gmail.com>
2017-02-25 20:45:10 -0800

    cmd/cacheserver/cacheutil: when cacheserver is started by other progs, detach it from the process group
    This makes the cacheserver less dependent on programs that start it. It
    already outlasted its parent but this also detaches if from signals that
    kill the parent. It makes it more likely that writebacks will complete.
    
    Change-Id: I53eeff9bdee16d214234d9af2acbcfdc44ca8b7c

---
+++ cmd/cacheserver/cacheutil/detach_posix.go
@@ -0,0 +1,18 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !windows
+package cacheutil // import "upspin.io/cmd/cacheserver/cacheutil"
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func detach(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		//Noctty: true,
+		Setpgid: true,
+	}
+}
---
+++ cmd/cacheserver/cacheutil/detach_windows.go
@@ -0,0 +1,12 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build windows
+
+package cacheutil // import "upspin.io/cmd/cacheserver/cacheutil"
+
+import "os/exec"
+
+func detach(cmd *exec.Cmd) {
+}
