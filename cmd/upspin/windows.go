e707c6800a16408d3bfd099c3d040c03ee413ab6
Fabian Wickborn <fabian@wickborn.net>
2017-03-01 23:37:17 +0100

    cmd/upspin: implement findUpspinBinaries for Windows
    DO NOT REVIEW - work in progress
    
    Fixes #297
    
    Change-Id: I898b16180af31c7867e82e2f70bfe910cc20961b

--- cmd/upspin/windows.go
+++ cmd/upspin/windows.go
@@ -6,11 +6,69 @@
 
 package main
 
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// pathExtensions returns the file extensions for executable files as a string
+// slice.
+// It evaluates and splits the PATHEXT environment variable, or, if the
+// environment variable is empty, returns a sensible set of defaults.
+func pathExtensions() []string {
+	var exts []string
+	x := os.Getenv("PATHEXT")
+	if x != "" {
+		for _, e := range strings.Split(strings.ToLower(x), `;`) {
+			if e == "" {
+				continue
+			}
+			if e[0] != '.' {
+				e = "." + e
+			}
+			exts = append(exts, e)
+		}
+	} else {
+		exts = []string{".com", ".exe", ".bat", ".cmd"}
+	}
+	return exts
+}
+
 // findUpspinBinaries finds all the upspin-* binaries in %PATHEXT%.
 // It may return the same name multiple times; the caller should
 // filter.
-// TODO: Make this actually work on Windows.
-// The code for os.exec/LookPath is a good guide.
 func findUpspinBinaries() []string {
-	return nil
+	path := os.Getenv("PATH")
+	var cmds []string
+	exts := pathExtensions()
+	for _, dir := range filepath.SplitList(path) {
+		if dir == "" {
+			dir = "."
+		}
+		fd, err := os.Open(dir)
+		if err != nil {
+			continue
+		}
+		files, err := fd.Readdir(0)
+		fd.Close()
+		if err != nil {
+			continue
+		}
+		for _, info := range files {
+			name := info.Name()
+			if !strings.HasPrefix(name, "upspin-") {
+				continue
+			}
+			if !info.Mode().IsRegular() {
+				continue
+			}
+			for _, e := range exts {
+				if strings.HasSuffix(info.Name(), e) {
+					cmds = append(cmds, name[len("upspin-"):len(name)-len(e)])
+				}
+			}
+		}
+	}
+	return cmds
 }
---
+++ cmd/upspin/windows_test.go
@@ -0,0 +1,63 @@
+// +build windows
+
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"testing"
+)
+
+func TestFindUpspinBinaries(t *testing.T) {
+	testFiles := []struct {
+		n string
+		b bool
+	}{
+		{"some/path/upspin-foo.exe", true},
+		{"some/path/upspin-bar.txt", false},
+		{"thirst/path/upspin-baz.bat", true},
+		{"fourth/path/upspin-qux.com", true},
+		{"yet/another/upspin-foo.cmd", true},
+	}
+	tmpDir, err := ioutil.TempDir("", "upspin-binary-test-")
+	if err != nil {
+		t.Fatalf("could not create temp dir: %v", err)
+	}
+	defer os.RemoveAll(tmpDir)
+	paths := map[string]bool{}
+	for _, tf := range testFiles {
+		d := filepath.Dir(filepath.Join(tmpDir, tf.n))
+		paths[d] = true
+		if err = os.MkdirAll(d, 0700); err != nil {
+			t.Errorf("could not create %s: %v", d, err)
+			continue
+		}
+		f, err := os.Create(filepath.Join(tmpDir, tf.n))
+		if err != nil {
+			t.Errorf("could not create temporary file %s: %v", tf.n, err)
+			continue
+		}
+		f.Close()
+	}
+
+	defer os.Setenv("PATH", os.Getenv("PATH"))
+	var newPath string
+	for k, _ := range paths {
+		newPath += k + string(filepath.ListSeparator)
+	}
+	err = os.Setenv("PATH", newPath)
+	if err != nil {
+		t.Fatalf("could not set PATH: %v", err)
+	}
+
+	binaries := findUpspinBinaries()
+	sort.Strings(binaries)
+	wanted := "baz;foo;foo;qux"
+	got := strings.Join(binaries, ";")
+	if wanted != got {
+		t.Fatalf("list of binaries wrong. wanted '%s', got '%s'", wanted, got)
+	}
+}
