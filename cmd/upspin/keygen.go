46010e6045fa5396ef1662fa2d8794e2cc9936f3
Eduardo Pinheiro <epinheiro@gmail.com>
2017-03-02 16:08:35 -0800

    cmd/upspin: add -rotate flag to keygen
    To prevent accidents, existing keys are only overwritten if
    -rotate is on.
    
    Also, make keygen work again. It was missing the glue between s.keygen
    and s.keygenCommand.
    
    Fixes #271
    
    Change-Id: Ic9dca949b753a1bd390e3e0a14f786e79733478b

--- cmd/upspin/keygen.go
+++ cmd/upspin/keygen.go
@@ -38,10 +38,12 @@ See the description for rotate for information about updating keys.
 	fs.String("curve", "p256", "cryptographic curve `name`: p256, p384, or p521")
 	fs.String("secretseed", "", "128 bit secret `seed` in proquint format")
 	fs.String("where", filepath.Join(os.Getenv("HOME"), ".ssh"), "`directory` to store keys")
+	fs.Bool("rotate", false, "whether to rotate existing keys and replace them with new ones")
 	s.ParseFlags(fs, args, help, "keygen [-curve=256] [-secretseed=seed] [-where=$HOME/.ssh]")
 	if fs.NArg() != 0 {
 		fs.Usage()
 	}
+	s.keygenCommand(fs)
 }
 
 func (s *State) keygenCommand(fs *flag.FlagSet) {
@@ -63,9 +65,10 @@ func (s *State) keygenCommand(fs *flag.FlagSet) {
 	if where == "" {
 		s.Exitf("-where must not be empty")
 	}
-	err = saveKeys(where)
+	rotate := subcmd.BoolFlag(fs, "rotate")
+	err = saveKeys(where, rotate)
 	if err != nil {
-		s.Exitf("saving previous keys failed(%v); keys not generated", err)
+		s.Exitf("saving previous keys failed, keys not generated: %s", err)
 	}
 	err = writeKeys(where, public, private)
 	if err != nil {
@@ -82,6 +85,9 @@ func (s *State) keygenCommand(fs *flag.FlagSet) {
 		fmt.Println("Do not share your private key or this command with anyone.")
 	} else {
 		fmt.Println("Do not share your private key with anyone.")
+	}
+	if rotate {
+		fmt.Println("\nTo install new keys in the key server, do 'upspin rotate' next.")
 	}
 	fmt.Println()
 }
@@ -148,7 +154,7 @@ func writeKeys(where, publicKey, privateKey string) error {
 	return nil
 }
 
-func saveKeys(where string) error {
+func saveKeys(where string, rotate bool) error {
 	var (
 		publicFile  = filepath.Join(where, "public.upspinkey")
 		privateFile = filepath.Join(where, "secret.upspinkey")
@@ -158,10 +164,17 @@ func saveKeys(where string) error {
 	// Read existing key pair.
 	private, err := ioutil.ReadFile(privateFile)
 	if os.IsNotExist(err) {
-		return nil // There is nothing we need to save.
+		// There is nothing we need to save. Did we expect it to be?
+		if rotate {
+			return errors.Str("no keys exist to rotate")
+		}
+		return nil
 	}
 	if err != nil {
 		return err
+	}
+	if !rotate {
+		return errors.Str("keys exists; -rotate must be used")
 	}
 	public, err := ioutil.ReadFile(publicFile)
 	if err != nil {
@@ -178,5 +191,10 @@ func saveKeys(where string) error {
 	if err != nil {
 		return err
 	}
-	return archive.Close()
+	err = archive.Close()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Successfully saved previous keys in %q", archiveFile)
+	return nil
 }
