112582a022d5fe0a6b63b3873af4fca2a2a1f955
Dave Presotto <presotto@gmail.com>
2017-03-03 13:43:57 -0800

    cmd/upspinfs: show symlink targets as relative in *nix
    All upspin symlinks are absolute/global, i.e., they contain the full
    upspin path name.  In *nix, symlinks can be either rooted or relative.
    Displaying my pedantic side, I originally made all all upspin symlinks
    show as rooted.  However, as Jacek Masiulaniec points out in issue 291
    an upspin directory tree. In that case, having the user's mountpoint
    in every symbolic link is a pain. For tar one could use -h to copy the
    contents rather than the symlink but it would be easier to just
    display relative paths in *nix, wherever possible.
    
    So, giving in to my soft chewy center, this cl changes the presentation
    of symbolic links in *nix to be relative.
    
    We still refuse to represent targets that are outside of upspin for a
    few reasons. Foremost, upspin links do not point outside of upspin.
    I would argue against changing that for security reasons.  For example,
    it would be nasty if using upspinfs to walk someone else's tree one could
    be fooled into changing things on their machine outside of upspin.
    
    Updates #291
    
    Change-Id: I1acf4a51317ee3b8204bf8ccea5180ac409b46e7

--- cmd/upspinfs/fs.go
+++ cmd/upspinfs/fs.go
@@ -11,6 +11,7 @@ import (
 	"io"
 	"os"
 	ospath "path"
+	"path/filepath"
 	"strings"
 	"sync"
 	"time"
@@ -96,8 +97,9 @@ func (h *handle) String() string {
 
 // newUpspinFS creates a new upspin file system.
 func newUpspinFS(config upspin.Config, mountpoint string, cacheDir string) *upspinFS {
-	if !strings.HasSuffix(mountpoint, "/") {
-		mountpoint = mountpoint + "/"
+	sep := string(filepath.Separator)
+	if !strings.HasSuffix(mountpoint, sep) {
+		mountpoint = mountpoint + sep
 	}
 	f := &upspinFS{
 		mountpoint: mountpoint,
@@ -791,15 +793,31 @@ func (n *node) Removexattr(ctx gContext.Context, req *fuse.RemovexattrRequest) e
 	return nil
 }
 
-func (dir *node) unixPathToUpspinPath(unixpath string) (upspin.PathName, error) {
-	if unixpath[0] != '/' {
-		return path.Join(dir.uname, unixpath), nil
+// convertRelPath converts a host relative path into an upspin one. It assumes
+// that the only difference is the separators. This will work with
+// windows and *nix. Not sure about other systems.
+func convertRelPath(path string) string {
+	if filepath.Separator == '/' {
+		return path
 	}
-	upspinpath := strings.TrimPrefix(unixpath, dir.f.mountpoint)
-	if unixpath != upspinpath {
-		return upspin.PathName(unixpath), errors.Str("symlink outside of upspin")
+	return strings.Replace(path, string(filepath.Separator), "/", -1)
+}
+
+// hostPathToUpspinPath takes a hostpath and returns an upspin path.
+func (dir *node) hostPathToUpspinPath(hostpath string) (upspin.PathName, error) {
+	mountrel := strings.TrimPrefix(hostpath, dir.f.mountpoint)
+	if hostpath != mountrel {
+		// We have a path that is relative to the mount point.
+		// Convert the separator if necessary and return as an
+		// upspin path.
+		return upspin.PathName(convertRelPath(mountrel)), nil
 	}
-	return upspin.PathName(upspinpath), nil
+	// Not relative to the mountpoint. If it is rooted, it is outside Upspin.
+	if filepath.IsAbs(hostpath) {
+		return upspin.PathName(hostpath), errors.Str("symlink outside of upspin")
+	}
+	// This is relative to dir. Convert the separators and append to the dir.
+	return path.Join(dir.uname, convertRelPath(hostpath)), nil
 }
 
 // Symlink implements fs.Symlink.
@@ -807,7 +825,7 @@ func (n *node) Symlink(ctx gContext.Context, req *fuse.SymlinkRequest) (fs.Node,
 	const op = "upspinfs/fs.Symlink"
 	n.Lock()
 	defer n.Unlock()
-	target, err := n.unixPathToUpspinPath(req.Target)
+	target, err := n.hostPathToUpspinPath(req.Target)
 	if err != nil {
 		return nil, e2e(errors.E(op, n.uname, err))
 	}
@@ -822,16 +840,41 @@ func (n *node) Symlink(ctx gContext.Context, req *fuse.SymlinkRequest) (fs.Node,
 	return nn, nil
 }
 
-func (n *node) upspinPathToUnixPath(path upspin.PathName) string {
-	// All upspin paths are absolute.
-	return n.f.mountpoint + string(path)
+// upspinPathToHostPath takes an Upspin path, target, and turns it into a host path relative
+// to the Upspin path, link.
+func (link *node) upspinPathToHostPath(target upspin.PathName) (string, error) {
+	parsedLink, err := path.Parse(link.uname)
+	if err != nil {
+		return "", e2e(err)
+	}
+	parsedTarget, err := path.Parse(target)
+	if err != nil {
+		return "", e2e(err)
+	}
+
+	// Create relative path.
+	nl := parsedLink.NElem()
+	nt := parsedTarget.NElem()
+	relpath := ""
+	for i := -1; ; i++ {
+		if i >= nl || i >= nt || parsedTarget.Elem(i) != parsedLink.Elem(i) {
+			for j := i; j < nl-1; j++ {
+				relpath = ospath.Join(relpath, "..")
+			}
+			for j := i; j < nt; j++ {
+				relpath = ospath.Join(relpath, parsedTarget.Elem(j))
+			}
+			break
+		}
+	}
+	return relpath, nil
 }
 
 // Symlink implements fs.NodeReadlinker.Readlink.
 func (n *node) Readlink(ctx gContext.Context, req *fuse.ReadlinkRequest) (string, error) {
 	const op = "upspinfs/fs.Readlink"
 	log.Debug.Printf("Readlink %q -> %q", n, n.link)
-	return n.upspinPathToUnixPath(n.link), nil
+	return n.upspinPathToHostPath(n.link)
 }
 
 // isEnoent returns true if we already know this path name doesn't exist.
