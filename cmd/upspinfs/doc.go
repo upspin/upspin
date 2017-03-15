6288ac0dafa96d2176de7431389be5d8159de344
Dave Presotto <presotto@gmail.com>
2017-03-15 13:13:50 -0700

    cmd/upspinfs: update documentation
    In particular, correct the flag informaiton and add a Limitations
    section.
    
    Change-Id: Ifef322501c748694a43c1983a11ae1eda3f48fc1
    Reviewed-on: https://upspin-review.googlesource.com/8402
    Reviewed-by: Eric Grosse <grosse@gmail.com>

--- cmd/upspinfs/doc.go
+++ cmd/upspinfs/doc.go
@@ -8,9 +8,10 @@
 Command upspinfs is a FUSE interface for Upspin. It presents Upspin
 files as a locally mounted file system.
 
-If $HOME/upspin/config specifies a cache server endpoint and cacheserver
-is not running, upspinfs will attempt to start it. Upspinfs will pass
-to cacheserver both its -log and -cache flags.
+If the config or flags specify a cache server endpoint and cacheserver
+is not running, upspinfs will attempt to start one. All the flags
+listed below are also passed to the cacheserver should one be
+started.
 
 Usage:
 	upspinfs [flags] mountpoint
@@ -19,13 +20,16 @@ Usage:
 
 The flags are:
 
-	-log=level
- 		Set the log level to 'level'.
-	-cachedir=directory
-		Cache all files being read or written in 'directory'/fscache.
+	-cachedir directory
+		'directory' will contain all file caches (default "$HOME/upspin")
+	-cachesize bytes
+		max disk bytes for cache (default 5000000000)
+	-config file
+		user's configuration file (default "$HOME/upspin/config")
+	-log level
+		level of logging: debug, info, error, disabled (default info)
 	-writethrough
-		Make storage cache writethrough.
-
+		make storage cache writethrough
 
 Examples:
 
@@ -35,5 +39,26 @@ Examples:
 	% ...
 	% killall -9 upspinfs
 	% umount $HOME/ufs
+
+Limitations:
+
+Uspinfs tries to present a Posix file system.
+However since Upspin semantics are
+different than Posix, some things will be different:
+
+- Files always appear owned by the user who started upspinfs
+
+- Permission bits are settable but are not stored in Upspin.
+After the final close of a file, when the kernel and FUSE decide to
+forget about the file, permissions will revert to 0700.
+The access to a file is determined by the intersection of the permission
+bits and the relevant Access file.
+
+- While random access will work, the first time a file is opened
+for read, it is read in its entirety and cached locally.
+
+- Hard links are really copy on write.
+The two names will refer to the original data until either file is changed.
+They will then diverge.
 */
 package main
