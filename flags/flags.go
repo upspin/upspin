8ae4dbd812642ad3c07ac516890e3b8e3dddff20
Eric Grosse <grosse@gmail.com>
2017-03-23 21:47:23 +0000

    client: add checks for malicious directory server
    If flag -prudent is set, run extra checks in the client normally
    enforced in the directory server.  This is intended for people who
    fear a buggy, malicious, compromised or compelled server.
    Cambridge Dictionary: "prudent: showing good judgment in
    avoiding risks and uncertainties; careful."
    
    Specifically, check that a file signer entry.Writer is the owner
    or currently has write permission.  There may be similar checks
    in the future that can share this flag. Eventually, when
    usability and performance tests confirm, it would be better
    to remove this flag.
    
    Updates #179
    
    Change-Id: Icde2846ba2860a5010d7668d4ca6623013485e8e

--- flags/flags.go
+++ flags/flags.go
@@ -75,6 +75,12 @@ var (
 	// StoreServerName is the Upspin user name of the StoreServer.
 	StoreServerUser = ""
 
+	// Prudent sets an extra security mode in the client to check that the writer listed
+	// in a directory entry is a user with write permission. This can protect against
+	// malicious directory servers at the cost of blocking read-only files written
+	// by users who are not the owner of the file's tree.
+	Prudent = false
+
 	// TLSCertFile and TLSKeyFile specify the location of a TLS
 	// certificate/key pair used for serving TLS (HTTPS).
 	TLSCertFile = ""
@@ -117,6 +123,17 @@ var flags = map[string]*flagVar{
 		arg: func() string { return strArg("serverconfig", configFlag{&ServerConfig}.String(), "") },
 	},
 	"storeserveruser": strVar(&StoreServerUser, "storeserveruser", "", "user name of the StoreServer"),
+	"prudent": &flagVar{
+		set: func() {
+			flag.BoolVar(&Prudent, "prudent", false, "protect against malicious directory server")
+		},
+		arg: func() string {
+			if !Prudent {
+				return ""
+			}
+			return "-prudent"
+		},
+	},
 	"tls": &flagVar{
 		set: func() {
 			flag.StringVar(&TLSCertFile, "tls_cert", "", "TLS Certificate `file` in PEM format")
