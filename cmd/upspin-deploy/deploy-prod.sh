66318b5607d318bcbc6559d68a2a4aed683e624b
Andrew Gerrand <adg@golang.org>
2017-04-07 11:42:42 +1000

    cloud: remove GCP-related packages and update users
    DO NOT SUBMIT until we have all the documentation changes lined up.
    
    Change-Id: I8c7df729c4c31e31eba6d6f42fe821aeceaf1fec

--- cmd/upspin-deploy/deploy-prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script deploys the Upspin servers running under upspin.io.
-
-go install && upspin-deploy -project=upspin-prod -domain=upspin.io -zone=us-central1-c -keyserver="" "$@"
