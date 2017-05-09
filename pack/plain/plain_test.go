331e496cbb53cd61a72233fe8df69a12478af257
Dmitri Shuralyov <shurcooL@gmail.com>
2017-05-09 02:44:51 -0400

    pack/plain: use testutil.Repo
    Helps #183.
    
    Change-Id: I1d621715a0f0e4c49b989b52289df707fee96c69

--- pack/plain/plain_test.go
+++ pack/plain/plain_test.go
@@ -9,8 +9,6 @@ import (
 	"crypto/rand"
 	"fmt"
 	"log"
-	"os"
-	"path/filepath"
 	"strings"
 	"testing"
 
@@ -20,6 +18,7 @@ import (
 	"upspin.io/pack"
 	"upspin.io/pack/internal/packtest"
 	"upspin.io/test/testfixtures"
+	"upspin.io/test/testutil"
 	"upspin.io/upspin"
 )
 
@@ -175,7 +174,7 @@ func setup(name upspin.UserName) (upspin.Config, upspin.Packer) {
 	if j < 0 {
 		log.Fatalf("malformed username %s", name)
 	}
-	f, err := factotum.NewFromDir(repo("key", "testdata", string(name[:j])))
+	f, err := factotum.NewFromDir(testutil.Repo("key", "testdata", string(name[:j])))
 	if err != nil {
 		log.Fatalf("unable to initialize factotum for %s", string(name[:j]))
 	}
@@ -210,13 +209,4 @@ func (d *dummyKey) Lookup(userName upspin.UserName) (*upspin.User, error) {
 }
 func (d *dummyKey) Dial(cc upspin.Config, e upspin.Endpoint) (upspin.Service, error) {
 	return d, nil
-}
-
-// repo returns the local pathname of a file in the upspin repository.
-func repo(dir ...string) string {
-	gopath := os.Getenv("GOPATH")
-	if len(gopath) == 0 {
-		log.Fatal("no GOPATH")
-	}
-	return filepath.Join(gopath, "src", "upspin.io", filepath.Join(dir...))
 }
