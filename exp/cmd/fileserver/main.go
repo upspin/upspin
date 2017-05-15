a6a33bc96fde87dbbcf6014b147e3c8eb34fda96
Andrew Gerrand <adg@golang.org>
2017-05-15 17:56:02 +1000

    exp/cmd/fileserver: add stand-alone server for serving a filesystem
    Change-Id: I67babe9480fea0ac9a8f604752a6a3bc8b5f5937
    Reviewed-on: https://upspin-review.googlesource.com/9563
    Reviewed-by: Rob Pike <r@golang.org>

---
+++ exp/cmd/fileserver/main.go
@@ -0,0 +1,60 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Command fileserver serves a local file system as an Upspin tree.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	"upspin.io/cloud/https"
+	"upspin.io/config"
+	"upspin.io/flags"
+	"upspin.io/log"
+	"upspin.io/rpc/dirserver"
+	"upspin.io/rpc/storeserver"
+	"upspin.io/upspin"
+
+	filesystem_dir "upspin.io/exp/dir/filesystem"
+	filesystem_store "upspin.io/exp/store/filesystem"
+
+	_ "upspin.io/key/transports"
+	_ "upspin.io/pack/eeintegrity"
+)
+
+func main() {
+	root := flag.String("root", "", "`path` to local file system root")
+
+	flags.Parse(flags.Server)
+
+	if *root == "" {
+		fmt.Fprintln(os.Stderr, "fileserver: the -root flag must be specified")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	cfg, err := config.FromFile(flags.Config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	opt := "root=" + *root
+	dir, err := filesystem_dir.New(cfg, opt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	store, err := filesystem_store.New(cfg, opt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	addr := upspin.NetAddr(flags.NetAddr)
+	http.Handle("/api/Store/", storeserver.New(cfg, store, addr))
+	http.Handle("/api/Dir/", dirserver.New(cfg, dir, addr))
+
+	https.ListenAndServeFromFlags(nil)
+}
