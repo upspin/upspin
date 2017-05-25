82cc8f3cfc132baba4cc75e1297da96758af8c9f
Rob Pike <r@golang.org>
2017-05-25 11:33:42 +1000

    user: break out ParseUser and ParseDomain
    To make it possible to validate group names in access files (not done yet),
    break apart user.Parse into ParseUser and ParseDomain.
    
    No functional change, although error messages will sometimes format differently.
    
    Update #381.
    
    Change-Id: Ia56a2b8ca2190e4e29220281c717aed6f1984f9e

--- user/user_test.go
+++ user/user_test.go
@@ -14,7 +14,7 @@ import (
 
 func TestParse(t *testing.T) {
 	type cases struct {
-		userName upspin.UserName
+		userName string
 		user     string
 		suffix   string
 		domain   string
@@ -31,40 +31,40 @@ func TestParse(t *testing.T) {
 		D
 	)
 	var tests = []cases{
-		{upspin.UserName("me@here.com"), "me", S, "here.com", ""},
-		{upspin.UserName("me@here.com."), "me", S, "here.com", ""},
-		{upspin.UserName("me+you@here.com"), "me+you", "you", "here.com", ""},
-		{upspin.UserName("me@HERE.com"), "me", S, "here.com", ""}, // Lower-case the domain.
-		{upspin.UserName("me.and.my.shadow@here.com"), "me.and.my.shadow", S, "here.com", ""},
-		{upspin.UserName(hugeOKName + "@foo.com"), hugeOKName, S, "foo.com", ""}, // Maximum accepted length, 254.
-		{upspin.UserName(hugeBadName + "@foo.com"), U, S, D, "name too long"},
-		{upspin.UserName("@"), U, S, D, "invalid operation: missing user name"},
-		{upspin.UserName("user@" + hugeOKDomain), "user", S, hugeOKDomain, ""}, // Maximum domain element length, 63.
-		{upspin.UserName("user@" + hugeBadDomain), U, S, D, "invalid domain name element"},
-		{upspin.UserName("user@a..com"), U, S, D, "invalid domain name element"},
-		{upspin.UserName("a@bcom"), U, S, D, "domain name must contain a period"},
-		{upspin.UserName("a@b@.com"), U, S, D, "user name must contain one @ symbol"},
-		{upspin.UserName("a@%.com"), U, S, D, "bad symbol in domain name"},
-		{upspin.UserName("@bbc.com"), U, S, D, "missing user name"},
-		{upspin.UserName("abc.com@"), U, S, D, "missing domain name"},
-		{upspin.UserName("a@b.co"), "a", S, "b.co", ""},
-		{upspin.UserName("a@b.c"), U, S, D, "invalid domain name"},
-		{upspin.UserName("me@here/.com"), U, S, D, "bad symbol in domain name"},
-		{upspin.UserName("me@here.com.."), U, S, D, "invalid domain name"}, // Two trailing dots.
-		{upspin.UserName("me+@here.com"), U, S, D, "empty +suffix in user name"},
-		{upspin.UserName("me+a+b@here.com"), U, S, D, "multiple +suffixes in user name"},
-		{upspin.UserName("me+/x@here.com"), U, S, D, "bad symbol in +suffix"},
+		{"me@here.com", "me", S, "here.com", ""},
+		{"me@here.com.", "me", S, "here.com", ""},
+		{"me+you@here.com", "me+you", "you", "here.com", ""},
+		{"me@HERE.com", "me", S, "here.com", ""}, // Lower-case the domain.
+		{"me.and.my.shadow@here.com", "me.and.my.shadow", S, "here.com", ""},
+		{hugeOKName + "@foo.com", hugeOKName, S, "foo.com", ""}, // Maximum accepted length, 254.
+		{hugeBadName + "@foo.com", U, S, D, "name too long"},
+		{"@", U, S, D, "invalid operation: missing user name"},
+		{"user@" + hugeOKDomain, "user", S, hugeOKDomain, ""}, // Maximum domain element length, 63.
+		{"user@" + hugeBadDomain, U, S, D, "invalid domain name element"},
+		{"user@a..com", U, S, D, "invalid domain name element"},
+		{"a@bcom", U, S, D, "domain name must contain a period"},
+		{"a@b@.com", U, S, D, "user name must contain one @ symbol"},
+		{"a@%.com", U, S, D, "bad symbol in domain name"},
+		{"@bbc.com", U, S, D, "missing user name"},
+		{"abc.com@", U, S, D, "missing domain name"},
+		{"a@b.co", "a", S, "b.co", ""},
+		{"a@b.c", U, S, D, "invalid domain name"},
+		{"me@here/.com", U, S, D, "bad symbol in domain name"},
+		{"me@here.com..", U, S, D, "invalid domain name"}, // Two trailing dots.
+		{"me+@here.com", U, S, D, "empty +suffix in user name"},
+		{"me+a+b@here.com", U, S, D, "multiple +suffixes in user name"},
+		{"me+/x@here.com", U, S, D, "bad symbol in +suffix"},
 		// A good PRECIS case: canonicalize the accent. These two should be the same user, "ê@here.com".
-		{upspin.UserName("\u00ea@here.com"), "\u00ea", S, "here.com", ""},  // Single code point.
-		{upspin.UserName("e\u0302@here.com"), "\u00ea", S, "here.com", ""}, // Accent as a combining character.
+		{"\u00ea@here.com", "\u00ea", S, "here.com", ""},  // Single code point.
+		{"e\u0302@here.com", "\u00ea", S, "here.com", ""}, // Accent as a combining character.
 		// Bad PRECIS cases.
-		{upspin.UserName("henry\u2163@here.com"), U, S, D, "precis: disallowed rune"},
-		{upspin.UserName("!!@here.com"), U, S, D, "invalid operation: user name contains only punctuation"},
+		{"henry\u2163@here.com", U, S, D, "precis: disallowed rune"},
+		{"!!@here.com", U, S, D, "invalid operation: user name contains only punctuation"},
 		// Special wildcard case.
-		{upspin.UserName("*@here.com"), "*", S, "here.com", ""}, // Single code point.
+		{"*@here.com", "*", S, "here.com", ""}, // Single code point.
 	}
 	for _, test := range tests {
-		u, s, d, err := Parse(test.userName)
+		u, s, d, err := Parse(upspin.UserName(test.userName))
 		if test.errStr == "" {
 			if err != nil {
 				t.Errorf("%q: expected no error, got %q", test.userName, err)
@@ -87,6 +87,117 @@ func TestParse(t *testing.T) {
 		}
 		if !strings.Contains(err.Error(), test.errStr) {
 			t.Errorf("%q: expected %q, got %q", test.userName, test.errStr, err)
+			continue
+		}
+	}
+}
+
+func TestParseUser(t *testing.T) {
+	type cases struct {
+		userName string
+		user     string
+		suffix   string
+		errStr   string
+	}
+	hugeOKName := strings.Repeat("a", 255-1)
+	hugeBadName := strings.Repeat("b", 255)
+	// Constants to make the test cases a little easier to read.
+	const (
+		U = ""
+		S
+	)
+	var tests = []cases{
+		{"", U, S, "invalid operation: missing user name"},
+		{"a", "a", S, ""},
+		{"me", "me", S, ""},
+		{"me+you", "me+you", "you", ""},
+		{"me", "me", S, ""}, // Lower-case the domain.
+		{"me.and.my.shadow", "me.and.my.shadow", S, ""},
+		{hugeOKName, hugeOKName, S, ""}, // Maximum accepted length, 254.
+		{hugeBadName, U, S, "name too long"},
+		{"me+", U, S, "empty +suffix in user name"},
+		{"me+a+b", U, S, "multiple +suffixes in user name"},
+		{"me+/x", U, S, "bad symbol in +suffix"},
+		// A good PRECIS case: canonicalize the accent. These two should be the same user, "ê".
+		{"\u00ea", "\u00ea", S, ""},  // Single code point.
+		{"e\u0302", "\u00ea", S, ""}, // Accent as a combining character.
+		// Bad PRECIS cases.
+		{"henry\u2163", U, S, "precis: disallowed rune"},
+		{"!!", U, S, "invalid operation: user name contains only punctuation"},
+		// Special wildcard case not valid here, only in Parse.
+		{"*", "*", S, "user name contains only punctuation"},
+	}
+	for _, test := range tests {
+		u, s, err := ParseUser(test.userName)
+		if test.errStr == "" {
+			if err != nil {
+				t.Errorf("%q: expected no error, got %q", test.userName, err)
+				continue
+			}
+			if u != test.user {
+				t.Errorf("%q: expected user %q, got %q", test.userName, test.user, u)
+			}
+			if s != test.suffix {
+				t.Errorf("%q: expected suffix %q, got %q", test.userName, test.suffix, s)
+			}
+			continue
+		}
+		if err == nil {
+			t.Errorf("%q: expected %q error, got none", test.userName, test.errStr)
+			continue
+		}
+		if !strings.Contains(err.Error(), test.errStr) {
+			t.Errorf("%q: expected %q, got %q", test.userName, test.errStr, err)
+			continue
+		}
+	}
+}
+
+func TestParseDomain(t *testing.T) {
+	type cases struct {
+		domainName string
+		domain     string
+		errStr     string
+	}
+	hugeOKDomain := strings.Repeat("x", 63) + ".com"
+	hugeBadDomain := strings.Repeat("y", 64) + ".com"
+	// Constants to make the test cases a little easier to read.
+	const D = ""
+	var tests = []cases{
+		{"", D, "invalid operation: missing domain name"},
+		{"here.com", "here.com", ""},
+		{"HERE.com", "here.com", ""},     // Lower-case the domain.
+		{hugeOKDomain, hugeOKDomain, ""}, // Maximum domain element length, 63.
+		{hugeBadDomain, D, "invalid domain name element"},
+		{"a..com", D, "invalid domain name element"},
+		{"bcom", D, "domain name must contain a period"},
+		{"b@.com", D, "bad symbol in domain name"},
+		{".com", D, "invalid domain name element"},
+		{"%.com", D, "bad symbol in domain name"},
+		{"b.co", "b.co", ""},
+		{"b.c", D, "invalid domain name"},
+		{"here/.com", D, "bad symbol in domain name"},
+		{"here.com..", D, "invalid domain name"}, // Two trailing dots.
+		{"*", D, "domain name must contain a period"},
+	}
+	for _, test := range tests {
+		d, err := ParseDomain(test.domainName)
+		if test.errStr == "" {
+			if err != nil {
+				t.Errorf("%q: expected no error, got %q", test.domainName, err)
+				continue
+			}
+			if d != test.domain {
+				t.Errorf("%q: expected domain %q, got %q", test.domainName, test.domain, d)
+			}
+			continue
+		}
+		if err == nil {
+			t.Errorf("%q: expected %q error, got none", test.domainName, test.errStr)
+			continue
+		}
+		if !strings.Contains(err.Error(), test.errStr) {
+			t.Errorf("%q: expected %q, got %q", test.domainName, test.errStr, err)
 			continue
 		}
 	}
