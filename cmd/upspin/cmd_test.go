542f4940560580191a7ec8b62fa933b27178ae4e
Rob Pike <r@golang.org>
2017-06-09 15:59:16 +1000

    cmd/upspin: the beginnings of a proper test
    Do some refactoring to make it easier to write upspin tests:
    
    - provide overrides for stdin, stdout, stderr.
    - allow commands to be run one at a time from external control,
    treating the command like a package. This is the purpose of the
    new Setup and Run functions.
    
    Finally, provide a trivial new test to demonstrate the workings. This
    will expand considerably as real tests are written. The test uses
    upbox to put the environment together, then runs a single "user"
    command to test it's working.
    
    Change-Id: I84a9e880970169be7bb1de5c732c80ab8e05450c
    Reviewed-on: https://upspin-review.googlesource.com/10073
    Reviewed-by: David Presotto <presotto@gmail.com>
    Reviewed-by: Eduardo Pinheiro <epinheiro@gmail.com>

---
+++ cmd/upspin/cmd_test.go
@@ -0,0 +1,90 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+
+	"upspin.io/exp/upbox"
+)
+
+const upboxSchema = `
+users:
+  - name: ann@example.com
+servers:
+  - name: keyserver
+  - name: storeserver
+  - name: dirserver
+domain: example.com
+`
+
+func TestCommands(t *testing.T) {
+	schema, err := upbox.SchemaFromYAML(upboxSchema, 8000)
+	if err != nil {
+		t.Fatalf("setting up schema: %v", err)
+	}
+	err = schema.Start()
+	if err != nil {
+		t.Fatalf("starting schema: %v", err)
+	}
+	defer func() {
+		err = schema.Stop()
+		if err != nil {
+			t.Fatalf("stopping schema: %v", err)
+		}
+	}()
+	// Run a "user" command.
+	args := []string{
+		"-config=" + schema.Config(schema.Users[0].Name),
+		"user",
+	}
+	state, cmdArgs := setup(args)
+	stdout, stderr, err := run(t, state, cmdArgs...)
+	if err != nil {
+		t.Error(err)
+	}
+	if stderr != "" {
+		t.Fatalf("errors running user command: %s", stderr)
+	}
+	if !strings.Contains(stdout, "name: ann@example.com") {
+		t.Fatalf("bad result from user command:\n%s\n", stdout)
+	}
+}
+
+type devNull struct{}
+
+func (devNull) Write(b []byte) (int, error) { return len(b), nil }
+func (devNull) Read([]byte) (int, error)    { return 0, io.EOF }
+func (devNull) Close() error                { return nil }
+
+func run(t *testing.T, state *State, args ...string) (stdoutStr, stderrStr string, err error) {
+	stdout := new(bytes.Buffer)
+	stderr := new(bytes.Buffer)
+	state.SetIO(devNull{}, stdout, stderr)
+	defer state.DefaultIO()
+	defer func() {
+		rec := recover()
+		switch r := rec.(type) {
+		case nil:
+		case string:
+			if r == "exit" {
+				// OK; this was a subcommand calling exit
+				return
+			}
+			err = fmt.Errorf("%v", r)
+		case error:
+			err = r
+		default:
+			err = fmt.Errorf("%v", r)
+		}
+	}()
+	state.Interactive = true // So we can regain control after an error.
+	state.run(args)
+	return stdout.String(), stderr.String(), nil
+}
