460874a09e1a9b2665591badbd5c4ec43c5d783a
Andrew Gerrand <adg@golang.org>
2017-06-29 15:07:49 +1000

    exp: remove experimental components, moved to exp.upspin.io
    These packages have been moved to the exp.upspin.io repository.
    
    Change-Id: I5ce9367a4bafd4043e7a23398fed0670d4c4ebe9

--- exp/README.md
+++
@@ -1,4 +0,0 @@
-# Experimental components
-
-This directory contains components that are experimental or
-under development.
--- exp/cmd/camserver/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo '
-ls camserver@example.com
-info camserver@example.com/frame.jpg
-cp camserver@example.com/frame.jpg .
-' | upbox -config=upbox.config
