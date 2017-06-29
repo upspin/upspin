358d2cc456c81cecacfaf5be4ca4e0ed68681db9
Dave Presotto <p@google.com>
2017-06-29 15:18:14 -0700

    cmd/upspinfs: use clientutil.ReadAll instead of client.Get to read an Access file
    Since WhichAccess returns the DirEntry for the Access file, no reason to use Get
    with the file name.
    
    Change-Id: Ibd8f591f06ea4d79c804f056e7864ae8da14858d
    Reviewed-on: https://upspin-review.googlesource.com/10501
    Reviewed-by: Rob Pike <r@golang.org>
    Reviewed-by: Andrew Gerrand <adg@golang.org>

--- cmd/upspinfs/fs.go
+++ cmd/upspinfs/fs.go
@@ -25,6 +25,7 @@ import (
 	"upspin.io/access"
 	"upspin.io/bind"
 	"upspin.io/client"
+	"upspin.io/client/clientutil"
 	"upspin.io/errors"
 	"upspin.io/log"
 	"upspin.io/path"
@@ -1007,7 +1008,7 @@ func (fs *upspinFS) checkAccess(name upspin.PathName, owner upspin.UserName, rig
 		// Everyone else can do nothing.
 		return errors.E(errors.Permission, name)
 	}
-	accessData, err := fs.client.Get(whichAccess.Name)
+	accessData, err := clientutil.ReadAll(fs.config, whichAccess)
 	if err != nil {
 		return err
 	}
