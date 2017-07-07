fc53b684222a47a6c2795d871528d5d8a2affccb
Rob Pike <r@golang.org>
2017-07-07 14:57:14 +1000

    upspin: test basic commands
    Add some more thorough tests to the command tests. These check
    basic operations such as mkdir, put, get, and link. Nothing fancy.
    
    Change the way the expect helper works to avoid regular expressions.
    Too much tech, and it wasn't working right anyway.
    
    These tests raise us from a coverage of 3.4%, to 13.7%.
    Not a lot, but a step in the right direction.
    
    Change-Id: Id733c5b6db67b9353407d40c001678a678b6380d

--- cmd/upspin/allcmds_test.go
+++ cmd/upspin/allcmds_test.go
@@ -9,7 +9,8 @@ import (
 )
 
 // cmdTest describes one sequence of subcommands to be run in order.
-// These are run by TestCommands in cmd_test.go
+// These are run by the callers of testCommands in cmd_test.go
+// The tests must be run sequentially. They build state as they run.
 type cmdTest struct {
 	name  string   // The name of the test set. This appears in the -v output.
 	cmds  []string // The subcommands to run.
@@ -19,8 +20,9 @@ type cmdTest struct {
 	post func(t *testing.T, r *runner, c *cmdTest, stdout, stderr string)
 }
 
-// These tests must be run sequentially. They build state as they run.
-var cmdTests = []cmdTest{
+// basicCmdTests exercises the basic upspin commands such as put, get, etc.
+var basicCmdTests = []cmdTest{
+	// A couple of basic checks, mostly to test the test scaffolding itself.
 	{
 		"user",
 		do("user"),
@@ -34,6 +36,7 @@ var cmdTests = []cmdTest{
 			"put ann@example.com/foo",
 			"get @/foo",
 			"ls -l @/foo",
+			"rm ann@example.com/foo", // Clean up afterwards.
 		),
 		"this is ann@example.com/foo\n",
 		expect(
@@ -46,5 +49,86 @@ var cmdTests = []cmdTest{
 		do("ls ann@example.com/bar"),
 		"",
 		fail("item does not exist"),
+	},
+	// Now the tests proper. Build some state and use it.
+	{
+		"build directories",
+		do(
+			"mkdir @/Group",
+			"mkdir @/Friends @/Friends/Photo",
+			"mkdir @/Private @/Private/Photo",
+			"ls -R",
+		),
+		"",
+		expect(
+			// ls output is sorted and breadth-first.
+			"Friends",
+			"Group",
+			"Private",
+			"Friends/Photo",
+			"Private/Photo",
+		),
+	},
+	putFile(
+		"@/Group/friends",
+		"pat@example.com chris@example.com\n",
+	),
+	putFile(
+		"@/Friends/Access",
+		"r,l: friends\n*:ann@example.com\n",
+	),
+	// Create and build a Public directory, but do it wrong first to check failure.
+	{
+		"prevent read:all after content",
+		do(
+			"mkdir @/BadPublic @/BadPublic/Photo",
+			"put @/BadPublic/Access",
+		),
+		"r,l: all\n*:ann@example.com\n",
+		fail("cannot add \"read:all\""),
+	},
+	{
+		"make public directory",
+		do(
+			"rm -R @/BadPublic",
+			"mkdir @/Public",
+			"put @/Public/Access",
+			"get @/Public/Access",
+			"mkdir @/Public/Photo",
+		),
+		"r,l: all\n*:ann@example.com\n",
+		expect("r,l: all\n*:ann@example.com\n"),
+	},
+	putFile(
+		"@/Friends/Photo/friends.jpg",
+		"this is friends.jpg",
+	),
+	putFile(
+		"@/Private/Photo/private.jpg",
+		"this is private.jpg",
+	),
+	putFile(
+		"@/Public/Photo/public.jpg",
+		"this is public.jpg",
+	),
+	{
+		"link to a file",
+		do(
+			"link @/Public/Photo/public.jpg @/tmp.jpg",
+			"get @/tmp.jpg",
+			"rm @/tmp.jpg",
+		),
+		"",
+		expect("this is public.jpg"),
+	},
+	{
+		"link to a directory",
+		do(
+			"link @/Public/Photo @/tmpdir",
+			"get @/tmpdir/public.jpg",
+			"rm @/tmpdir",
+		),
+		"",
+		expect("this is public.jpg"),
 	},
 }
