7c44135c9925b28392bd834e754ae2db7dfb15db
Eduardo Pinheiro <epinheiro@gmail.com>
2017-07-18 14:17:59 -0700

    store/server: fix test
    Fixes #432
    
    Change-Id: I33f388becca7a60da60a0b671022ef78bebaa8b3

--- store/server/server_test.go
+++ store/server/server_test.go
@@ -5,7 +5,6 @@
 package server
 
 import (
-	"strings"
 	"testing"
 
 	"upspin.io/cloud/storage"
@@ -81,24 +80,24 @@ func TestNew(t *testing.T) {
 	if err == nil {
 		t.Fatalf("Expected error")
 	}
-	expected := "invalid operation"
-	if !strings.Contains(err.Error(), expected) {
-		t.Errorf("Expected %q, got %q", expected, err)
+	expErr := errors.E(errors.Invalid)
+	if !errors.Match(expErr, err) {
+		t.Errorf("Expected %v, got %v", expErr, err)
 	}
 
-	_, err = New("backend=disk,dance=the macarena")
+	_, err = New("backend=Disk", "basePath=/tmp", "dance=the macarena")
 	if err == nil {
 		t.Fatalf("Expected error")
 	}
-	expected = "invalid operation"
-	if !strings.Contains(err.Error(), expected) {
-		t.Errorf("Expected %q, got %q", expected, err)
+	expErr = errors.E(errors.IO)
+	if !errors.Match(expErr, err) {
+		t.Errorf("Expected %v, got %v", expErr, err)
 	}
 
 	if testing.Short() {
 		t.Skip("skipping part of test when network unavailable; depends on credential availability")
 	}
-	_, err = New("backend=Disk")
+	_, err = New("backend=Disk", "basePath=++/tmp")
 	if err != nil {
 		t.Fatal(err)
 	}
