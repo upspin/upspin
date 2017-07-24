8647221ee41a1f460bcd033f3b131a328f3855f1
Andrew Gerrand <adg@golang.org>
2017-07-24 11:49:08 +1000

    upspin: replace non-essential Config methods with a Value method
    The CacheEndpoint, CertPool, and Flags methods of the Config interface
    were added to plumb various values from the config file through to the
    parts of the system that need them. As we ask more of the config file
    we may be inclined to add more methods to upspin.Config, but we should
    resist.
    
    Instead, this change adds a catch-all Value method that returns the
    string data (that may be YAML-structured) for a given config file key,
    and updates the usages of CacheEndpoint, CertPool, and Flags to use the
    Value method instead.
    
    To aid in this we add a SetValue helper to the config package
    and a CacheEndpoint helper to the rpc package.
    
    Change-Id: I374f49ba284819f0cf3a87dffe132ea1263eb6c3

---
+++ rpc/cache.go
@@ -0,0 +1,33 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package rpc
+
+import (
+	"upspin.io/errors"
+	"upspin.io/rpc/local"
+	"upspin.io/upspin"
+)
+
+func CacheEndpoint(cfg upspin.Config) (*upspin.Endpoint, error) {
+	const op = "rpc.CacheEndpoint"
+
+	v := cfg.Value("cache")
+	if v == "" {
+		return nil, nil
+	}
+	// TODO(adg): do we need to parse a potential endpoint here? There was
+	// a TODO(p) in package config to phase out cacheserver endpoints.
+	switch v {
+	case "y", "yes", "true":
+		name := local.LocalName(cfg, "cacheserver")
+		ep, err := upspin.ParseEndpoint(name)
+		if err != nil {
+			return nil, errors.E(op, errors.Internal, err)
+		}
+		return ep, nil
+	default:
+		return nil, nil
+	}
+}
---
+++ rpc/certpool.go
@@ -0,0 +1,65 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package rpc
+
+import (
+	"crypto/x509"
+	"io/ioutil"
+	"path/filepath"
+	"sync"
+
+	"upspin.io/errors"
+	"upspin.io/upspin"
+)
+
+var certPoolCache struct {
+	sync.Mutex
+	m map[string]*x509.CertPool // [dir]pool
+}
+
+func certPoolFromConfig(cfg upspin.Config) (*x509.CertPool, error) {
+	dir := cfg.Value("tlscerts")
+	if dir == "" {
+		return nil, nil
+	}
+	certPoolCache.Lock()
+	defer certPoolCache.Unlock()
+	if p := certPoolCache.m[dir]; p != nil {
+		return p, nil
+	}
+	p, err := certPoolFromDir(dir)
+	if err == nil {
+		if certPoolCache.m == nil {
+			certPoolCache.m = make(map[string]*x509.CertPool)
+		}
+		certPoolCache.m[dir] = p
+	}
+	return p, err
+}
+
+// certPoolFromDir parses any PEM files in the provided directory
+// and returns the resulting pool.
+func certPoolFromDir(dir string) (*x509.CertPool, error) {
+	var pool *x509.CertPool
+	fis, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, errors.Errorf("reading TLS Certificates in %q: %v", dir, err)
+	}
+	for _, fi := range fis {
+		name := fi.Name()
+		if filepath.Ext(name) != ".pem" {
+			continue
+		}
+		pem, err := ioutil.ReadFile(filepath.Join(dir, name))
+		if err != nil {
+			return nil, errors.Errorf("reading TLS Certificate %q: %v", name, err)
+		}
+		if pool == nil {
+			pool = x509.NewCertPool()
+		}
+		pool.AppendCertsFromPEM(pem)
+	}
+	return pool, nil
+}
