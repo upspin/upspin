8699357b3acdf7ff086e5e10ab8a089143a00079
Rob Pike <r@golang.org>
2017-07-26 12:01:57 +1000

    cmd/frontend: provide a -local flag to simplify starting a local instance
    I can never remember how to set up a local frontend server, and it's not
    obvious from the documentation. Fix both issues by implementing and
    documenting a -local flag that handles all the details.
    
    Now
    	frontend -local
    runs a local, insecure instance on localhost:8080.
    
    Change-Id: I1878b4efa79eb2da9f41f82d56579217cc80280c

--- cmd/frontend/frontend.go
+++ cmd/frontend/frontend.go
@@ -4,6 +4,10 @@
 
 // Frontend provides a web server that serves documentation and meta
 // tags to instruct "go get" where to find the Upspin source repository.
+//
+// The -local flag is for running a local binary, such as for testing the
+// documentation. With this flag set, frontend ignores the config file, runs HTTP
+// only on localhost:8080 and does not serve release binaries.
 package main // import "upspin.io/cmd/frontend"
 
 import (
