2aa5149d01fc08b0123e7f31d6ea04806586585a
Dave Presotto <presotto@gmail.com>
2017-09-06 08:11:35 -0700

    pack,client,cmd/upspinfs: Add SetTime for DirEntries
    Allows the changing of the Time field in a DirEntry. In client.SetTime,
    if the PathName refers to a link, do not follow it but instead change
    the Time of the link itself.
    
    Also change client.Dup and client.Rename link behavior to match.
    
    Fixes #440
    
    Change-Id: I5f6666a31e425511665ff98987eea69120d4b4fd

--- client/client.go
+++ client/client.go
@@ -743,8 +743,35 @@ func (c *Client) Rename(oldName, newName upspin.PathName) error {
 	return err
 }
 
+// SetTime implements upspin.Client.
+func (c *Client) SetTime(name upspin.PathName, t upspin.Time) error {
+	const op = "client.SetTime"
+	m, s := newMetric(op)
+	defer m.Done()
+
+	entry, _, err := c.lookup(op, &upspin.DirEntry{Name: name}, lookupLookupFn, doNotFollowFinalLink, s)
+	if err != nil {
+		return errors.E(op, err)
+	}
+
+	packer := pack.Lookup(entry.Packing)
+	if packer == nil {
+		return errors.E(op, name, errors.Invalid, errors.Errorf("unrecognized Packing %d", c.config.Packing()))
+	}
+	if err := packer.SetTime(c.config, entry, t); err != nil {
+		return errors.E(op, err)
+	}
+
+	// Record directory entry.
+	_, _, err = c.lookup(op, entry, putLookupFn, doNotFollowFinalLink, s)
+	if err != nil {
+		return errors.E(op, err)
+	}
+	return nil
+}
+
 func (c *Client) dupOrRename(op string, oldName, newName upspin.PathName, rename bool, s *metric.Span) (*upspin.DirEntry, error) {
-	entry, _, err := c.lookup(op, &upspin.DirEntry{Name: oldName}, lookupLookupFn, followFinalLink, s)
+	entry, _, err := c.lookup(op, &upspin.DirEntry{Name: oldName}, lookupLookupFn, doNotFollowFinalLink, s)
 	if err != nil {
 		return nil, err
 	}
@@ -782,7 +809,7 @@ func (c *Client) dupOrRename(op string, oldName, newName upspin.PathName, rename
 		return nil, errors.E(op, err)
 	}
 	if !oldParsed.Drop(1).Equal(newParsed.Drop(1)) {
-		accessEntry, _, err := c.lookup(op, entry, whichAccessLookupFn, followFinalLink, s)
+		accessEntry, _, err := c.lookup(op, entry, whichAccessLookupFn, doNotFollowFinalLink, s)
 		if err != nil {
 			return nil, errors.E(op, trueOldName, err)
 		}
@@ -796,7 +823,7 @@ func (c *Client) dupOrRename(op string, oldName, newName upspin.PathName, rename
 	}
 
 	// Record directory entry.
-	entry, _, err = c.lookup(op, entry, putLookupFn, followFinalLink, s)
+	entry, _, err = c.lookup(op, entry, putLookupFn, doNotFollowFinalLink, s)
 	if err != nil {
 		return nil, err
 	}
