9f6012bc67abc9d38dd09dc5f293d4af60e7a39a
Andrew Gerrand <adg@golang.org>
2017-09-28 22:10:38 +1000

    rpc: remove log print that caused a panic
    If a user without a Factotum tries to make an authenticated RPC request
    then this error path will be taken, but then trying to log the
    Factotum().PublicKey() would trigger a panic. Remove the redundant log
    message; the returned error should be sufficient.
    
    Change-Id: I6c98637b753773d3805a06c763700505e9a8e04b
    Reviewed-on: https://upspin-review.googlesource.com/13320
    Reviewed-by: Rob Pike <r@golang.org>

--- rpc/client.go
+++ rpc/client.go
@@ -19,7 +19,6 @@ import (
 
 	"upspin.io/bind"
 	"upspin.io/errors"
-	"upspin.io/log"
 	"upspin.io/rpc/local"
 	"upspin.io/upspin"
 
@@ -152,7 +151,6 @@ func (c *httpClient) makeAuthenticatedRequest(op, method string, req pb.Message)
 		// Otherwise prepare an auth request.
 		authMsg, err := signUser(c.config, clientAuthMagic, serverAddr(c))
 		if err != nil {
-			log.Error.Printf("%s: signUser: %s using key %s", op, err, c.config.Factotum().PublicKey())
 			return nil, false, errors.E(op, err)
 		}
 		header.Set(authRequestHeader, strings.Join(authMsg, ","))
