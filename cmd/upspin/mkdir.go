52ada60d59f7eba128cad708bbfb6424d1933b46
Andrew Gerrand <adg@golang.org>
2017-10-22 09:59:13 +1100

    cmd/upspin: implement mkdir -p
    Change-Id: I66d76d4fd85706f8e3f06145a11c37436182db0f

--- cmd/upspin/mkdir.go
+++ cmd/upspin/mkdir.go
@@ -4,7 +4,13 @@
 
 package main
 
-import "flag"
+import (
+	"flag"
+
+	"upspin.io/errors"
+	"upspin.io/path"
+	"upspin.io/upspin"
+)
 
 func (s *State) mkdir(args ...string) {
 	const help = `
@@ -15,15 +21,30 @@ treating its arguments as literal text even if they contain special
 characters. (Leading @ signs are always expanded.)
 `
 	fs := flag.NewFlagSet("mkdir", flag.ExitOnError)
+	parent := fs.Bool("p", false, "Make all parent directories.")
 	glob := globFlag(fs)
-	s.ParseFlags(fs, args, help, "mkdir directory...")
+	s.ParseFlags(fs, args, help, "mkdir [-p] directory...")
 	if fs.NArg() == 0 {
 		usageAndExit(fs)
 	}
 	for _, name := range s.expandUpspin(fs.Args(), *glob) {
-		_, err := s.Client.MakeDirectory(name)
-		if err != nil {
-			s.Exit(err)
-		}
+		s.doMkdir(name, *parent)
+	}
+}
+
+func (s *State) doMkdir(name upspin.PathName, parent bool) {
+	p, err := path.Parse(name)
+	if err != nil {
+		s.Exit(err)
+	}
+	if parent && p.NElem() > 0 {
+		s.doMkdir(p.Drop(1).Path(), parent)
+	}
+	_, err = s.Client.MakeDirectory(name)
+	if parent && errors.Match(errors.E(errors.Exist), err) {
+		return
+	}
+	if err != nil {
+		s.Exit(err)
 	}
 }
