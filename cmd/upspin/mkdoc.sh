115777e3fed437ff51c66b26ae2eca3bf873dca9
Rob Pike <r@golang.org>
2017-10-23 10:53:35 +1100

    cmd/upspin: fix gendoc so the defaults from mkdoc.sh point to example.com
    When running gendoc, it was using the config of the person doing the
    invocation, which was potentially dangerous and at best silly and causing
    needless churn as different people ran it.
    
    Change gendoc to pay attention to the arguments, and then run it
    with a temporary config for ann@example.com.
    
    Also add a -config flag to upspin-setupstorage. It is worthless
    but its absence broke the docs generated by providing the
    flag everywhere.
    
    Change-Id: I207b5c6d4f5514374897621b917c7c5b68eefc1e

--- cmd/upspin/mkdoc.sh
+++ cmd/upspin/mkdoc.sh
@@ -1,5 +1,22 @@
 #!/bin/bash -e
 
+# Create a temporary config so the defaults point to example.com.
+mkdir /tmp/gendoc$$
+
+trap "rm -rf /tmp/gendoc$$" 0
+
+upspin keygen /tmp/gendoc$$ > /dev/null 2>&1
+
+cat >/tmp/gendoc$$/config <<!
+username: ann@example.com
+packing: ee
+
+keyserver: remote,key.example.com:443
+dirserver: remote,dir.example.com:443
+storeserver: remote,store.example.com:443
+secrets: /tmp/gendoc$$
+!
+
 go build -tags gendoc -o upspin.gendoc
-./upspin.gendoc gendoc
+./upspin.gendoc -config /tmp/gendoc$$/config gendoc
 rm upspin.gendoc
