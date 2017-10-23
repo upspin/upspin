06799fd4548296dfd9ac658f62ce2f5e97cb5897
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2017-10-23 13:20:44 +0200

    multiple: allow running insecure local instances of {key,dir,store}servers
    This CL is only mailed here to stimulate discussion and update #276.
    
    Change-Id: I7004fc79eea7e9f71b8162c3223761b42b30e832

---
+++ serverutil/addr.go
@@ -0,0 +1,33 @@
+package serverutil
+
+import (
+	"net"
+
+	"upspin.io/rpc/local"
+)
+
+// IsLoopback returns true if the name only resolves to loopback addresses.
+func IsLoopback(addr string) bool {
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		host = addr
+	}
+	if host == "localhost" || host == "127.0.0.1" || host == "::1" {
+		return true
+	}
+	// Check for local IPC.
+	if local.IsLocal(host) {
+		return true
+	}
+	// Check for loopback network.
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		return false
+	}
+	for _, ip := range ips {
+		if !ip.IsLoopback() {
+			return false
+		}
+	}
+	return true
+}
