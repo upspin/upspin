598d2a0d8f90ab8861ff9dfae13228499d8444e9
Dave Presotto <p@google.com>
2017-11-30 14:47:36 -0800

    dir/dircache: check access before put's and delete's
    In preparation for writeback of DirEntries (as opposed to the
    current writethrough), I've added a check in the Put and
    Delete RPCs. I don't currently obey the result, just check
    it against what the server really does to make sure I've
    got it correct.
    
    The next step will be an actual writeback queue.
    
    Change-Id: I22a40483f599a51d99ac6bff17e1789235554529

--- dir/dircache/server.go
+++ dir/dircache/server.go
@@ -12,15 +12,28 @@ import (
 
 	"upspin.io/access"
 	"upspin.io/bind"
+	"upspin.io/client/clientutil"
 	"upspin.io/errors"
 	"upspin.io/log"
 	"upspin.io/path"
 	"upspin.io/upspin"
 )
 
+// parsedAccess contains a parsed Access file and its sequence number.
+type parsedAccess struct {
+	a   *access.Access
+	seq int64
+}
+
 // server is a SecureServer that talks to a DirServer interface and serves requests.
 type server struct {
+	// cfg is a config that points directly to the uncached server.
 	cfg upspin.Config
+
+	// loopbackCfg is a config that points back at this dircache.
+	// It exists to allow dircache to use itself and the associated
+	// storecache when reading access and group files.
+	loopbackCfg upspin.Config
 
 	// The on disk log.
 	clog *clog
@@ -31,18 +44,23 @@ type server struct {
 
 	// The directory server this dialed server should talk to.
 	authority upspin.Endpoint
+
+	// Access file cache.
+	accessFiles map[upspin.PathName]parsedAccess
 }
 
 // New creates a new DirServer cache reading in the log and writing out a new compacted log.
-func New(cfg upspin.Config, cacheDir string, maxLogBytes int64, flushBlock func(upspin.Location)) (upspin.DirServer, error) {
+func New(cfg, loopbackCfg upspin.Config, cacheDir string, maxLogBytes int64, flushBlock func(upspin.Location)) (upspin.DirServer, error) {
 	clog, err := openLog(cfg, ospath.Join(cacheDir, "dircache"), maxLogBytes)
 	if err != nil {
 		return nil, err
 	}
 	return &server{
-		cfg:        cfg,
-		clog:       clog,
-		flushBlock: flushBlock,
+		cfg:         cfg,
+		loopbackCfg: loopbackCfg,
+		clog:        clog,
+		flushBlock:  flushBlock,
+		accessFiles: make(map[upspin.PathName]parsedAccess),
 	}, nil
 }
 
@@ -141,9 +159,15 @@ func (s *server) Put(entry *upspin.DirEntry) (*upspin.DirEntry, error) {
 		return dir.Put(entry)
 	}
 
-	// Since the directory server needs to read the Access/Group file
-	// we need to ensure that it is flushed from any cache before the Put.
-	if s.flushBlock != nil && access.IsAccessControlFile(entry.Name) {
+	// Can we Put?
+	// Note: we are currently just using granted and accErr as a check to make sure
+	// we have correctly matched the access semantics of the server. In the near
+	// future it will be used to allow writebacks rather than writethroughs of
+	// DirEntries.
+	granted, accErr := s.canPut(name, entry)
+
+	// Wait for Access and Group block writes to flush.
+	if s.flushBlock != nil && access.IsAccessControlFile(name) {
 		for _, b := range entry.Blocks {
 			s.flushBlock(b.Location)
 		}
@@ -153,21 +177,33 @@ func (s *server) Put(entry *upspin.DirEntry) (*upspin.DirEntry, error) {
 	defer s.clog.globalLock.Unlock()
 
 	de, err := dir.Put(entry)
-	if err == nil {
-		// If the put worked, remember it.
-		if de != nil {
-			entry.Sequence = de.Sequence
-			s.clog.inSequence(entry.Name, entry.Sequence)
+	if err != nil {
+		// Keep track of our access checks until we are sure they
+		// match the server.
+		if granted {
+			log.Error.Printf("access refused but we predicted granted: %s, %v, %s", name, *entry, err)
 		}
-		s.clog.logRequest(putReq, name, err, entry)
-
-		// If this was a Put of the root, retry the watch.
-		parsed, perr := path.Parse(entry.Name)
-		if perr == nil && parsed.IsRoot() {
-			s.clog.retryWatch(parsed)
-		}
+		return de, err
 	}
 
+	// If the put worked, remember it.
+	if de != nil {
+		entry.Sequence = de.Sequence
+		s.clog.inSequence(entry.Name, entry.Sequence)
+	}
+	s.clog.logRequest(putReq, name, err, entry)
+
+	// If this was a Put of the root, retry the watch.
+	parsed, perr := path.Parse(entry.Name)
+	if perr == nil && parsed.IsRoot() {
+		s.clog.retryWatch(parsed)
+	}
+
+	// Keep track of our access checks until we are sure they
+	// match the server.
+	if !granted {
+		log.Error.Printf("access granted but we predicted refused: %s, %s", name, accErr)
+	}
 	return de, err
 }
 
@@ -185,11 +221,30 @@ func (s *server) Delete(name upspin.PathName) (*upspin.DirEntry, error) {
 		return dir.Delete(name)
 	}
 
+	// Can we Delete?
+	// Note: we are currently just using granted and accErr as a check to make sure
+	// we have correctly matched the access semantics of the server. In the near
+	// future it will be used to allow writebacks rather than writethroughs of
+	// DirEntries.
+	granted, accErr := s.can(name, access.Delete)
+
 	s.clog.globalLock.Lock()
 	defer s.clog.globalLock.Unlock()
 
 	de, err := dir.Delete(name)
 	s.clog.logRequest(deleteReq, name, err, de)
+
+	// Keep track of our access checks until we are sure they
+	// match the server.
+	if granted {
+		if err != nil {
+			log.Error.Printf("access refused but we predicted granted: %s, %s", name, err)
+		}
+	} else {
+		if err == nil {
+			log.Error.Printf("access granted but we predicted refused: %s, %s", name, accErr)
+		}
+	}
 
 	return de, err
 }
@@ -246,4 +301,100 @@ type operation string
 
 func (op operation) log(err error) {
 	logf("%v failed: %v", op, err)
+}
+
+// This must be called while holding locks since it recurses into the cacheserver.
+func (s *server) can(name upspin.PathName, right access.Right) (bool, error) {
+	// Talk to myself to determine which Access to use.
+	de, err := s.WhichAccess(name)
+	if err != nil {
+		return false, err
+	}
+	if de == nil {
+		// No Access file. If this is the owner,
+		// allow any access, otherwise no access.
+		p, err := path.Parse(name)
+		if err != nil {
+			return false, err
+		}
+		if p.User() != s.cfg.UserName() {
+			return false, errors.E("not owner")
+		}
+		return true, nil
+	}
+
+	// Get a parsed version of it.
+	acc, err := s.readAndParseAccess(de)
+	if err != nil {
+		return false, err
+	}
+
+	// Can we access?
+	return acc.Can(s.cfg.UserName(), right, name, s.load)
+}
+
+// This must be called while holding locks since it recurses into the cacheserver.
+func (s *server) canPut(name upspin.PathName, de *upspin.DirEntry) (bool, error) {
+	switch de.Sequence {
+	case upspin.SeqIgnore:
+		break
+	case upspin.SeqNotExist:
+		if _, err := s.Lookup(name); err == nil {
+			return false, errors.E(name, errors.Exist)
+		}
+	default:
+		ode, err := s.Lookup(name)
+		if err != nil {
+			return false, errors.E(name, errors.NotExist)
+		}
+		if ode.Sequence != de.Sequence {
+			return false, errors.E(name, errors.Invalid, "sequence number")
+		}
+	}
+	return s.can(name, access.Write)
+}
+
+// readAndParseAccess returns a parsed Access file. It maintains
+// a cache of parsed access files checking the current sequence
+// number to make sure the parsed version is current.
+//
+// This must be called while holding locks since it recurses
+// into itself via the ReadAll.
+//
+// TODO: In the future we may need to write our own version of
+// client.Get and client.ReadAll to avoid the extra overhead
+// of the RPCs. However, client.Get is complex enough that
+// we're doing it this way the first time around with the
+// assumption that the parsed access file cache will make the
+// extra overhead insignificant.
+func (s *server) readAndParseAccess(de *upspin.DirEntry) (*access.Access, error) {
+	// First look in cache.
+	pa, ok := s.accessFiles[de.Name]
+	if ok && pa.seq == de.Sequence {
+		return pa.a, nil
+	}
+
+	// Not found or out of date. Fetch one.
+	contents, err := clientutil.ReadAll(s.loopbackCfg, de)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse and put into the cache.
+	acc, err := access.Parse(de.Name, contents)
+	if err != nil {
+		return nil, err
+	}
+	s.accessFiles[de.Name] = parsedAccess{acc, de.Sequence}
+
+	return acc, nil
+}
+
+// load retrieves the contents of a file via the caches.
+func (s *server) load(name upspin.PathName) ([]byte, error) {
+	de, err := s.Lookup(name)
+	if err != nil {
+		return nil, err
+	}
+	return clientutil.ReadAll(s.loopbackCfg, de)
 }
