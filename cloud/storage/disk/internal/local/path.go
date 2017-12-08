59e2ab5109f572a08e1bd464dca63fd7d67759c9
Andrew Gerrand <adg@golang.org>
2017-12-08 11:48:16 +1100

    cloud/storage/disk: implement storage.Lister
    Change-Id: I4ab0557caf356c27c43c3df5b1b8dd8f197289ba

--- cloud/storage/disk/internal/local/path.go
+++ cloud/storage/disk/internal/local/path.go
@@ -7,6 +7,9 @@ package local // import "upspin.io/cloud/storage/disk/internal/local"
 import (
 	"encoding/base64"
 	"path/filepath"
+	"strings"
+
+	"upspin.io/errors"
 )
 
 // Both encodings compute the base64 encoding of the reference.
@@ -17,6 +20,12 @@ import (
 //	ab/cd/de/ef+
 // Here the + is used as a pad to guarantee the file is not confusable
 // with a directory. The tree is never longer than 5 levels.
+
+// Use RawURLEncoding to ensure path-safe characters.
+var enc = base64.RawURLEncoding
+
+// Maximum number of directories to create in a single path.
+const numDirs = 5
 
 // Path returns the file path to hold the contents of the blob with the
 // specified reference. The returned path is rooted in the provided base
@@ -31,16 +40,9 @@ func Path(base, ref string) string {
 	// so we use + as a pad. We also pad to make sure the path is long
 	// enough.
 	// We avoid multiple allocations by thinking ahead.
-	enc := base64.RawURLEncoding
 	length := enc.EncodedLen(len(ref))
 	buf := make([]byte, length, length+4) // Extra room to pad.
 	enc.Encode(buf, []byte(ref))
-	const (
-		// Maximum number of directories to create in a single path.
-		// The longest path is therefore base plus numDirs plus a final element.
-		numDirs  = 5
-		numElems = 1 + numDirs + 1 // Base plus directories plus file name.
-	)
 	// Need at least 3 bytes, two for a directory and one for a file.
 	// An empty ref gets the blob name "..../++/+".
 	for len(buf) < 3 {
@@ -50,7 +52,8 @@ func Path(base, ref string) string {
 	if len(buf) < 2*numDirs+1 && len(buf)%2 == 0 {
 		buf = append(buf, '+')
 	}
-	str := string(buf) // This is the blob name to turn into a file path name.
+	str := string(buf)               // This is the blob name to turn into a file path name.
+	const numElems = 1 + numDirs + 1 // Base plus directories plus file name.
 	elems := make([]string, 0, numElems)
 	elems = append(elems, base)
 	for i := 1; i < numElems-1; i++ {
@@ -62,6 +65,28 @@ func Path(base, ref string) string {
 	}
 	elems = append(elems, str)
 	return filepath.Join(elems...)
+}
+
+// Ref returns the reference for the given path name. The path name must have
+// the storage base path stripped from it, and should not begin with a path
+// separator.
+func Ref(path string) (string, error) {
+	// A sanity check.
+	if strings.Count(path, string(filepath.Separator)) > numDirs {
+		return "", errors.Errorf("path %q contains too many directories", path)
+	}
+	// Remove path separators.
+	encoded := strings.Replace(path, string(filepath.Separator), "", -1)
+	// Remove trailing padding (+ symbols).
+	if i := strings.Index(encoded, "+"); i >= 0 {
+		encoded = encoded[:i]
+	}
+	// Decode base64-encoded path.
+	ref, err := enc.DecodeString(encoded)
+	if err != nil {
+		return "", errors.Errorf("%q: %v", encoded, err)
+	}
+	return string(ref), nil
 }
 
 // OldPath returns the file path to hold the contents of the blob with the
