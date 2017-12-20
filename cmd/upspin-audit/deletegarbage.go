f30946769229622c99d4c6d7223f0b68df3b2b30
Andrew Gerrand <adg@golang.org>
2017-12-20 12:25:38 +1100

    cmd/upspin-audit: add -backup flag to delete-garbage subcommand
    This change adds a -backup flag to make local copies of blocks before
    they are deleted from the store server.
    
    A future change may add a mechanism to restore deleted blocks.
    
    Change-Id: Iaf0cacd266763ce4ac6ecee282538bc9e1305aba

--- cmd/upspin-audit/deletegarbage.go
+++ cmd/upspin-audit/deletegarbage.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 
 	"upspin.io/bind"
+	"upspin.io/cloud/storage"
+	"upspin.io/cloud/storage/disk"
 	"upspin.io/errors"
 	"upspin.io/upspin"
 )
@@ -22,15 +24,32 @@ run of find-garbage. It operates on the store endpoint of the current user.
 It must be run as the same Upspin user as the store server itself,
 as only that user has permission to delete blocks.
 
+The -backup flag specifies a local directory in which to store local
+copies of the blocks before they are deleted.
+
 Misuse of this command may result in permanent data loss. Use with caution.
 `
 	fs := flag.NewFlagSet("delete-garbage", flag.ExitOnError)
 	dataDir := dataDirFlag(fs)
+	backupDir := fs.String("backup", "", "local directory in which to store deleted blocks")
 	s.ParseFlags(fs, args, help, "audit delete-garbage")
 
 	if fs.NArg() != 0 {
 		fs.Usage()
 		os.Exit(2)
+	}
+
+	var backup storage.Storage
+	if *backupDir != "" {
+		var err error
+		backup, err = disk.New(&storage.Opts{
+			Opts: map[string]string{
+				"basePath": *backupDir,
+			},
+		})
+		if err != nil {
+			s.Exit(err)
+		}
 	}
 
 	for _, fi := range s.latestFilesWithPrefix(*dataDir, garbageFilePrefix) {
@@ -48,10 +67,11 @@ Misuse of this command may result in permanent data loss. Use with caution.
 		}
 		const numWorkers = 10
 		d := deleter{
-			State: s,
-			store: store,
-			refs:  make(chan upspin.Reference),
-			stop:  make(chan bool, numWorkers),
+			State:  s,
+			store:  store,
+			backup: backup,
+			refs:   make(chan upspin.Reference),
+			stop:   make(chan bool, numWorkers),
 		}
 		for i := 0; i < numWorkers; i++ {
 			go d.worker()
@@ -74,17 +94,38 @@ Misuse of this command may result in permanent data loss. Use with caution.
 
 // deleter holds the state of delete-garbage workers.
 type deleter struct {
-	State *State
-	store upspin.StoreServer
-	refs  chan upspin.Reference
-	stop  chan bool
+	State  *State
+	store  upspin.StoreServer
+	backup storage.Storage // skip backup if nil
+	refs   chan upspin.Reference
+	stop   chan bool
 }
 
 // worker receives refs from refs and deletes them from store. If the store
 // return a permission error then worker sends a value to stop.
 func (d *deleter) worker() {
 	for ref := range d.refs {
+		if d.backup != nil {
+			b, _, _, err := d.store.Get(ref)
+			if errors.Is(errors.NotExist, err) {
+				continue
+			}
+			if err != nil {
+				d.State.Fail(err)
+				continue
+			}
+			if err := d.backup.Put(string(ref), b); err != nil {
+				d.State.Fail(err)
+				// Stop the entire process if we fail to write the
+				// block locally, as future puts will likely fail also.
+				d.stop <- true
+				return
+			}
+		}
 		err := d.store.Delete(ref)
+		if errors.Is(errors.NotExist, err) {
+			continue
+		}
 		if err != nil {
 			d.State.Fail(err)
 			// Stop the entire process if we get a permission error;
