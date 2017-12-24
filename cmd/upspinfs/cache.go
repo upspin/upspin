d525129aceba1a46b69567444050a619f8eb8750
Dave Presotto <presotto@gmail.com>
2017-12-24 11:01:51 -0800

    cmd/upspinfs: replace timed refeshes in upspinfs with Watch based refresh
    The previous version had a very short valid time for anything it
    gave the kernel to limit the periods of inconsistency between
    the kernel and the directory servers. It also refreshed any
    cached DirEntries fairly often to limit its own inconsistency.
    This created performance issues where we could only trade
    performance agains time of inconsistency.
    
    This version uses Watch to maintain consistency. There is still
    a window of inconsistency as the Watch events percolate across
    the network but a much smaller one. Also since we know when things
    change, we can force the kernel to invalidate cached information
    and thereby feel safe in increasing the kernel cache validity time.
    
    A somewhat related change in dircache removes an unnecessary check
    that was proportional to the size of a directory. This two was
    affecting performance, though only in very large directories.
    
    The new watch.go is very similar to proxied.go in dircache. I
    should eventually merge them but not until I get this working
    and have had some experience with it.
    
    Fixes #549
    
    DO NOT REVIEW
    
    Not quite ready for prime time. I still have to figure out how
    to handle DirServers that don't implement Watch.
    
    Change-Id: I8df66be9d606f7d43dfd4abd4bc3d9f389267f57

--- cmd/upspinfs/cache.go
+++ cmd/upspinfs/cache.go
@@ -147,9 +147,6 @@ func (c *cache) open(h *handle, flags fuse.OpenFlags) error {
 		}
 	}
 
-	// Invalidate the kernel cache, this is a new version.
-	n.f.invalidateChan <- n
-
 	// Create an unpacker to decrypt the file blocks.
 	packer := pack.Lookup(entry.Packing)
 	if packer == nil {
