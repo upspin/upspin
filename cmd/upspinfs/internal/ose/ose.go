0fc80133937cd86fbcd778ddda775f09c97ce5b9
Eric Grosse <grosse@gmail.com>
2018-04-08 12:11:00 -0700

    upspinfs/internal/ose: fill hole in WriteAt
    ose.File needs to explicitly encrypt zero holes, not use
    the underlying os sparse files.
    
    There is still a test failure for Rename, which is
    listed as issue #580.
    
    Fix #578.
    
    Change-Id: I189ddb3f7326e68fdff895759f8270ff8bd18300
    Reviewed-on: https://upspin-review.googlesource.com/18880
    Reviewed-by: David Presotto <presotto@gmail.com>

--- cmd/upspinfs/internal/ose/ose.go
+++ cmd/upspinfs/internal/ose/ose.go
@@ -24,6 +24,7 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/binary"
+
 	"upspin.io/cache"
 
 	"fmt"
@@ -55,11 +56,13 @@ var state = struct {
 }
 
 // File represents an encrypted file.
+// There are no holes, in other words the disk file is not sparse.
 type File struct {
 	name string
 	f    *os.File
 	benc cipher.Block
 	refs int
+	size int64
 }
 
 // OpenFile opens an encrypted file.
@@ -70,6 +73,10 @@ func OpenFile(name string, flag int, mode os.FileMode) (*File, error) {
 	}
 	state.Lock()
 	defer state.Unlock()
+	st, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
 	file, ok := state.nameToFile[name]
 	if ok {
 		file.f.Close()
@@ -83,6 +90,7 @@ func OpenFile(name string, flag int, mode os.FileMode) (*File, error) {
 	}
 	file.f = f
 	file.refs++
+	file.size = st.Size()
 	state.toRemove.Remove(file.name)
 	return file, nil
 }
@@ -108,6 +116,7 @@ func Create(name string) (*File, error) {
 	}
 	file.f = f
 	file.refs++
+	file.size = 0
 	state.toRemove.Remove(file.name)
 	return file, nil
 }
@@ -155,7 +164,17 @@ func RemoveAll(subtree string) error {
 
 // Truncate shortens a file.
 func Truncate(name string, size int64) error {
-	return os.Truncate(name, size)
+	state.Lock()
+	defer state.Unlock()
+	file, ok := state.nameToFile[name]
+	if !ok {
+		return fmt.Errorf("file to truncate doesn't exist: %s", name)
+	}
+	err := os.Truncate(name, size)
+	if err == nil {
+		file.size = size
+	}
+	return err
 }
 
 // Close closes a file. If the ref count goes to zero, the file is removed.
@@ -195,8 +214,37 @@ func (file *File) ReadAt(b []byte, off int64) (int, error) {
 // WriteAt encrypts the content and writes it to the file.
 // Unlike os.WriteAt, this changes the contents of b.
 func (file *File) WriteAt(b []byte, off int64) (int, error) {
+	if off > file.size {
+		// This WriteAt implicitly extends the file. Fill the hole.
+		state.Lock()
+		for off > file.size {
+			m := off - file.size
+			if m > 1024*1024 {
+				m = 1024 * 1024
+			}
+			hole := make([]byte, m)
+			file.xor(hole, file.size)
+			n, err := file.f.WriteAt(hole, file.size)
+			if err != nil {
+				state.Unlock()
+				return 0, err
+			}
+			if n == 0 {
+				state.Unlock()
+				return 0, fmt.Errorf("zero write filling hole, expected %d", len(hole))
+			}
+			file.size += int64(n)
+		}
+		state.Unlock()
+	}
 	file.xor(b, off)
-	return file.f.WriteAt(b, off)
+	n, err := file.f.WriteAt(b, off)
+	state.Lock()
+	if n > 0 && file.size < off+int64(n) {
+		file.size = off + int64(n)
+	}
+	state.Unlock()
+	return n, err
 }
 
 const aesKeyLen = 32
