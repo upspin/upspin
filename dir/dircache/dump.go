03c2bda3698ddc4ffd4b3166cf0fd74c3d39b9ee
Rob Pike <r@golang.org>
2022-12-03 15:13:49 +1100

    dir/dircache: remove + from comment about build constraint
    At some point vet started looking harder for build constraints, and that
    broke our build because the "+build" string in the comment about the
    "+build" constraint made it think we had a bad constraint.
    
    Thanks to rsc for figuring this out. I doubt I ever would have.
    
    Change-Id: I0ecb448c7ccf94ae8cfd25e52a4f0433b5870082

--- dir/dircache/dump.go
+++ dir/dircache/dump.go
@@ -2,10 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// This is meant for debugging. To include in the library, remove the +build line
-// and then go install.
+// This file is meant for debugging. To include in the library, remove the build
+// constraints (one for each historical format) and then go install.
 
-//+build ignore
+//go:build ignore
+// +build ignore
 
 package dircache
 
